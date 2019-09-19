9cd8fac6639fafe3f01fd8ac364b2e98e38d6220
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-09-19 14:26:38 +0100

    County ref data
    #16630

---
+++ src/Common/Common/ReferenceData/County.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.ReferenceData
+{
+    /// <summary>
+    /// Reference data. Geographic region within a country.
+    /// </summary>
+    public class County : BaseReferenceDatum
+    {
+        public virtual Country Country { get; set; }
+    }
+}
