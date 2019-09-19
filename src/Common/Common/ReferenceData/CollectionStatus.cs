d9028fd62e06ab0a0100e2301d0b7ced770c48d0
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-09-19 11:58:06 +0100

    CollectionStatus ref data
    16741

---
+++ src/Common/Common/ReferenceData/CollectionStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.ReferenceData
+{
+    /// <summary>
+    /// Reference data. Status description for a given collection.
+    /// </summary>
+    class CollectionStatus : SortedBaseReferenceDatum
+    {
+    }
+}
