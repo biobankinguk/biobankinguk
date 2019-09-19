c9edfa396f4ecd1fc540768eb533b1029fd1efe4
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-09-19 15:29:10 +0100

    Reference data - funders
    #16634

---
+++ src/Common/Common/ReferenceData/Funder.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.ReferenceData
+{
+    /// <summary>
+    /// Reference Data. Funding body for a given organisation.
+    /// </summary>
+    public class Funder : BaseReferenceDatum
+    {
+    }
+}
