997941302d21cec1336e70b9e33ac6035f577644
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-09-19 10:36:15 +0100

    Added base datum classes (ported from abandoned core rewrite).

---
+++ src/Common/Common/ReferenceData/BaseReferenceDatum.cs
@@ -0,0 +1,20 @@
+﻿namespace Common.ReferenceData
+{
+    /// <summary>
+    /// Base class holding common properties for Reference Data entities.
+    /// </summary>
+    public class BaseReferenceDatum
+    {
+        public int Id { get; set; }
+
+        public string Value { get; set; }
+    }
+
+    /// <summary>
+    /// Base class for Reference Data which has a sort order.
+    /// </summary>
+    public class SortedBaseReferenceDatum : BaseReferenceDatum
+    {
+        public int SortOrder { get; set; }
+    }
+}
