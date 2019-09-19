0abc401b2698d3cd02743bff35070b2d8b4a8b92
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-09-19 10:42:32 +0100

    AnnualStatistics Entity
    #16623

---
+++ src/Common/Common/ReferenceData/AnnualStatistic.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.ReferenceData
+{
+    /// <summary>
+    /// Annual Statistics for a given organisation
+    /// </summary>
+    class AnnualStatistic : BaseReferenceDatum
+    {
+        public string Group { get; set; }
+    }
+}
