c0e305d6df313290677c99469a752e54f7f915dd
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-09-24 10:50:26 +0100

    Ref Data - MaterialType
    #16638

---
+++ src/Common/Common/ReferenceData/MaterialType.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.ReferenceData
+{
+    /// <summary>
+    /// Reference data. The type of material for a given MaterialDetail.
+    /// </summary>
+    public class MaterialType : SortedBaseReferenceDatum
+    {
+    }
+}
