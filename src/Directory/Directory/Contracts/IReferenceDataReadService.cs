a1213c51591854553dae0f364437b8160ee9dc9d
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-09-26 15:06:49 +0100

    Removed empty lines in interface

--- src/Directory/Directory/Contracts/IReferenceDataReadService.cs
+++ src/Directory/Directory/Contracts/IReferenceDataReadService.cs
@@ -10,92 +10,48 @@ namespace Directory.Contracts
     public interface IReferenceDataReadService
     {
         Task<ICollection<AccessCondition>> ListAccessConditions();
-
         Task<AccessCondition> GetAccessCondition(int id);
-
         Task<ICollection<AgeRange>> ListAgeRanges();
-
-
         Task<AgeRange> GetAgeRange(int id);
-
         Task<ICollection<AnnualStatistic>> ListAnnualStatistics();
-
         Task<AnnualStatistic> GetAnnualStatistic(int id);
-
         Task<ICollection<AssociatedDataProcurementTimeframe>> ListAssociatedDataProcurementTimeframes();
-
         Task<AssociatedDataProcurementTimeframe> GetAssociatedDataProcurementTimeframe(int id);
-
         Task<ICollection<AssociatedDataType>> ListAssociatedDateTypes();
-
         Task<AssociatedDataType> GetAssociatedDataType(int id);
-
         Task<ICollection<CollectionPercentage>> ListCollectionPercentages();
-
         Task<CollectionPercentage> GetCollectionPercentage(int id);
-
         Task<ICollection<CollectionPoint>> ListCollectionPoints();
-
         Task<CollectionPoint> GetCollectionPoint(int id);
-
         Task<ICollection<CollectionStatus>> ListCollectionStatuses();
-
         Task<CollectionStatus> GetCollectionStatus(int id);
-
         Task<ICollection<CollectionType>> ListCollectionTypes();
-
         Task<CollectionType> GetCollectionType(int id);
-
         Task<ICollection<ConsentRestriction>> ListConsentRestrictions();
-
         Task<ConsentRestriction> GetConsentRestriction(int id);
-
         Task<ICollection<Country>> ListCountries();
-
         Task<Country> GetCountry(int id);
-
         Task<ICollection<County>> ListCounties();
-
         Task<County> GetCounty(int id);
-
         Task<ICollection<DonorCount>> ListDonorCounts();
-
         Task<DonorCount> GetDonorCount(int id);
-
         Task<ICollection<Funder>> ListFunders();
-
         Task<Funder> GetFunder(int id);
-
         Task<ICollection<HtaStatus>> ListHtaStatuses();
-
         Task<HtaStatus> GetHtaStatus(int id);
-
         Task<ICollection<MacroscopicAssessment>> ListMacroscopicAssessments();
-
         Task<MacroscopicAssessment> GetMacroscopicAssessment(int id);
-
         Task<ICollection<MaterialType>> ListMaterialTypes();
-
         Task<MaterialType> GetMaterialType(int id);
-
         Task<ICollection<OntologyTerm>> ListOntologyTerms();
-
         Task<OntologyTerm> GetOntologyTerm(string id);
-
         Task<ICollection<ServiceOffering>> ListServiceOfferings();
-
         Task<ServiceOffering> GetServiceOffering(int id);
-
         Task<ICollection<Sex>> ListSexes();
-
         Task<Sex> GetSex(int id);
-
         Task<ICollection<SopStatus>> ListSopStatuses();
-
         Task<SopStatus> GetSopStatus(int id);
-
         Task<ICollection<StorageTemperature>> ListStorageTemperatures();
-
         Task<StorageTemperature> GetStorageTemperature(int id);
     }
 }
