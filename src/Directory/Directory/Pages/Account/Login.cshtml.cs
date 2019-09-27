311c502c965ef88e74456a26c24940ec6b50d0b5
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-09-27 09:27:53 +0100

    added bootstrap from cdn while it's our only dependency. added placeholder login page

---
+++ src/Directory/Directory/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Directory.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
