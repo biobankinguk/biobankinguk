0e1ed39c51a13e88522c90a9a01c5c3a640caf91
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-09-30 09:39:06 +0100

    Added Base DTOs
    #17050

---
+++ src/Common/Common/DTO/BaseRefDataDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Common.DTO
+{
+    /// <summary>
+    /// Base DTO for ref data, for use when handling ref data when no ID value is applicable.
+    /// </summary>
+    public class RefDataBaseDto
+    {
+        public string Value { get; set; }
+    }
+
+    /// <summary>
+    /// Base DTO which adds sort order when its applicable.
+    /// </summary>
+    public class SortedRefDataBaseDto : RefDataBaseDto
+    {
+        public int SortOrder { get; set; }
+    }
+}
