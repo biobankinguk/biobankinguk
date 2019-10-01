884aa83b11d2ea6ef2f076cd478ac82b38e53557
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-10-01 15:29:27 +0100

    CollectionStatusController
    #16804

---
+++ src/Directory/Directory/Controllers/CollectionStatusController.cs
@@ -0,0 +1,70 @@
+﻿using Common.DTO;
+using Directory.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Directory.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CollectionStatusController : Controller
+    {
+        private readonly IReferenceDataReadService _readService;
+        private readonly IReferenceDataWriterService _writeService;
+
+        public CollectionStatusController(IReferenceDataReadService readService, IReferenceDataWriterService writeService)
+        {
+            _readService = readService;
+            _writeService = writeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+           => Ok(await _readService.ListCollectionStatuses());
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var collectionStatus = await _readService.GetCollectionStatus(id);
+            if (collectionStatus == null)
+                return NotFound();
+
+            return Ok(collectionStatus);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] SortedRefDataBaseDto collectionStatus)
+        {
+            var createdCollectionStatus = await _writeService.CreateCollectionStatus(collectionStatus);
+            return CreatedAtAction("Get", new { id = createdCollectionStatus.Id }, createdCollectionStatus);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] SortedRefDataBaseDto collectionStatus)
+        {
+            if (_readService.GetCollectionStatus(id) == null)
+                return BadRequest();
+
+            await _writeService.UpdateCollectionStatus(id, collectionStatus);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (await _readService.GetCollectionStatus(id) == null)
+                return NotFound();
+
+            await _writeService.DeleteCollectionStatus(id);
+
+            return NoContent();
+        }
+    }
+}
