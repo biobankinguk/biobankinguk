902585e27f334141a6ba217169c998dbd2b56de8
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-10-01 10:53:09 +0100

    MacroscopicAssessment
    #16796

---
+++ src/Directory/Directory/Controllers/MacroscopicAssessmentController.cs
@@ -0,0 +1,68 @@
+﻿using Common.DTO;
+using Directory.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Threading.Tasks;
+
+namespace Directory.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MacroscopicAssessmentController : Controller
+    {
+        private readonly IReferenceDataReadService _readService;
+        private readonly IReferenceDataWriterService _writeService;
+
+        public MacroscopicAssessmentController(IReferenceDataReadService readService, IReferenceDataWriterService writeService)
+        {
+            _readService = readService;
+            _writeService = writeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+           => Ok(await _readService.ListMacroscopicAssessments());
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var macroscopicAssessment = await _readService.GetMacroscopicAssessment(id);
+            if (macroscopicAssessment == null)
+                return NotFound();
+
+            return Ok(macroscopicAssessment);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] RefDataBaseDto macroscopicAssessment)
+        {
+            var createdMacroscopicAssessment = await _writeService.CreateMacroscopicAssessment(macroscopicAssessment);
+            return CreatedAtAction("Get", new { id = createdMacroscopicAssessment.Id }, createdMacroscopicAssessment);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, RefDataBaseDto macroscopicAssessment)
+        {
+            if (_readService.GetMacroscopicAssessment(id) == null)
+                return BadRequest();
+
+            await _writeService.UpdateMacroscopicAssessment(id, macroscopicAssessment);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (await _readService.GetMacroscopicAssessment(id) == null)
+                return NotFound();
+
+            await _writeService.DeleteMacroscopicAssessment(id);
+
+            return NoContent();
+        }
+    }
+}
