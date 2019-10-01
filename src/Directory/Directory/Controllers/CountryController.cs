80c1a00ea857562e27c571b7aeac17cc39fa5e9c
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-10-01 13:54:01 +0100

    CountryController
    #16802

---
+++ src/Directory/Directory/Controllers/CountryController.cs
@@ -0,0 +1,67 @@
+﻿using Common.DTO;
+using Directory.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Directory.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : Controller
+    {
+        private readonly IReferenceDataReadService _readService;
+        private readonly IReferenceDataWriterService _writeService;
+
+        public CountryController(IReferenceDataReadService readService, IReferenceDataWriterService writeService)
+        {
+            _readService = readService;
+            _writeService = writeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+           => Ok(await _readService.ListCountries());
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var country = await _readService.GetCountry(id);
+            if (country == null)
+                return NotFound();
+
+            return Ok(country);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] SortedRefDataBaseDto country)
+        {
+            var createdCountry = await _writeService.CreateCountry(country);
+            return CreatedAtAction("Get", new { id = createdCountry.Id }, createdCountry);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, SortedRefDataBaseDto country)
+        {
+            if (_readService.GetCountry(id) == null)
+                return BadRequest();
+
+            await _writeService.UpdateCountry(id, country);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (await _readService.GetCountry(id) == null)
+                return NotFound();
+
+            await _writeService.DeleteCountry(id);
+
+            return NoContent();
+        }
+    }
+}
