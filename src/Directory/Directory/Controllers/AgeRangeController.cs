bfc6a34dee029707dc86750747f32b148ba374b7
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-10-02 11:27:04 +0100

    Age range Controller
    #16811

---
+++ src/Directory/Directory/Controllers/AgeRangeController.cs
@@ -0,0 +1,67 @@
+﻿using Common.DTO;
+using Directory.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Directory.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgeRangeController : Controller
+    {
+        private readonly IReferenceDataReadService _readService;
+        private readonly IReferenceDataWriterService _writeService;
+
+        public AgeRangeController(IReferenceDataReadService readService, IReferenceDataWriterService writeService)
+        {
+            _readService = readService;
+            _writeService = writeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+           => Ok(await _readService.ListAgeRanges());
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var collectionPoint = await _readService.GetAgeRange(id);
+            if (collectionPoint == null)
+                return NotFound();
+
+            return Ok(collectionPoint);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] SortedRefDataBaseDto collectionPoint)
+        {
+            var createdAgeRange = await _writeService.CreateAgeRange(collectionPoint);
+            return CreatedAtAction("Get", new { id = createdAgeRange.Id }, createdAgeRange);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] SortedRefDataBaseDto collectionPoint)
+        {
+            if (_readService.GetAgeRange(id) == null)
+                return BadRequest();
+
+            await _writeService.UpdateAgeRange(id, collectionPoint);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (await _readService.GetAgeRange(id) == null)
+                return NotFound();
+
+            await _writeService.DeleteAgeRange(id);
+
+            return NoContent();
+        }
+    }
+}
