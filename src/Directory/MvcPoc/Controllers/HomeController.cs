8d64a2815f0502e8bcce20e2588fed1c7a6214ea
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-10-03 14:00:58 +0100

    added a basic mvc app to use as poc for server side auth client

---
+++ src/Directory/MvcPoc/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcPoc.Models;
+
+namespace MvcPoc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Index() => "go to `/Home/Identity` to test authorising against a protected route using the IdP";
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
