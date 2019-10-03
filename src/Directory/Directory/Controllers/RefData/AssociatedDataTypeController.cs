838296a249ebeaddc4e9293f4deecf2c36c6a6ec
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-10-03 13:59:50 +0100

    AssociatedDataType Controller
    #17171

--- src/Directory/Directory/Controllers/RefData/AssociatedDataTypeController.cs
+++ src/Directory/Directory/Controllers/RefData/AssociatedDataTypeController.cs
@@ -1,7 +1,9 @@
-﻿using Common.DTO;
+﻿using Common.Data.ReferenceData;
+using Common.DTO;
 using Directory.Contracts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Threading.Tasks;
 
 namespace Directory.Controllers.RefData
@@ -20,11 +22,14 @@ namespace Directory.Controllers.RefData
             _writeService = writeService;
         }
 
+        [SwaggerOperation(Description = "List of all Associated Data Type")]
         [HttpGet]
         public async Task<IActionResult> Index()
            => Ok(await _readService.ListAssociatedDataTypes());
 
-
+        [SwaggerOperation(Description = "Get a single Associated Data Type by ID")]
+        [SwaggerResponse(200, "The Associated Data Type with the requested ID.", typeof(AssociatedDataType))]
+        [SwaggerResponse(404, "No Associated Data Type was found with the provided ID.")]
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
@@ -35,6 +40,9 @@ namespace Directory.Controllers.RefData
             return Ok(collectionPoint);
         }
 
+        [SwaggerOperation(Description = "Creates a new Associated Data Type")]
+        [SwaggerResponse(201, "The Associated Data Type was created", typeof(AssociatedDataType))]
+        [SwaggerResponse(400, "The data is invalid")]
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] SortedRefDataBaseDto collectionPoint)
         {
@@ -42,6 +50,9 @@ namespace Directory.Controllers.RefData
             return CreatedAtAction("Get", new { id = createdAssociatedDataType.Id }, createdAssociatedDataType);
         }
 
+        [SwaggerOperation(Description = "Updates an existing Associated Data Type")]
+        [SwaggerResponse(204, "The Associated Data Type was updated successfully.")]
+        [SwaggerResponse(400, "No Associated Data Type was found with the provided ID.")]
         [HttpPut("{id}")]
         public async Task<IActionResult> Put(int id, [FromBody] SortedRefDataBaseDto collectionPoint)
         {
@@ -53,6 +64,8 @@ namespace Directory.Controllers.RefData
             return NoContent();
         }
 
+        [SwaggerOperation("Delete a single Associated Data Type by ID.")]
+        [SwaggerResponse(204, "The Associated Data Type was succesfully deleted.")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
