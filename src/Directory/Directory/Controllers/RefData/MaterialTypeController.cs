401ee6e75f4c264bb52d89b676ed02fae7ca0b0e
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-10-04 15:42:39 +0100

    Material Type Swagger
    #17186

--- src/Directory/Directory/Controllers/RefData/MaterialTypeController.cs
+++ src/Directory/Directory/Controllers/RefData/MaterialTypeController.cs
@@ -1,7 +1,9 @@
-﻿using Common.DTO;
+﻿using Common.Data.ReferenceData;
+using Common.DTO;
 using Directory.Contracts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Threading.Tasks;
 
 namespace Directory.Controllers.RefData
@@ -20,11 +22,14 @@ namespace Directory.Controllers.RefData
             _writeService = writeService;
         }
 
+        [SwaggerOperation("List of all Material Types")]
         [HttpGet]
         public async Task<IActionResult> Index()
            => Ok(await _readService.ListMaterialTypes());
 
-
+        [SwaggerOperation("Get a single Material Type by ID")]
+        [SwaggerResponse(200, "The Material Type with the requested ID.", typeof(MaterialType))]
+        [SwaggerResponse(404, "No Material Type was found with the provided ID.")]
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
@@ -35,6 +40,9 @@ namespace Directory.Controllers.RefData
             return Ok(materialType);
         }
 
+        [SwaggerOperation("Creates a new Material Type")]
+        [SwaggerResponse(201, "The Material Type was created", typeof(MaterialType))]
+        [SwaggerResponse(400, "The data is invalid")]
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] SortedRefDataBaseDto materialType)
         {
@@ -42,6 +50,9 @@ namespace Directory.Controllers.RefData
             return CreatedAtAction("Get", new { id = createdMaterialType.Id }, createdMaterialType);
         }
 
+        [SwaggerOperation("Updates an existing Material Type")]
+        [SwaggerResponse(204, "The Material Type was updated successfully.")]
+        [SwaggerResponse(400, "No Material Type was found with the provided ID.")]
         [HttpPut("{id}")]
         public async Task<IActionResult> Put(int id, [FromBody] SortedRefDataBaseDto materialType)
         {
@@ -53,6 +64,8 @@ namespace Directory.Controllers.RefData
             return NoContent();
         }
 
+        [SwaggerOperation("Delete a single Material Type by ID.")]
+        [SwaggerResponse(204, "The Material Type was succesfully deleted.")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
