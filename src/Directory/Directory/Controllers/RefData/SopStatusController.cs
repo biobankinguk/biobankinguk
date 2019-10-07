ff7d210baf3b893d8430e9cc3e3d9438fe72a3d1
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-10-07 09:14:22 +0100

    SopStatus Swagger
    #17190

--- src/Directory/Directory/Controllers/RefData/SopStatusController.cs
+++ src/Directory/Directory/Controllers/RefData/SopStatusController.cs
@@ -1,7 +1,9 @@
-﻿using Common.DTO;
+﻿using Common.Data.ReferenceData;
+using Common.DTO;
 using Directory.Contracts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Threading.Tasks;
 
 namespace Directory.Controllers.RefData
@@ -20,11 +22,14 @@ namespace Directory.Controllers.RefData
             _writeService = writeService;
         }
 
+        [SwaggerOperation("List of all SopStatus")]
         [HttpGet]
         public async Task<IActionResult> Index()
             => Ok(await _readService.ListSopStatuses());
-        
 
+        [SwaggerOperation("Get a single SOP Status by ID")]
+        [SwaggerResponse(200, "The SOP Status with the requested ID.", typeof(SopStatus))]
+        [SwaggerResponse(404, "No SOP Status was found with the provided ID.")]
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
@@ -34,15 +39,20 @@ namespace Directory.Controllers.RefData
 
             return Ok(sopStatus);
         }
-            
 
+        [SwaggerOperation("Creates a new Material Type")]
+        [SwaggerResponse(201, "The Material Type was created", typeof(MaterialType))]
+        [SwaggerResponse(400, "The data is invalid")]
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] SortedRefDataBaseDto sopStatus)
         {
             var createdSopStatus = await _writeService.CreateSopStatus(sopStatus);
             return CreatedAtAction("Get", new { id = createdSopStatus.Id }, createdSopStatus);
         }
-         
+
+        [SwaggerOperation("Updates an existing SOP Status")]
+        [SwaggerResponse(204, "The SOP Status was updated successfully.")]
+        [SwaggerResponse(400, "No SOP Status was found with the provided ID.")]
         [HttpPut("{id}")]
         public async Task<IActionResult> Put(int id, [FromBody] SortedRefDataBaseDto sopStatus)
         {
@@ -54,6 +64,8 @@ namespace Directory.Controllers.RefData
             return NoContent();
         }
 
+        [SwaggerOperation("Delete a single SOP Status by ID.")]
+        [SwaggerResponse(204, "The SOP Status was succesfully deleted.")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
