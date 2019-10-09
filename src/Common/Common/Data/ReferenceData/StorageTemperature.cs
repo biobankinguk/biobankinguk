177646c41eea5940e0313a771fa765837d730dca
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-10-09 11:17:44 +0100

    Some more data seeding and change to StorageTemp

--- src/Common/Common/Data/ReferenceData/StorageTemperature.cs
+++ src/Common/Common/Data/ReferenceData/StorageTemperature.cs
@@ -5,5 +5,7 @@
     /// </summary>
     public class StorageTemperature : SortedBaseReferenceDatum
     {
+        public string? Minimum { get; set; }
+        public string? Maximum { get; set; }
     }
 }
