12e0607aeb8887aa38e31fe800c77ae5f27e8911
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-10-24 10:25:30 +0100

    🔥 Sherwood: gone as we don't need a component library right now

--- src/sherwood/stories/Box.stories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Box from "../Box";
-
-storiesOf("Box", module)
-  .add("default", () => <Box>Hello World</Box>)
-  .add("styled-system", () => <Box color="red" ml={50}>Hello World</Box>);
