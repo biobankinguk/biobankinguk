8d2277b834b36a7f0bde355dfc9a17834d81a001
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-10-24 10:47:12 +0100

    ⚡Theme: theme no longer includes chakra base
    it can be merged with other base themes if desirable in the target app

--- src/theme/src/theme.js
+++ src/theme/src/theme.js
@@ -1,20 +1,11 @@
-import { theme } from "@chakra-ui/core";
 import { literals, site, brand } from "./data/colors";
 import buildPalettes from "./build/color-palette";
 
 export default {
-  ...theme,
-
   colors: {
-    ...theme.colors,
     literals,
     brand,
-    defaultBackground: theme.colors.gray["200"],
+    defaultBackground: "#e9ecef",
     ...buildPalettes(site)
-  },
-
-  // Chakra's icons don't survive our current build process
-  // but we can use other libraries such as `react-icons`
-  // in the actual app, and the app build will tree shake
-  icons: undefined
+  }
 };
