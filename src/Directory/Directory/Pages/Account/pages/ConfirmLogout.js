8db67ec8e4e5628d3ae293da22dce6312e995a4e
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-10-24 13:50:16 +0100

    ⚡Core: ConfirmLogout page layout and style

--- src/Directory/Directory/Pages/Account/pages/ConfirmLogout.js
+++ src/Directory/Directory/Pages/Account/pages/ConfirmLogout.js
@@ -2,31 +2,36 @@ import React from "react";
 import { Formik, Form } from "formik";
 import { postObjectAsFormData, constants } from "js-forms";
 import Layout from "../../Shared/Layout";
+import { Text, Button, Flex } from "@chakra-ui/core";
 
 const ConfirmLogout = () => (
-  <Layout>
-    <h1>Logout</h1>
+  <Layout heading="Logout">
+    <Flex flexDirection="column" alignItems="center">
+      <Text p={4}>Would you like to logout of the UKCRC Tissue Directory?</Text>
 
-    <p>Would you like to logout of the UKCRC Tissue Directory?</p>
-
-    <Formik
-      onSubmit={(values, actions) => {
-        const aspForm = document.getElementById("asp-form");
-        postObjectAsFormData(aspForm.action, {
-          ...values,
-          [constants.aspNetCoreCsrf]:
-            aspForm.elements[constants.aspNetCoreCsrf].value
-        });
-        actions.setSubmitting(false);
-      }}
-      render={p => (
-        <Form>
-          <button type="submit" disabled={p.isSubmitting}>
-            Yes
-          </button>
-        </Form>
-      )}
-    />
+      <Formik
+        onSubmit={(values, actions) => {
+          const aspForm = document.getElementById("asp-form");
+          postObjectAsFormData(aspForm.action, {
+            ...values,
+            [constants.aspNetCoreCsrf]:
+              aspForm.elements[constants.aspNetCoreCsrf].value
+          });
+          actions.setSubmitting(false);
+        }}
+        render={p => (
+          <Form>
+            <Button
+              variantColor="primary"
+              type="submit"
+              disabled={p.isSubmitting}
+            >
+              Logout
+            </Button>
+          </Form>
+        )}
+      />
+    </Flex>
   </Layout>
 );
 
