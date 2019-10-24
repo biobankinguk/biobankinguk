9d3a3c1bc9730e13eea22bd197a8913fc3e77d5a
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-10-24 12:09:01 +0100

    ⚡Core: Login Form model errors bubble up properly

--- src/Directory/Directory/Pages/Account/pages/Login.js
+++ src/Directory/Directory/Pages/Account/pages/Login.js
@@ -1,11 +1,28 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
 import Layout from "../../Shared/Layout";
+import { Flex, Alert, AlertDescription } from "@chakra-ui/core";
 
-const Login = () => (
-  <Layout heading="Login">
-    <LoginForm />
-  </Layout>
-);
+const Login = () => {
+  const valSummary = document.getElementById("asp-validation-summary");
+  let valErrors = null;
+  if (valSummary && valSummary.className.includes("validation-summary-errors"))
+    valErrors = (
+      <Alert status="error" my={2} p={2}>
+        <AlertDescription
+          dangerouslySetInnerHTML={{ __html: valSummary.innerHTML }}
+        ></AlertDescription>
+      </Alert>
+    );
+
+  return (
+    <Layout heading="Login">
+      <Flex flexDirection="column">
+        {valErrors}
+        <LoginForm />
+      </Flex>
+    </Layout>
+  );
+};
 
 export default Login;
