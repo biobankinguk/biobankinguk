0cc709e77b6f24e6ac9194740b1b77e6db3e62c2
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-10-24 15:33:15 +0100

    ✨Core: Initial Razor Page for Registration

---
+++ src/Directory/Directory/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Directory.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public string? Route { get; set; }
+
+        [BindProperty]
+        [Required]
+        [DataType(DataType.Text)]
+        public string? FullName { get; set; }
+
+        [BindProperty]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [BindProperty]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? EmailConfirm { get; set; }
+
+        [BindProperty]
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [BindProperty]
+        [Required]
+        [DataType(DataType.Password)]
+        public string? PasswordConfirm { get; set; }
+
+        public void OnGet() => Page();
+
+
+    }
+}
