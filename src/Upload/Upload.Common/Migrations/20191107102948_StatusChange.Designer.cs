ad686fcbab1cdc575f48ca9abc6ce4de14f26af3
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2019-11-07 14:55:43 +0000

    Migration for data change.

--- src/Upload/Upload.Common/Migrations/20191107102948_StatusChange.Designer.cs
+++ src/Upload/Upload.Common/Migrations/20191107102948_StatusChange.Designer.cs
@@ -4,14 +4,16 @@ using Common.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Upload.Common.Migrations
 {
     [DbContext(typeof(UploadContext))]
-    partial class UploadContextModelSnapshot : ModelSnapshot
+    [Migration("20191107102948_StatusChange")]
+    partial class StatusChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
