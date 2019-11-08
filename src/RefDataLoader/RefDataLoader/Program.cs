e71d50662b5ec4ba449dffb401f50b6b157a0d42
Jonathan Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2019-11-08 10:11:54 +0000

    ⚡RefDataLoader: Now uses Generic Host (#16)

--- src/RefDataLoader/RefDataLoader/Program.cs
+++ src/RefDataLoader/RefDataLoader/Program.cs
@@ -1,50 +1,16 @@
-﻿using Config;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.IO;
-using System.Net.Http.Headers;
+﻿using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
 
 namespace RefDataLoader
 {
     public static class Program
     {
-        //todo figure out how to use fancy core 3.0 stuff to tidy this up
-        static async Task Main()
-        {
-            var dataService = (DataService)ConfigureServices()
-               .GetService(typeof(IDataService));
-            await dataService.SeedData();
-        }
+        static async Task Main(string[] args)
+            => await CreateHostBuilder(args).RunConsoleAsync();
 
-        private static IServiceProvider ConfigureServices()
-        {
-            var services = new ServiceCollection();
-
-            // Config
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", false, true)
-                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", true)
-                .AddEnvironmentVariables()
-                .Build();
-            services.AddSingleton(_ => config);
-            services.Configure<ApiSettings>(config.GetSection("ApiSettings"));
-
-            services.AddTransient<IDataService, DataService>();
-
-            // Services
-            services.AddHttpClient<IDataService, DataService>(client =>
-            {
-                client.BaseAddress = new Uri(config["ApiSettings:BaseUri"]);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(
-                    new MediaTypeWithQualityHeaderValue("application/json"));
-            });
-            
-
-            return services.BuildServiceProvider();
-        }
+        public static IHostBuilder CreateHostBuilder(string[] args)
+            => Host.CreateDefaultBuilder(args)
+                .ConfigureServices(Startup.ConfigureServices)
+                .ConfigureServices(Startup.Configure);
     }
 }
---
+++ src/RefDataLoader/RefDataLoader/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http.Headers;
+using Config;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace RefDataLoader
+{
+    public static class Startup
+    {
+        /// <summary>
+        /// Configure Dependencies
+        /// </summary>
+        public static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            var config = context.Configuration;
+
+            services.Configure<ApiSettings>(config.GetSection("ApiSettings"));
+
+            services.AddHttpClient(string.Empty, client =>
+            {
+                client.BaseAddress = new Uri(config["ApiSettings:BaseUri"]);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+            });
+        }
+
+        /// <summary>
+        /// Configure Startup Services
+        /// </summary>
+        public static void Configure(HostBuilderContext context, IServiceCollection services)
+        {
+            services.AddHostedService<DataService>();
+        }
+    }
+}
