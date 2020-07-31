db8bc2450753dd9c9d2f7d4068e27e35e8a972a3
James Fleming <jamesfleming91@gmail.com>
2020-07-31 10:16:22 +0100

    Renamed MockWebApp to PublicationMockApp. Deleted testPublications

--- src/Publications/Publications/Services/Contracts/IBiobankService.cs
+++ src/Publications/Publications/Services/Contracts/IBiobankService.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Publications.Services.Contracts
 {
     public interface IBiobankService
     {
-        Task<List<string>> GetOrganisationNames();
     }
 }
---
+++ src/Publications/Publications/Services/Contracts/IEPMCService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publications.Services.Contracts
+{
+    public interface IEPMCService
+    {
+        Task<PublicationDTO> GetPublicationById(int publicationId);
+
+        Task<List<PublicationDTO>> GetOrganisationPublications(string biobank);
+    }
+}
