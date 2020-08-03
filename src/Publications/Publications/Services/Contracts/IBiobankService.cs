039f676a1610bc128c272fbf825672ad67eddc68
James Fleming <jamesfleming91@gmail.com>
2020-08-03 10:16:15 +0100

    Added IBiobankService

--- src/Publications/Publications/Services/Contracts/IBiobankService.cs
+++ src/Publications/Publications/Services/Contracts/IBiobankService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Publications.Services.Contracts
 {
     public interface IBiobankService
     {
+        Task<List<string>> GetOrganisationNames();
     }
 }
--- src/Publications/Publications/Services/Hosted/FetchPublicationsService.cs
+++ src/Publications/Publications/Services/Hosted/FetchPublicationsService.cs
@@ -32,7 +32,7 @@ namespace Publications.Services
                 // DI of required services
                 var publicationService = scope.ServiceProvider.GetRequiredService<IPublicationService>();
                 var biobankWebService = scope.ServiceProvider.GetRequiredService<IBiobankService>();
-                var epmcWebService = scope.ServiceProvider.GetRequiredService<IEMPCService>();
+                var epmcWebService = scope.ServiceProvider.GetRequiredService<IEPMCService>();
 
                 // Call directory for all active organisation
                 var biobanks = await biobankWebService.GetOrganisationNames();
