1424a6c72ee8abdbf55d669e3db33a68bb6e7ece
Joseph Lavagna <josephlavagna1998@gmail.com>
2020-08-05 14:33:01 +0100

    Added connection strings from Class Library to Azure app local settings

--- src/Publications/PublicationsAzureFunctions/EPMCFunction.cs
+++ src/Publications/PublicationsAzureFunctions/EPMCFunction.cs
@@ -1,16 +1,10 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Publications.Services;
 using Publications.Services.Contracts;
-using Publications;
-using Microsoft.Extensions.Configuration;
 
 namespace PublicationsAzureFunctions
 {
@@ -27,8 +21,11 @@ namespace PublicationsAzureFunctions
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            string biobank = "test";
+            //This string will be passed through the endpoint
+            string biobank = "CANDAS";
             var publications  = _epmc.GetOrganisationPublications(biobank);
+            
+
 
             return new OkObjectResult(publications);
         }
