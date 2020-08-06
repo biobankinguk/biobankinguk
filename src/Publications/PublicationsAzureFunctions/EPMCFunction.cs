c2932cfab6c7c28c8c2c5c31e3694fd4279b9b61
Joseph Lavagna <josephlavagna1998@gmail.com>
2020-08-06 14:01:33 +0100

    Working Azure Function - gets pubs and posts them to Azure DB - Added new migration, changed connection strings - needs cleaning up

--- src/Publications/PublicationsAzureFunctions/EPMCFunction.cs
+++ src/Publications/PublicationsAzureFunctions/EPMCFunction.cs
@@ -13,23 +13,32 @@ namespace PublicationsAzureFunctions
     public class EPMCFunction
     {
         private readonly IEPMCService _epmc;
-        public EPMCFunction(IEPMCService empc)
+        private IPublicationService _publication;
+        public EPMCFunction(IEPMCService empc, IPublicationService publication)
         {
             _epmc = empc;
+            _publication = publication;
         }
 
         [FunctionName("EPMCFunction")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "GetPublications/{biobank}")] 
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "GetPublications/{biobank}")] 
         HttpRequest req,
             string biobank,
             ILogger log)
         {
+            log.LogInformation($"Fetching publications for {biobank}");
             //Consume API and load into DTO
-            Task<List<Publications.PublicationDTO>> publications = _epmc.GetOrganisationPublications(biobank);
+            var publications = await _epmc.GetOrganisationPublications(biobank);
 
-            //Trigger next function (Publications service) and pass DTO 
-            return new OkObjectResult($"Hello, {biobank}");
+            await _publication.AddOrganisationPublications(biobank, publications);
+
+            log.LogInformation($"Fetched {publications.Count} for {biobank}");
+            
+            return new OkObjectResult($"Fetched {publications.Count} for {biobank}");
+
+    
+            
         }
     }
 }
--- src/Publications/PublicationsAzureFunctions/Startup.cs
+++ src/Publications/PublicationsAzureFunctions/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -15,12 +16,18 @@ namespace PublicationsAzureFunctions
 {
     class Startup : FunctionsStartup
     {
+   
         public override void Configure(IFunctionsHostBuilder builder)
         {
+
+            string SqlConnection = "Server=tcp:publicationsazuresql.database.windows.net,1433;Initial Catalog=Publications;Persist Security Info=False;User ID=azureuser;Password=Publications123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            builder.Services.AddDbContext<PublicationDbContext>(options =>
+                options.UseSqlServer(SqlConnection));
             builder.Services.AddHttpClient();
             builder.Services.AddScoped<IEPMCService, EMPCWebService>();
             builder.Services.AddScoped<IPublicationService, PublicationService>();
 
         }
+
     }
 }
