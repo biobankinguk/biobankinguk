b45f8681ad075582652bb7244b306f3a01467fe7
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2020-12-02 11:47:04 +0100

    remove migrations and relegate entities control to directory

--- src/Analytics/Analytics/AnalyticsDbContext.cs
+++ src/Analytics/Analytics/AnalyticsDbContext.cs
@@ -13,20 +13,6 @@ namespace Analytics.Data
         public DbSet<Organisation> Organisations { get; set; }
 
         public AnalyticsDbContext(DbContextOptions<AnalyticsDbContext> options) : base(options) { }
-
-        /*  Required for DesignTime creation of the context. Used for running EF migrations in CLI and devops pipelines. */
-        public class AnalyticsDbContextFactory : IDesignTimeDbContextFactory<AnalyticsDbContext>
-        {
-            public AnalyticsDbContext CreateDbContext(string[] args)
-            {
-                var options = new DbContextOptionsBuilder<AnalyticsDbContext>();
-
-                // Connection string passed from environment variable as EF doesn't (yet) support supplying it as a CLI parameter
-                options.UseSqlServer(Environment.GetEnvironmentVariable("analyticsdb_connection"), options => options.EnableRetryOnFailure());
-                
-                return new AnalyticsDbContext(options.Options);
-            }
-        }
     }
 
 }
