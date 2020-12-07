29e8eb32e53d376bf96e635157986192910b6ae6
James Fleming <james.fleming@nottingham.ac.uk>
2020-12-07 15:51:54 +0000

    Ammended submission service to apply to only open submissions

--- src/Api/SubmissionExpiryJob/Services/SubmissionService.cs
+++ src/Api/SubmissionExpiryJob/Services/SubmissionService.cs
@@ -28,30 +28,44 @@ namespace Biobanks.SubmissionExpiryJob.Services
             var expiry = DateTime.Now.Subtract(TimeSpan.FromDays(_settings.ExpiryDays));
 
             return await _db.Submissions
-                    .Where(s => s.StatusChangeTimestamp < expiry)
-                    .Select(s => s.BiobankId)
-                    .Distinct()
-                    .ToListAsync();
-        } 
+                .GroupBy(s => s.BiobankId)
+                .Select(g => 
+                    // Select Latest Open Submission
+                    g.Where(s => s.Status.Value == Statuses.Open)
+                        .OrderByDescending(s => s.StatusChangeTimestamp)
+                        .FirstOrDefault()
+                )  
+                .Where(s => 
+                    // Filter Out Submission That Haven't Expired
+                    s != null && s.StatusChangeTimestamp < expiry
+                )
+                .Select(s => s.BiobankId)
+                .ToListAsync();
+        }
 
         public async Task ExpireSubmissions(int organisationId)
         {
-            // Remove All of Submitted Data
+            // Remove All of Staged Data
             await _db.StagedDiagnoses.Where(sd => sd.OrganisationId == organisationId).DeleteAsync();
             await _db.StagedSamples.Where(ss => ss.OrganisationId == organisationId).DeleteAsync();
             await _db.StagedTreatments.Where(st => st.OrganisationId == organisationId).DeleteAsync();
 
-            // Remove All of Submitted Deletes
+            // Remove All of Staged Deletes
             await _db.StagedDiagnosisDeletes.Where(sdd => sdd.OrganisationId == organisationId).DeleteAsync();
             await _db.StagedSampleDeletes.Where(ssd => ssd.OrganisationId == organisationId).DeleteAsync();
             await _db.StagedTreatmentDeletes.Where(std => std.OrganisationId == organisationId).DeleteAsync();
 
-            // Mark Submissions As Rejected
-            foreach (var submission in _db.Submissions.Where(s => s.BiobankId == organisationId))
-            {
-                submission.StatusChangeTimestamp = DateTime.Now;
-                submission.Status = await _db.Statuses.FirstOrDefaultAsync(s => s.Value == Statuses.Expired);
-            }
+            // Mark Submissions As Expired
+            var expiredStatus = await _db.Statuses.FirstOrDefaultAsync(s => s.Value == Statuses.Expired);
+
+            await _db.Submissions
+                .Where(s => s.BiobankId == organisationId)
+                .Where(s => s.Status.Value == Statuses.Open)
+                .ForEachAsync(s =>
+                {
+                    s.StatusChangeTimestamp = DateTime.Now;
+                    s.Status = expiredStatus;
+                });
 
             await _db.SaveChangesAsync();
         }
