823d2fced8533412b8abfc8b4dff93b61e628f1f
Joseph Lavagna <josephlavagna1998@gmail.com>
2020-12-10 16:25:33 +0000

    Added join entity to LegacyProject

---
+++ src/LegacyData/Entities/JoinEntities/MaterialTypeMaterialTypeGroup.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Biobanks.Common.Data.Entities.ReferenceData;
+
+namespace Biobanks.Common.Data.Entities.JoinEntities
+{
+    /// <summary>
+    /// Many to many join table for Material Types and Material Type Groups
+    /// </summary>
+    public class MaterialTypeMaterialTypeGroup
+    {
+        /// <summary>
+        /// Foreign key column.
+        /// </summary>
+        public int MaterialTypeId { get; set; }
+        /// <summary>
+        /// The Material Type in the relationship.
+        /// </summary>
+        [Required]
+        public MaterialType MaterialType { get; set; }
+
+        /// <summary>
+        /// Foreign key column.
+        /// </summary>
+        public int MaterialTypeGroupId { get; set; }
+        /// <summary>
+        /// The Material Type Group in the relationship.
+        /// </summary>
+        [Required]
+        public MaterialTypeGroup MaterialTypeGroup { get; set; }
+    }
+}
