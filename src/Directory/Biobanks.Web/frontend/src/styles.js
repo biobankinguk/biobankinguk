0a9479060053e4be004cedbed161765aea3fcdb5
Jonathan Couldridge <jonathan.couldridge@nottingham.ac.uk>
2021-01-04 09:06:48 +0000

    initial webpack

---
+++ src/Directory/Biobanks.Web/frontend/src/styles.js
@@ -0,0 +1,3 @@
+// Note that bootstrap 3's css references the bootstrap fonts
+// so we distribute those too, even if we never really use them.
+import "bootstrap/dist/css/bootstrap.min.css";
---
+++ src/Directory/Biobanks.Web/frontend/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
