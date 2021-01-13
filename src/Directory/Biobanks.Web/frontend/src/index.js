d2a340df53ce3bc688503d73553af978571bf8e7
Jonathan Couldridge <jonathan.couldridge@nottingham.ac.uk>
2021-01-13 11:31:01 +0000

    bundling jquery, separating css

--- src/Directory/Biobanks.Web/frontend/src/index.js
+++ src/Directory/Biobanks.Web/frontend/src/index.js
@@ -1,3 +1,5 @@
 import "./styles";
 
+import "./globals";
+
 import "bootstrap";
--- src/Directory/Biobanks.Web/frontend/webpack.config.js
+++ src/Directory/Biobanks.Web/frontend/webpack.config.js
@@ -1,9 +1,25 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: (resourcePath, context) => {
+                // publicPath is the relative path of the resource to the context
+                // e.g. for ./css/admin/main.css the publicPath will be ../../
+                // while for ./css/main.css the publicPath will be ../
+                return path.relative(path.dirname(resourcePath), context) + "/";
+              },
+            },
+          },
+          "css-loader",
+        ],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
@@ -19,4 +35,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new MiniCssExtractPlugin()],
 };
