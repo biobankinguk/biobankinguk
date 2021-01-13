e6e6d86a8617405f36ddb45b136f8f0a3a0d18e1
Jonathan Couldridge <jonathan.couldridge@nottingham.ac.uk>
2021-01-13 12:14:43 +0000

    all the css

--- src/Directory/Biobanks.Web/frontend/src/styles.js
+++ src/Directory/Biobanks.Web/frontend/src/styles.js
@@ -1,3 +1,24 @@
 // Note that bootstrap 3's css references the bootstrap fonts
 // so we distribute those too, even if we never really use them.
+
+// Bootstrap 3.x + theme
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap-theme.min.css";
+
+// DataTables.net + plugins, Bootstrap 3.x styles
+import "datatables.net-bs/css/dataTables.bootstrap.min.css";
+import "datatables.net-buttons-bs/css/buttons.bootstrap.min.css";
+import "datatables.net-rowreorder-bs/css/rowReorder.bootstrap.min.css";
+
+// Other
+import "bootstrap-slider/dist/css/bootstrap-slider.min.css";
+
+import "font-awesome/css/font-awesome.min.css";
+
+// local vendor copies
+import "./vendor/css/typeahead.js-bootstrap3.css";
+
+// local source
+import "./css/mdd_styles.css";
+import "./css/site.css";
+import "./css/nav.css";
