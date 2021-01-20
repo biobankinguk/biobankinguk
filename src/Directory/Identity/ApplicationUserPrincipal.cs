12dc4efbaa54a71b64d4c3ae5752458954b2a679
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2021-01-20 16:33:44 +0000

    Moved custom claims logic to method

--- src/Directory/Identity/ApplicationUserPrincipal.cs
+++ src/Directory/Identity/ApplicationUserPrincipal.cs
@@ -14,9 +14,19 @@ namespace Directory.Identity
         //Add custom claims...
         public string Name => FindFirst(CustomClaimType.FullName).Value; //ClaimTypes.Name is used for username, which is acquirable directly from the identity instead
         public string Email => FindFirst(CustomClaimType.Email).Value;
-        public Dictionary<int, string> Biobanks => FindAll(CustomClaimType.Biobank).ToDictionary(x => JsonConvert.DeserializeObject<KeyValuePair<int, string>>(x.Value).Key, x => JsonConvert.DeserializeObject<KeyValuePair<int, string>>(x.Value).Value);
-        public Dictionary<int, string> Networks => FindAll(CustomClaimType.Network).ToDictionary(x => JsonConvert.DeserializeObject<KeyValuePair<int, string>>(x.Value).Key, x => JsonConvert.DeserializeObject<KeyValuePair<int, string>>(x.Value).Value);
-        public Dictionary<int, string> BiobankRequests => FindAll(CustomClaimType.BiobankRequest).ToDictionary(x => JsonConvert.DeserializeObject<KeyValuePair<int, string>>(x.Value).Key, x => JsonConvert.DeserializeObject<KeyValuePair<int, string>>(x.Value).Value);
-        public Dictionary<int, string> NetworkRequests => FindAll(CustomClaimType.NetworkRequest).ToDictionary(x => JsonConvert.DeserializeObject<KeyValuePair<int, string>>(x.Value).Key, x => JsonConvert.DeserializeObject<KeyValuePair<int, string>>(x.Value).Value);
+        public Dictionary<int, string> Biobanks => CreateClaimsDictionary(CustomClaimType.Biobank);
+        public Dictionary<int, string> Networks => CreateClaimsDictionary(CustomClaimType.Network);
+        public Dictionary<int, string> BiobankRequests => CreateClaimsDictionary(CustomClaimType.BiobankRequest);
+        public Dictionary<int, string> NetworkRequests => CreateClaimsDictionary(CustomClaimType.NetworkRequest);
+
+        /// <summary>
+        /// Return all claims of a given type as a dictionary containing Id and Name
+        /// </summary>
+        /// <param name="claimType"></param>
+        /// <returns></returns>
+        private Dictionary<int, string> CreateClaimsDictionary(string claimType)
+            => FindAll(claimType).ToDictionary(x => JsonConvert
+            .DeserializeObject<KeyValuePair<int, string>>(x.Value).Key, x => JsonConvert
+            .DeserializeObject<KeyValuePair<int, string>>(x.Value).Value);
     }
 }
