a0454c52efb1d0fd77482bf5a5ad74d808a24021
Joseph Lavagna <josephlavagna1998@gmail.com>
2021-01-21 15:59:31 +0000

    Added Annotation Entity

---
+++ src/Directory/Entity/Data/Annotation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directory.Entity.Data
+{
+    public class Annotation
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Uri { get; set; }
+
+        public List<Publication> Publications { get; set; }
+    }
+}
