11845f4effc893fee8194312fd644ab108cdeaab
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-01-25 12:41:50 +0000

    update disease statuses

--- src/Directory/Biobanks.Web/Scripts/ADAC/adac-disease-statuses.js
+++ src/Directory/Biobanks.Web/Scripts/ADAC/adac-disease-statuses.js
@@ -64,11 +64,12 @@ function AdacDiseaseStatusViewModel() {
         if (action == 'Add') {
             var ajaxType = 'POST'
             var url = resourceUrl;
+            var feedbackfn = setAddFeedback; // cf. adac-refdata-feedback.js
         } else if (action == 'Update') {
             var ajaxType = 'PUT';
             var url = resourceUrl + '/' + $(e.target.Id).val();
+            var feedbackfn = setEditFeedback; // cf. adac-refdata-feedback.js
         }
-        var successRedirect = action.toLowerCase() + "-success-redirect";
 
         // Make AJAX Call
         $.ajax({
@@ -80,8 +81,8 @@ function AdacDiseaseStatusViewModel() {
                 _this.dialogErrors.removeAll();
                 if (data.success) {
                     _this.hideModal();
-                    window.location.href =
-                        form.data(successRedirect) + "?Name=" + data.name;
+                    feedbackfn(data.name,
+                        form.data("success-redirect"), form.data("refdata-type"));
                 }
                 else {
                     if (Array.isArray(data.errors)) {
@@ -166,14 +167,36 @@ $(function () {
   });
 
   $(".delete-confirm").click(function (e) {
-    e.preventDefault();
-    var $link = $(this);
-    bootbox.confirm(
-      "Are you sure you want to delete " + $link.data("disease-status") + "?",
-      function (confirmation) {
-        confirmation && window.location.assign($link.attr("href"));
-      }
-    );
+      e.preventDefault();
+
+      var $link = $(this);
+      var linkData = $link.data("refdata-model")
+      var url = $link.data("resource-url") + "/" + linkData.Id;
+
+      bootbox.confirm("Are you sure you want to delete " + linkData.Description + "?",
+          function (confirmation) {
+              if (confirmation) {
+                  // Make AJAX Call
+                  $.ajax({
+                      url: url,
+                      type: 'DELETE',
+                      success: function (data, textStatus, xhr) {
+                          if (data.success) {
+                              setDeleteFeedback(data.name,
+                                  $link.data("success-redirect"), $link.data("refdata-type"))
+                          }
+                          else {
+                              if (Array.isArray(data.errors)) {
+                                  if (data.errors.length > 0) {
+                                      window.feedbackMessage(data.errors[0], "warning");
+                                  }
+                              }
+                          }
+                      }
+                  });
+              }
+          }
+      );
   });
 
   adacDiseaseStatusVM = new AdacDiseaseStatusViewModel();
