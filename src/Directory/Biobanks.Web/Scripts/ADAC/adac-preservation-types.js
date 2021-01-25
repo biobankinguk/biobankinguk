e9c722b7f250bd9877e9b3b3ba8c5aacc7bb2647
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-01-25 16:51:15 +0000

    update preservation types

--- src/Directory/Biobanks.Web/Scripts/ADAC/adac-preservation-types.js
+++ src/Directory/Biobanks.Web/Scripts/ADAC/adac-preservation-types.js
@@ -66,11 +66,12 @@ function AdacPreservationTypeViewModel() {
         if (action == 'Add') {
             var ajaxType = 'POST'
             var url = resourceUrl;
+            var feedbackfn = setAddFeedback; // cf. adac-refdata-feedback.js
         } else if (action == 'Update') {
             var ajaxType = 'PUT';
             var url = resourceUrl + '/' + $(e.target.Id).val();
+            var feedbackfn = setEditFeedback; // cf. adac-refdata-feedback.js
         }
-        var successRedirect = action.toLowerCase() + "-success-redirect";
 
         // Make AJAX Call
         $.ajax({
@@ -82,8 +83,8 @@ function AdacPreservationTypeViewModel() {
                 _this.dialogErrors.removeAll();
                 if (data.success) {
                     _this.hideModal();
-                    window.location.href =
-                        form.data(successRedirect) + "?Name=" + data.name;
+                    feedbackfn(data.name,
+                        form.data("success-redirect"), form.data("refdata-type"));
                 }
                 else {
                     if (Array.isArray(data.errors)) {
@@ -106,10 +107,31 @@ $(function () {
         e.preventDefault();
 
         var $link = $(this);
+        var linkData = $link.data("refdata-model")
+        var url = $link.data("resource-url") + "/" + linkData.Id;
 
-        bootbox.confirm("Are you sure you want to delete " + $link .data("preservation-type") + "?",
+        bootbox.confirm("Are you sure you want to delete " + linkData.Description + "?",
             function (confirmation) {
-                confirmation && window.location.assign($link.attr("href"));
+                if (confirmation) {
+                    // Make AJAX Call
+                    $.ajax({
+                        url: url,
+                        type: 'DELETE',
+                        success: function (data, textStatus, xhr) {
+                            if (data.success) {
+                                setDeleteFeedback(data.name,
+                                    $link.data("success-redirect"), $link.data("refdata-type"))
+                            }
+                            else {
+                                if (Array.isArray(data.errors)) {
+                                    if (data.errors.length > 0) {
+                                        window.feedbackMessage(data.errors[0], "warning");
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
             }
         );
     });
