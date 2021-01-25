8756cc2a79aa32b466fabaf10283f6ea8c8e9d12
Joseph Lavagna <josephlavagna1998@gmail.com>
2021-01-25 10:56:02 +0000

    Added migration for source column

---
+++ src/Directory/Data/Migrations/202101251055146_AddSourceColumnToPublications.cs
@@ -0,0 +1,18 @@
+﻿namespace Directory.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddSourceColumnToPublications : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Publications", "Source", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Publications", "Source");
+        }
+    }
+}
