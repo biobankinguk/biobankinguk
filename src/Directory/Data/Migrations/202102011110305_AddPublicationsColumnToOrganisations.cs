6245895920375541ec08ea5238a9d1f15610129a
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-02-01 18:53:07 +0000

    Add migrations to add new column

---
+++ src/Directory/Data/Migrations/202102011110305_AddPublicationsColumnToOrganisations.cs
@@ -0,0 +1,18 @@
+﻿namespace Directory.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddPublicationsColumnToOrganisations : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Organisations", "IncludePublications", c => c.Boolean());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Organisations", "IncludePublications");
+        }
+    }
+}
