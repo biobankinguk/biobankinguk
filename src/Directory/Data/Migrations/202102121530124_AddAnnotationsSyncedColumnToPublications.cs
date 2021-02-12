51b795d448ce6cd942bdbc97cd1d586259546fd1
Joseph Lavagna <josephlavagna1998@gmail.com>
2021-02-12 15:31:11 +0000

    Added AnnotationSynced column to Publications

---
+++ src/Directory/Data/Migrations/202102121530124_AddAnnotationsSyncedColumnToPublications.cs
@@ -0,0 +1,18 @@
+﻿namespace Biobanks.Directory.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAnnotationsSyncedColumnToPublications : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Publications", "AnnotationsSynced", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Publications", "AnnotationsSynced");
+        }
+    }
+}
