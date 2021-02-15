334b3f9e744286e2da255fab80729e8a73a7a652
Joseph Lavagna <josephlavagna1998@gmail.com>
2021-02-15 15:51:21 +0000

    Merge typo

--- src/Publications/Publications/Services/Contracts/IEPMCService.cs
+++ src/Publications/Publications/Services/Contracts/IEPMCService.cs
@@ -15,6 +15,5 @@ namespace Publications.Services.Contracts
 
         //Annotations
         Task<List<AnnotationDTO>> GetPublicationAnnotations(string publicationId, string source);
-        Task<List<AnnotationDTO>> GetPublicationAnnotations(string publicationId, string source);
     }
 }
--- src/Publications/PublicationsAzureFunctions/Testing.cs
+++ src/Publications/PublicationsAzureFunctions/Testing.cs
@@ -36,9 +36,9 @@ namespace PublicationsAzureFunctions
             ILogger log)
         {
             //Pulls Publications from directory (test), gets annotations from API and pushes to Azure DB
-            await _fetchAnnotationsService.StartAsync(cancellationToken);
-            await _fetchPublicationsService.StopAsync(cancellationToken);
-            //var test = await _recommendationsService.CalculateRecommendation("27658825", "MED");
+            //await _fetchAnnotationsService.StartAsync(cancellationToken);
+            //await _fetchAnnotationsService.StopAsync(cancellationToken);
+            var test = await _recommendationsService.CalculateRecommendation("27658825", "MED");
             return new OkObjectResult("Done");
         }
     }
