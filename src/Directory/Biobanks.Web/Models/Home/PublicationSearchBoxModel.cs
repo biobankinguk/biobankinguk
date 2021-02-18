4440bf7cfbafbc059d4f346be2abcea1fdf63f68
Joseph Lavagna <josephlavagna1998@gmail.com>
2021-02-18 11:18:30 +0000

    PublicationSearch Model for config

---
+++ src/Directory/Biobanks.Web/Models/Home/PublicationSearchBoxModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Biobanks.Web.Models.Home
+{
+    public class PublicationSearchBoxModel
+    {
+        public string SearchTitle { get; set; }
+
+        public string SearchSubTitle { get; set; }
+
+        public string SearchRelatedPublications { get; set; }
+
+        public string SearchRelatedBiobanks { get; set; }
+
+    }
+}
