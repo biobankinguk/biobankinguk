722a5fa4bb3f8e3b1eedfb62c05c9a43cf1eb0b7
bradthompson <38660186+bradthompson995@users.noreply.github.com>
2021-02-18 17:01:23 +0000

    Update facet-groups.js

--- src/Directory/Biobanks.Web/Scripts/Search/facet-groups.js
+++ src/Directory/Biobanks.Web/Scripts/Search/facet-groups.js
@@ -1,21 +1,19 @@
-﻿$('.facet-group').on('hidden.bs.collapse', function (a) {
+﻿$('.facet-group').on('hidden.bs.collapse', function () {
     $('#' + $(this).data('icon-id')).removeClass('fa-plus-square');
     $('#' + $(this).data('icon-id')).removeClass('fa-minus-square');
 
     $('#' + $(this).data('icon-id')).addClass('fa-plus-square');
 
     $.cookie($(this).attr('id') + 'Collapsed', true);
-    a.stopPropagation()
 });
 
-$('.facet-group').on('shown.bs.collapse', function(a) {
+$('.facet-group').on('shown.bs.collapse', function() {
     $('#' + $(this).data('icon-id')).removeClass('fa-plus-square');
     $('#' + $(this).data('icon-id')).removeClass('fa-minus-square');
 
     $('#' + $(this).data('icon-id')).addClass('fa-minus-square');
 
     $.cookie($(this).attr('id') + 'Collapsed', false);
-    a.stopPropagation()
 });
 
 
