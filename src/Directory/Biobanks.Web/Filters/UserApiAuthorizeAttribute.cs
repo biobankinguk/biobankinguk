1ee9ba8d34ff97e11c8d1b653954905eb0f813a8
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-02-18 15:34:10 +0000

    override http authorize attribute to retrun 403

---
+++ src/Directory/Biobanks.Web/Filters/UserApiAuthorizeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http.Controllers;
+using System.Net.Http;
+using System.Net;
+
+namespace Biobanks.Web.Filters
+{
+    public class UserApiAuthorizeAttribute: System.Web.Http.AuthorizeAttribute
+    {
+        //override the HTTP Unauthorized handler to return a 403 Forbidden, instead of 401 Unauthorized
+        protected override void HandleUnauthorizedRequest(HttpActionContext filterContext)
+        {
+            filterContext.Response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+        }
+    }
+}
