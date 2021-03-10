2c7d035da07bad9c95fb57c34a73403d0290d0f2
James Fleming <james.fleming@nottingham.ac.uk>
2021-03-10 09:55:11 +0000

    Creation of ExtractionProcedure entity

---
+++ src/Entities/Shared/ReferenceData/ExtractionProcedure.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biobanks.Entities.Shared.ReferenceData
+{
+    public class ExtractionProcedure
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+
+        public int SortOrder { get; set; }
+
+        public int MaterialTypeId { get; set; }
+
+        public virtual MaterialType MaterialType { get; set; }
+    }
+}
