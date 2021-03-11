885f08a9e0c325185f237cca05f1572a41a7f23b
Jonathan Couldridge <jcouldridge@outlook.com>
2021-03-11 17:18:58 +0000

    Added Azure Functions worker project

---
+++ src/Submissions/AzFunctions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
