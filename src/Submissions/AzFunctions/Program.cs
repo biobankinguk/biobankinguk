438c367ba129e170104e32a5224070fd11ed666a
Jonathan Couldridge <jcouldridge@outlook.com>
2021-03-11 17:32:45 +0000

    Azure Functions Isolated initial app bootstrap

--- src/Submissions/AzFunctions/Program.cs
+++ src/Submissions/AzFunctions/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using AzFunctions;
 
-namespace AzFunctions
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using System.Reflection;
+
+// Configure the Host
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureFunctionsWorkerDefaults()
+
+    .ConfigureAppConfiguration(b =>
+        b.AddUserSecrets(Assembly.GetAssembly(typeof(Startup))))
+
+    // Configure dependencies, like ASP.NET Core
+    .ConfigureServices(Startup.ConfigureServices);
+
+// Run the app
+await host.Build().RunAsync();
---
+++ src/Submissions/AzFunctions/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using System;
+
+namespace AzFunctions
+{
+    class Startup
+    {
+        internal static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
