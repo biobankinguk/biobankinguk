7e5c540facd0e746c27f1d3e12bba863f33a0728
Jonathan Couldridge <jcouldridge@outlook.com>
2021-03-11 15:21:05 +0000

    Combined IQueueWriteService in Core

--- src/Submissions/Core/Services/Contracts/IQueueWriteService.cs
+++ src/Submissions/Core/Services/Contracts/IQueueWriteService.cs
@@ -2,8 +2,25 @@
 
 namespace Biobanks.Submissions.Core.Services.Contracts
 {
+    /// <summary>
+    /// Service for writing messages to storage queues.
+    /// </summary>
     public interface IQueueWriteService
     {
+        /// <summary>
+        /// Pushes a new message to a given storage queue.
+        /// </summary>
+        /// <param name="queue">ID of the storage queue to push the message to.</param>
+        /// <param name="message">Message to push to the given storage queue.</param>
+        /// <returns></returns>
+        Task PushAsync(string queue, string message);
+    
+        /// <summary>
+        /// Delete a message from a storage queue
+        /// </summary>
+        /// <param name="queue">ID of the storage queue containing the message.</param>
+        /// <param name="messageId">ID of the message to delete.</param>
+        /// <returns></returns>
         Task DeleteAsync(string queue, string messageId);
     }
 }
