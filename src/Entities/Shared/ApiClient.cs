574d036de5ca4164ffaf1e707ab5da21b20b1c6a
Jonathan Couldridge <jcouldridge@outlook.com>
2021-03-15 16:08:18 +0000

    AB#32212: ✨ ApiClient Entity

---
+++ src/Entities/Shared/ApiClient.cs
@@ -0,0 +1,11 @@
+﻿namespace Biobanks.Entities.Shared
+{
+    public class ApiClient
+    {
+        public int Id { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecretHash { get; set; }
+    }
+}
