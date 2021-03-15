750fd4d96a4d94077415e20a5de6d5b15c6d475a
Jonathan Couldridge <jcouldridge@outlook.com>
2021-03-15 14:55:06 +0000

    simplified Functions program

--- src/Submissions/AzFunctions/Program.cs
+++ src/Submissions/AzFunctions/Program.cs
@@ -1,18 +1,9 @@
 ﻿using AzFunctions;
-
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-
-using System.Reflection;
 
 // Configure the Host
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureFunctionsWorkerDefaults()
-
-    .ConfigureAppConfiguration(b =>
-        b.AddUserSecrets(Assembly.GetAssembly(typeof(Startup))))
-
-    // Configure dependencies, like ASP.NET Core
     .ConfigureServices(Startup.ConfigureServices);
 
 // Run the app
