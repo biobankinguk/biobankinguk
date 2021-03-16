37570dcba75d74023c832dfe2d7837dfe23e1a74
Jonathan Couldridge <jcouldridge@outlook.com>
2021-03-16 08:44:34 +0000

    AB#32213: ⚡ Many-to-Many  ApiClient - Organisation

--- src/Entities/Shared/ApiClient.cs
+++ src/Entities/Shared/ApiClient.cs
@@ -1,11 +1,19 @@
-﻿namespace Biobanks.Entities.Shared
+﻿using Biobanks.Entities.Data;
+
+using System.Collections.Generic;
+
+namespace Biobanks.Entities.Shared
 {
     public class ApiClient
     {
         public int Id { get; set; }
 
+        public string Name { get; set; }
+
         public string ClientId { get; set; }
 
         public string ClientSecretHash { get; set; }
+
+        public virtual ICollection<Organisation> Organisations { get; set; }
     }
 }
