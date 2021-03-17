b5c8c753822a0463c635c3a0bcf2f74fa64562f7
Jonathan Couldridge <jcouldridge@outlook.com>
2021-03-17 16:52:35 +0000

    initial command line app config

---
+++ src/IdentityModel/IdentityTool/Program.cs
@@ -0,0 +1,24 @@
+﻿using IdentityTool;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using Serilog;
+
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.Parsing;
+using System.Reflection;
+
+var cmd = new CommandLineBuilder(new MyRootCommand())
+    .UseDefaults()
+    .UseHost(hostArgs =>
+        Host.CreateDefaultBuilder(hostArgs)
+            .ConfigureAppConfiguration(b =>
+                b.AddUserSecrets(Assembly.GetAssembly(typeof(MyRootCommand))))
+            .UseSerilog((context, services, loggerConfig) => loggerConfig
+                .ReadFrom.Configuration(context.Configuration)
+                .Enrich.FromLogContext()))
+    .Build();
+
+await cmd.InvokeAsync(args);
