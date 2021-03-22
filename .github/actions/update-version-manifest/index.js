7742fbec984b0c91df09a79ff7fc3a60c11f8211
Jonathan Couldridge <jcouldridge@outlook.com>
2021-03-22 13:49:26 +0000

    AB#32890: Action for version manifest updates

---
+++ .github/actions/update-version-manifest/index.js
@@ -0,0 +1,71 @@
+const core = require("@actions/core");
+const fs = require("fs");
+const yaml = require("js-yaml");
+const semver = require("semver");
+
+/** path to the manifest YAML file */
+const manifestPath = core.getInput("manifest-path");
+
+/** fixed (non semver) release types */
+const releaseTypes = {
+  NONE: "none",
+  FORCE: "force",
+};
+
+/**
+ * bump the manifest by reference based on release type
+ * @param {*} manifest The manifest object
+ */
+const manifestBump = (manifest) => {
+  const [release, label] = manifest.release.split(" ");
+  manifest.version = semver.inc(manifest.version, release, label);
+};
+
+/**
+ * reset the working manifest and save changes to disk
+ * @param {*} manifest The manifest object
+ */
+const persistChanges = (manifest) => {
+  // reset the manifest release type
+  manifest.release = releaseTypes.NONE;
+  fs.writeFileSync(manifestPath, yaml.dump(manifest));
+};
+
+try {
+  // try parse version.yml
+  const manifest = yaml.load(fs.readFileSync(manifestPath));
+  console.log(`Manifest:`, manifest);
+
+  // check version is valid semver
+  if (
+    !semver.valid(manifest.version, {
+      includePrerelease: true,
+    })
+  )
+    throw Error("Manifest Version is not a valid SemVer");
+
+  // record whether the manifest triggers a release
+  const doRelease = manifest.release !== releaseTypes.NONE;
+  core.setOutput("doRelease", doRelease);
+  console.log("doRelease:", doRelease);
+
+  // version bump manifest accordingly for release type
+  switch (manifest.release) {
+    case releaseTypes.NONE:
+    case releaseTypes.FORCE:
+      break;
+    default:
+      manifestBump(manifest);
+  }
+
+  // save the updated manifest, if any release
+  if (doRelease) {
+    persistChanges(manifest);
+  }
+
+  // record the (possibly updated) version
+  core.setOutput("version", manifest.version);
+  console.log("version:", manifest.version);
+} catch (e) {
+  core.setFailed(e.message);
+}
