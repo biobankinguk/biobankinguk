74a627b02d09da9c5b1c0a1145d964ac823b2b00
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2021-03-26 14:22:55 +0000

    Submissions model and view

---
+++ src/Directory/Biobanks.Web/Models/Biobank/SubmissionsModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Biobanks.Web.Models.Biobank
+{
+    public class SubmissionsModel
+    {
+        #region Form Collection
+        public IEnumerable<ReferenceDataModel> AccessConditions { get; set; }
+        public IEnumerable<ReferenceDataModel> CollectionTypes { get; set; }
+
+        #endregion
+
+        #region Properties
+
+        [Required(ErrorMessage = "Please select an access condition.")]
+        [Display(Name = "Access condition")]
+        public int AccessCondition { get; set; }
+
+        [Required(ErrorMessage = "Please select a collection Type.")]
+        [Display(Name = "Collection type")]
+        public int? CollectionType { get; set; }
+        #endregion
+    }
+}
