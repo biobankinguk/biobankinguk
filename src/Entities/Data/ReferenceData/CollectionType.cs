f1eaa77d064bf1e6c06a3112aedc6eb66264bc96
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-04-07 17:10:16 +0100

    update collection type entity for foreign key

--- src/Entities/Data/ReferenceData/CollectionType.cs
+++ src/Entities/Data/ReferenceData/CollectionType.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Biobanks.Entities.Data.ReferenceData
@@ -10,5 +11,7 @@ namespace Biobanks.Entities.Data.ReferenceData
         public string Value { get; set; }
 
         public int SortOrder { get; set; }
+
+        public virtual ICollection<Organisation> Organisations { get; set; }
     }
 }
