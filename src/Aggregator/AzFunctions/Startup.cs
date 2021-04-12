567547e9f90060d09b4f568d3d66fd5ee515e158
James Fleming <james.fleming@nottingham.ac.uk>
2021-04-12 10:28:11 +0100

    AB#29888: Create Aggregator Function App and Workflow (#182)
    * Creation of Aggregator Core lib and Azure Function
    * Added Build Workflow for Github Actions
    * Added Aggregator to labeler workflow

---
+++ src/Aggregator/AzFunctions/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Biobanks.Aggregator.AzFunctions
+{
+    class Startup
+    {
+        internal static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+        }
+    }
+}
