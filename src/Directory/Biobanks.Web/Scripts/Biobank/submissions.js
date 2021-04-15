c67ee378e4978bc3a8fbaa43402b12cbfd06fff2
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-04-15 17:59:10 +0100

    add submissions js

---
+++ src/Directory/Biobanks.Web/Scripts/Biobank/submissions.js
@@ -0,0 +1,21 @@
+﻿function copyText(fieldId) {
+    /* Get the text field */
+    var source = document.getElementById(fieldId);
+
+    /* Select the text field */
+    source.select();
+    source.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+}
+
+//wire up bootbox confirmation
+$(".copy-text").click(function (e) {
+    e.preventDefault();
+    var $btn = $(this);
+    var txtbox = $btn.data("target")
+    copyText(txtbox);
+    /* Alert the copied text */
+    alert("Copied the text: " + source.value);
+});
