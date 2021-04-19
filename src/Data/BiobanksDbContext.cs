1b77ae9c5b831def385db2a965adf7d32ee806eb
James Fleming <james.fleming@nottingham.ac.uk>
2021-04-19 15:41:48 +0100

    Update BiobanksDbContext.cs

--- src/Data/BiobanksDbContext.cs
+++ src/Data/BiobanksDbContext.cs
@@ -239,8 +239,8 @@ namespace Biobanks.Data
                     x.LowerBound,
                     x.UpperBound
                 }).IsUnique();
-        
-        
+
+
         }
 
         public BiobanksDbContext(DbContextOptions options) : base(options) { }
