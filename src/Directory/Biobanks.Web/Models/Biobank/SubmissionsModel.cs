4cecc0f9c7804d6d522a9a764e78250dcfc8db6e
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-04-19 12:58:15 +0100

    remove private key from submission model

--- src/Directory/Biobanks.Web/Models/Biobank/SubmissionsModel.cs
+++ src/Directory/Biobanks.Web/Models/Biobank/SubmissionsModel.cs
@@ -28,13 +28,8 @@ namespace Biobanks.Web.Models.Biobank
         [Display(Name = "Collection type")]
         public int? CollectionType { get; set; }
 
-        
-        public bool GenerateKey { get; set; }
-
         [Display(Name="Public Key")]
         public string PublicKey { get; set; }
-        [Display(Name="Private Key")]
-        public string PrivateKey { get; set; }
 
         #endregion
     }
