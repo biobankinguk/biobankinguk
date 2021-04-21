cb4caa033ef257e7683bbea5bd10becb6140a1f3
Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
2021-04-21 10:13:55 +0100

    Basic class for function

---
+++ src/Submissions/AzFunctions/Functions/SubmissionLiveFunction.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using System.Threading.Tasks;
+
+namespace AzFunctions.Functions
+{
+    class SubmissionLiveFunction
+    {
+
+        [Function("Submissions_Staging")]
+        public async Task Run()
+        {
+
+        }
+    }
+}
