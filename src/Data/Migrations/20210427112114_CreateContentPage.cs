d42cb150b96a2a4dd273c65db5334feea213525d
Thomas Read <thomasread99@gmail.com>
2021-04-27 12:22:47 +0100

    created the content page entity and dbset

---
+++ src/Data/Migrations/20210427112114_CreateContentPage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Biobanks.Data.Migrations
+{
+    public partial class CreateContentPage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContentPages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RouteSlug = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsEnabled = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContentPages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContentPages");
+        }
+    }
+}
---
+++ src/Entities/Data/ContentPage.cs
@@ -0,0 +1,20 @@
+﻿using Biobanks.Entities.Shared.ReferenceData;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Biobanks.Entities.Data.ReferenceData;
+using System;
+
+namespace Biobanks.Entities.Data
+{
+    public class ContentPage
+    {
+        [Key]
+        public int Id { get; set; }
+        public string RouteSlug { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public bool IsEnabled { get; set; }
+
+    }
+}
