d84606dad1fbdb6c08e523a965b1c6f5814002cb
James Fleming <james.fleming@nottingham.ac.uk>
2021-04-27 11:15:34 +0100

    Added ReferenceDataService for all ref data service methods

---
+++ src/Aggregator/Core/Services/ReferenceDataService.cs
@@ -0,0 +1,53 @@
+﻿using Biobanks.Data;
+using Biobanks.Entities.Data.ReferenceData;
+using System.Linq;
+using System.Xml;
+
+namespace Biobanks.Aggregator.Core.Services
+{
+    public class ReferenceDataService
+    {
+        private readonly BiobanksDbContext _db;
+
+        public ReferenceDataService(BiobanksDbContext db)
+        {
+            _db = db;
+        }
+
+        public AgeRange GetAgeRange(string age)
+            => _db.AgeRanges.ToList().FirstOrDefault(y =>
+                    XmlConvert.ToTimeSpan(y.LowerBound) <= XmlConvert.ToTimeSpan(age) &&
+                    XmlConvert.ToTimeSpan(y.UpperBound) >= XmlConvert.ToTimeSpan(age)) ?? GetDefaultAgeRange();
+
+        public CollectionPercentage GetCollectionPercentage(decimal percentage)
+            => _db.CollectionPercentages.FirstOrDefault(y =>
+                    y.LowerBound <= percentage &&
+                    y.UpperBound >= percentage);
+
+        public CollectionStatus GetCollectionStatus(bool complete)
+        {
+            return complete
+                ? _db.CollectionStatus.Where(x => x.Value == "Completed").First()
+                : _db.CollectionStatus.Where(x => x.Value == "In progress").First();
+        }
+
+        public DonorCount GetDonorCount(int count)
+            => _db.DonorCounts.First(x => x.LowerBound <= count && x.UpperBound >= count);
+
+        public AccessCondition GetDefaultAccessCondition()
+            => _db.AccessConditions.OrderBy(x => x.SortOrder).First();
+
+        public AgeRange GetDefaultAgeRange()
+            => _db.AgeRanges.First(x => x.LowerBound == null && x.UpperBound == null);
+
+        public HtaStatus GetDefaultHtaStatus()
+            => _db.HtaStatus.OrderBy(x => x.SortOrder).First();
+
+        public CollectionPoint GetDefaultCollectionPoint()
+            => _db.CollectionPoints.OrderBy(x => x.SortOrder).First();
+
+        public CollectionType GetDefaultCollectionType()
+            => _db.CollectionTypes.OrderBy(x => x.SortOrder).First();
+
+    }
+}
