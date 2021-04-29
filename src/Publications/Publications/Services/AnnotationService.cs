1357292678c50dbdaef249ba6d425cd9e5dc1afa
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-04-29 17:49:39 +0100

    update annotation service to use new dbcontext

--- src/Publications/Publications/Services/AnnotationService.cs
+++ src/Publications/Publications/Services/AnnotationService.cs
@@ -6,50 +6,59 @@ using Biobanks.Publications.Services.Contracts;
 using Biobanks.Publications.Services.Dto;
 using Biobanks.Entities.Data.ReferenceData;
 using Biobanks.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Biobanks.Publications.Services
 {
     public class AnnotationService : IAnnotationService
     {
         private BiobanksDbContext _ctx;
-        private IBiobankReadService _biobankReadService;
 
-        public AnnotationService(BiobanksDbContext ctx, IBiobankReadService biobankReadService)
+        public AnnotationService(BiobanksDbContext ctx)
         {
             _ctx = ctx;
-            _biobankReadService = biobankReadService;
         }
 
 
         public async Task AddPublicationAnnotations(string publicationId, IEnumerable<AnnotationDTO> annotations)
         {
+            var publication = await _ctx.Publications.Include(o => o.Annotations).FirstOrDefaultAsync(x => x.PublicationId == publicationId);
 
-            var publication = await _biobankReadService.GetPublicationById(publicationId);
-
+            var annotationList = new List<Annotation>();
             foreach (var annotation in annotations)
             {
                 foreach (var tags in annotation.Tags)
                 {
-                    //If annotation doesn't already exist (new annotation)
-                    if (!_ctx.Annotations.Any(x => x.Name == tags.Name.ToLower()))
+                    var annotationEntity = new Annotation()
                     {
-                        //add new annotation and link to publication
-                        publication.Annotations.Add(new Annotation { Name = tags.Name.ToLower() });
-                    }
-                    else
-                    {
-                        //link annotation to publication
-                        var oldAnnotation = _ctx.Annotations.First(x => x.Name == tags.Name.ToLower());
-                        if (publication.Annotations.Any(x=>x.Id == oldAnnotation.Id))
-                            publication.Annotations.Add(oldAnnotation);
-                    }
+                        Name = tags.Name.ToLower()
+                    };
+                    annotationList.Add(annotationEntity);
                 }
             }
-      
-            publication.AnnotationsSynced = DateTime.Now;
-            _ctx.Update(publication);
 
-           await _ctx.SaveChangesAsync();
+            //Remove duplicate Annotation Names
+            var annList = annotationList.GroupBy(x => x.Name).Select(x => x.First()).ToHashSet();
+            
+            foreach (var annotation in annList)
+            {
+                //If annotation doesn't already exist (new annotation)
+                if (_ctx.Annotations.Any(x => x.Name == annotation.Name))
+                {
+                    //link annotation to publication
+                    var oldAnnotation = _ctx.Annotations.First(x => x.Name == annotation.Name);
+                    if (!publication.Annotations.Any(x => x.Id == oldAnnotation.Id))
+                        publication.Annotations.Add(oldAnnotation);
+                }
+                else
+                {
+                    //add new annotation and link to publication
+                    publication.Annotations.Add(annotation);
+                }
+            }
+
+            publication.AnnotationsSynced = DateTime.Now;
+            await _ctx.SaveChangesAsync();
         }
     }
 }
