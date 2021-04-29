be7ee65d0810c4cf69ee1ca81c6470c0946a9016
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-04-29 17:52:10 +0100

    add timer type

---
+++ src/Publications/PublicationsAzFunctions/Types/ScheduleStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicationsAzFunctions.Types
+{
+    public class ScheduleStatus
+    {
+        public DateTime Last { get; set; }
+
+        public DateTime Next { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
