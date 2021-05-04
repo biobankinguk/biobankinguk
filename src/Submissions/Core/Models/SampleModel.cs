0aae6dfbde14878fc6a580e1e44413dc13c164c3
James Fleming <james.fleming@nottingham.ac.uk>
2021-05-04 16:34:40 +0100

    Added missing field to SampleModel - the missing field was causing a validation error

--- src/Submissions/Core/Models/SampleModel.cs
+++ src/Submissions/Core/Models/SampleModel.cs
@@ -57,11 +57,13 @@ namespace Biobanks.Submissions.Core.Models
 
         #region Extracted Sample Properties
 
-        public string ExtractionProcedure { get; set; }
-
         public string SampleContent { get; set; }
 
         public string SampleContentMethod { get; set; }
+
+        public OntologyField SampleContentOntologyField { get; set; } = OntologyField.Code;
+
+        public string ExtractionProcedure { get; set; }
 
         public OntologyField ExtractionProcedureOntologyField { get; set; } = OntologyField.Code;
 
