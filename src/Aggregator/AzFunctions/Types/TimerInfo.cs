52583c975f8a32a948772d7585f5fc2ae5c30678
Jonathan Couldridge <jcouldridge@outlook.com>
2021-05-06 14:48:49 +0100

    aggregator - removed local timer types

--- src/Aggregator/AzFunctions/Types/TimerInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Biobanks.Aggregator.AzFunctions.Types
-{
-    public class TimerInfo
-    {
-        public ScheduleStatus ScheduleStatus { get; set; }
-
-        public bool IsPastDue { get; set; }
-    }
-}
