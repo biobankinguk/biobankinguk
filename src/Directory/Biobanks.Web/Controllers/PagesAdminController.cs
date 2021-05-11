bfd6d469f91cc2ca07eb6658b9f3d7430902b335
Thomas Read <thomasread99@gmail.com>
2021-05-11 15:58:36 +0100

    created the model and index in the controller

---
+++ src/Directory/Biobanks.Web/Controllers/PagesAdminController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Biobanks.Services.Contracts;
+using Biobanks.Web.Filters;
+using Biobanks.Web.Models.ADAC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Biobanks.Web.Controllers
+{
+    [UserAuthorize(Roles = "ADAC")]
+    public class PagesAdminController : ApplicationBaseController
+    {
+        private readonly IContentPageService _contentPageService;
+
+        private readonly IMapper _mapper;
+
+        public PagesAdminController(IContentPageService contentPageService, IMapper mapper)
+        {
+            _contentPageService = contentPageService;
+            _mapper = mapper;
+        }
+
+        public ActionResult Index()
+        {
+            // List all ContentPages
+            var contentPage = _contentPageService.ListContentPages();
+
+            if (contentPage != null)
+            {
+                // Map to ViewModel
+                var model = _mapper.Map<ViewModel>(contentPage);
+                // Return View
+                return View(model);
+            }
+            else
+                return HttpNotFound();
+        }
+    }
+}
---
+++ src/Directory/Biobanks.Web/Models/ADAC/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Biobanks.Web.Models.ADAC
+{
+    public class ViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
