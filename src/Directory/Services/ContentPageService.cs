be2c37bbd65367badf2f67c1032efa16448c4591
Thomas Read <thomasread99@gmail.com>
2021-05-11 10:29:58 +0100

    stylistic and PR changes

--- src/Directory/Services/ContentPageService.cs
+++ src/Directory/Services/ContentPageService.cs
@@ -24,11 +24,13 @@ namespace Biobanks.Services
 
         public async Task Create(string title, string body, string slug)
         {
-            var page = new ContentPage();
-            page.Title = title;
-            page.Body = body;
-            page.RouteSlug = slug;
-            page.LastUpdated = DateTime.UtcNow;
+            var page = new ContentPage
+            {
+                Title = title,
+                Body = body,
+                RouteSlug = slug,
+                LastUpdated = DateTime.UtcNow
+            };            
 
             _db.ContentPages.Add(page);
             await _db.SaveChangesAsync();
@@ -47,45 +49,34 @@ namespace Biobanks.Services
                 page.Body = body;
                 page.RouteSlug = slug;
                 page.LastUpdated = DateTime.UtcNow;
-                await _db.SaveChangesAsync();
-                return;
-            }
-
-            //_db.ContentPages.Attach(contentPage);
-            //_db.Entry(contentPage).State = EntityState.Modified;            
+                await _db.SaveChangesAsync();                
+            }                        
         }
 
         public async Task Delete(int id)
         {
             var page = await _db.ContentPages.FindAsync(id);
-            if (page == null)
-            {
-                return;
-            }
-            else
+            if (page != null)
             {
                 _db.ContentPages.Remove(page);
                 await _db.SaveChangesAsync();
-                return;
-            }            
+            }                                   
         }
 
         public IEnumerable<ContentPage> ListContentPages()
         {
             IQueryable<ContentPage> query = _db.ContentPages;
-
-            return query.ToList();                        
+            return query.ToList();
         }
-
+     
         public async Task<ContentPage> GetById(int id)
-        {
-            return await _db.ContentPages.FindAsync(id);
-        }
+            => await _db.ContentPages.FindAsync(id);
+          
+        
 
         public async Task<ContentPage> GetBySlug(string routeSlug)
-        {
-            return await _db.ContentPages.FirstOrDefaultAsync(x => x.RouteSlug == routeSlug);
-        }
+            => await _db.ContentPages.FirstOrDefaultAsync(x => x.RouteSlug == routeSlug);
+        
 
     }
 }
