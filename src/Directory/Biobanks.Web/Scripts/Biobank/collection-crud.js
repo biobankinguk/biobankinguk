709f03278675b5f2727ea2c03010ac1306ec4605
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-05-19 17:38:14 +0100

    add js file to disable inputs

---
+++ src/Directory/Biobanks.Web/Scripts/Biobank/collection-crud.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    var fromapi = $("#FromApi").val().toLowerCase();
+    
+    if (fromapi == "true") {
+        // set text inputs except description to read only
+        $('input[type=text], input[type=number], textarea:not(#Description)').attr("readonly", true);
+        //disable unselected radio buttons
+        $(':radio:not(:checked)').attr('disabled', true);
+        //disable checkboses
+        $(':checkbox').attr('disabled', true);
+    }
+});
