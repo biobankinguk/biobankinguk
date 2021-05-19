9a243eafcb0e300adbf90de1b5b0463fb2a9fe0d
Jonathan Couldridge <jcouldridge@outlook.com>
2021-05-19 10:43:01 +0100

    warnings

--- src/Submissions/Api/Config/WorkersOptions.cs
+++ src/Submissions/Api/Config/WorkersOptions.cs
@@ -8,9 +8,24 @@ namespace Biobanks.Submissions.Api.Config
     /// </summary>
     public static class WorkersRecurringJobs
     {
+        /// <summary>
+        /// The Google Analytics data fetcher
+        /// </summary>
         public const string Analytics = "analytics";
+
+        /// <summary>
+        /// The EPMC Publications data fetcher
+        /// </summary>
         public const string Publications = "publications";
+
+        /// <summary>
+        /// The Submissions -> Directory Aggregator
+        /// </summary>
         public const string Aggregator = "aggregator";
+
+        /// <summary>
+        /// The expired Submissions clean up
+        /// </summary>
         public const string SubmissionsExpiry = "submissions-expiry";
     }
 
@@ -19,7 +34,15 @@ namespace Biobanks.Submissions.Api.Config
     /// </summary>
     public enum WorkersQueueService
     {
+        /// <summary>
+        /// Queued Worker Jobs (e.g. Submissions Staging) will be queued via Azure Queue Storage,
+        /// which should in turn trigger some external worker run (e.g. Functions, WebJobs...)
+        /// </summary>
         AzureQueueStorage,
+
+        /// <summary>
+        /// Queued Worker Jobs will be queued and triggered by Hangfire and run by this API process
+        /// </summary>
         Hangfire
     }
 
@@ -30,8 +53,14 @@ namespace Biobanks.Submissions.Api.Config
     /// </summary>
     public class WorkersOptions
     {
+        /// <summary>
+        /// Which jobs as per <see cref="WorkersRecurringJobs" /> should be scheduled by this API's Hangfire server
+        /// </summary>
         public List<string> HangfireRecurringJobs { get; set; } = new();
 
+        /// <summary>
+        /// Which service to use for Queued Worker Jobs
+        /// </summary>
         public WorkersQueueService QueueService { get; set; } = WorkersQueueService.Hangfire;
     }
 }
