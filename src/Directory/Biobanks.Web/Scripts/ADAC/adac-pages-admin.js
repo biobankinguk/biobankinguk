ce19649767e9a8f63482a23b6ad6f899d1c3c747
Thomas Read <thomasread99@gmail.com>
2021-05-25 11:11:50 +0100

    removed redundant code and added to bootbox

--- src/Directory/Biobanks.Web/Scripts/ADAC/adac-pages-admin.js
+++ src/Directory/Biobanks.Web/Scripts/ADAC/adac-pages-admin.js
@@ -2,10 +2,14 @@
     $(".delete-confirm").click(function (e) {
         e.preventDefault();
 
-        bootbox.confirm("Are you sure you want to delete?",
+        var $link = $(this);
+        var linkData = $link.data("refdata-model")
+        var url = $link.data("resource-url") + "/" + linkData.Id;
+
+        bootbox.confirm("Are you sure you want to delete" + linkData.Description + "?",
             function (confirmation) {
                 if (confirmation) {
-
+                    deleteRefData(url, $link.data("success-redirect"), $link.data("refdata-type"));
                 }
             }
         );
