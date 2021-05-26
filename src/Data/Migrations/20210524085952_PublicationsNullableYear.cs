689e455541f46e048f0e09f7faa22d95d3191f0c
James Fleming <james.fleming@nottingham.ac.uk>
2021-05-26 15:55:48 +0100

    Made publication year nullable (#243)

---
+++ src/Data/Migrations/20210524085952_PublicationsNullableYear.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Biobanks.Data.Migrations
+{
+    public partial class PublicationsNullableYear : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Year",
+                table: "Publications",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Year",
+                table: "Publications",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
