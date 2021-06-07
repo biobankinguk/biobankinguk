f744669883d678f857f1090d0e8ed7d6d2eb5e45
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-06-07 17:22:49 +0100

    add publication seatch model

---
+++ src/Directory/Biobanks.Web/Models/Shared/PublicationSearchModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace Biobanks.Web.Models.Shared
+{
+    public class PublicationSearchModel
+    {
+        [JsonProperty("id")]
+        public string PublicationId { get; set; }
+
+        public string Title { get; set; }
+
+        [JsonProperty("authorString")]
+        public string Authors { get; set; }
+
+        [JsonProperty("journalTitle")]
+        public string Journal { get; set; }
+
+        [JsonProperty("pubYear")]
+        public int Year { get; set; }
+
+        public string DOI { get; set; }
+
+    }
+}
