5077f329881a24a27013b93018ed2e5d1ad3b453
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2021-06-15 16:26:18 +0100

    add extraction procedure javasxript

---
+++ src/Directory/Biobanks.Web/Scripts/ADAC/adac-extraction-procedure.js
@@ -0,0 +1,193 @@
+function DiseaseStatus(ontologyTermId, description, otherTerms) {
+    this.ontologyTermId = ko.observable(ontologyTermId);
+    this.description = ko.observable(description);
+    this.otherTerms = ko.observableArray(otherTerms);
+}
+
+function DiseaseStatusModal(ontologyTermId, description, otherTerms) {
+    this.modalModeAdd = "Add";
+    this.modalModeEdit = "Update";
+
+    this.mode = ko.observable(this.modalModeAdd);
+
+    this.diseaseStatus = ko.observable(
+        new DiseaseStatus(ontologyTermId, description, otherTerms)
+    );
+}
+
+function AdacDiseaseStatusViewModel() {
+
+    var _this = this;
+
+    this.modalId = "#disease-status-modal";
+    this.modal = new DiseaseStatusModal("", "", []);
+    this.dialogErrors = ko.observableArray([]);
+
+    this.showModal = function () {
+        _this.dialogErrors.removeAll(); //clear errors on a new show
+        $(_this.modalId).modal("show");
+    };
+
+    this.hideModal = function () {
+        $(_this.modalId).modal("hide");
+    };
+
+    this.openModalForAdd = function () {
+        $("#OntologyTermId").prop("readonly", false);
+
+        _this.modal.mode(_this.modal.modalModeAdd);
+        _this.modal.diseaseStatus(new DiseaseStatus("", "",[]));
+        _this.setPartialEdit(false);
+        _this.showModal();
+    };
+
+  this.openModalForEdit = function (_, event) {
+    _this.modal.mode(_this.modal.modalModeEdit);
+
+      var diseaseStatus = $(event.currentTarget).data("disease-status");
+      let otherTerms = (diseaseStatus.OtherTerms
+          ? diseaseStatus.OtherTerms.split(",").map(item => item.trim())
+          : diseaseStatus.OtherTerms)
+
+    _this.modal.diseaseStatus(
+      new DiseaseStatus(
+          diseaseStatus.OntologyTermId,
+          diseaseStatus.Description,
+          otherTerms
+
+      )
+    );
+
+    _this.setPartialEdit($(event.currentTarget).data("partial-edit"));
+    _this.showModal();
+  };
+
+    this.modalSubmit = function (e) {
+        e.preventDefault();
+        var form = $(e.target); // get form as a jquery object
+
+        //Concatenate other terms (exclude null/empty/whitespace strings)
+        $("#OtherTerms").val(_this.modal.diseaseStatus().otherTerms().filter(x => x && x.trim()).join(','));
+
+        // Get Action Type
+        var action = _this.modal.mode();
+        if (action == 'Add') {
+            addRefData(_this, form.data("resource-url"), form.serialize(),
+                form.data("success-redirect"), form.data("refdata-type")); // cf. adac-refdata-utility.js
+        } else if (action == 'Update') {
+            editRefData(_this, form.data("resource-url") + '/' + $(e.target.Id).val(), form.serialize(),
+                form.data("success-redirect"), form.data("refdata-type"));
+        }
+    };
+
+    //Turns on/off partial editing of some input fields
+    this.setPartialEdit = function (flag) {
+        if (flag == true) {
+            $("#OntologyTermId").prop('readonly', true);
+            $("#Description").prop('readonly', true);
+        }
+        else {
+            $("#OntologyTermId").prop('readonly', false);
+            $("#Description").prop('readonly', false);
+        }
+    }
+
+    this.addOtherTerms = function () {
+        _this.modal.diseaseStatus().otherTerms.push("");
+    }
+    this.removeOtherTerms = function (idx) {
+        _this.modal.diseaseStatus().otherTerms.splice(idx,1)
+    }
+}
+
+var adacDiseaseStatusVM;
+
+$(function () {
+    // jquery plugin to serialise checkboxes as bools
+    (function ($) {
+        $.fn.serialize = function () {
+        return $.param(this.serializeArray());
+        };
+
+        $.fn.serializeArray = function () {
+            var o = $.extend(
+            {
+                checkboxesAsBools: true,
+            },
+            {}
+            );
+
+            var rselectTextarea = /select|textarea/i;
+            var rinput = /text|hidden|password|search/i;
+
+            return this.map(function () {
+            return this.elements ? $.makeArray(this.elements) : this;
+            })
+            .filter(function () {
+                return (
+                this.name &&
+                !this.disabled &&
+                (this.checked ||
+                    (o.checkboxesAsBools && this.type === "checkbox") ||
+                    rselectTextarea.test(this.nodeName) ||
+                    rinput.test(this.type))
+                );
+            })
+            .map(function (i, elem) {
+                const val = $(this).val();
+                return val == null
+                ? null
+                : $.isArray(val)
+                ? $.map(val, (innerVal) => ({ name: elem.name, value: innerVal }))
+                : {
+                    name: elem.name,
+                    value:
+                        o.checkboxesAsBools && this.type === "checkbox" //moar ternaries!
+                        ? this.checked
+                            ? "true"
+                            : "false"
+                        : val,
+                    };
+            })
+            .get();
+        };
+    }
+    )(jQuery);
+
+    $("#disease-statuses")["DataTable"]({
+        columnDefs: [
+            { orderable: false, targets: 4 },
+            { width: "180px", targets: 4 },
+        ],
+        paging: false,
+        info: false,
+        autoWidth: false,
+        language: {
+            search: "Filter: ",
+        },
+    });
+
+    $("#modal-disease-status-form").submit(function (e) {
+        adacDiseaseStatusVM.modalSubmit(e);
+    });
+
+  $(".delete-confirm").click(function (e) {
+      e.preventDefault();
+
+      var $link = $(this);
+      var linkData = $link.data("refdata-model")
+      var url = $link.data("resource-url") + "/" + linkData.OntologyTermId;
+
+      bootbox.confirm("Are you sure you want to delete " + linkData.Description + "?",
+          function (confirmation) {
+              if (confirmation) {
+                  deleteRefData(url, $link.data("success-redirect"), $link.data("refdata-type"));
+              }
+          }
+      );
+  });
+
+    adacDiseaseStatusVM = new AdacDiseaseStatusViewModel();
+
+    ko.applyBindings(adacDiseaseStatusVM);
+});
