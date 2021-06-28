9fa9c907f7aee969d298b14807c445e47569e602
Joseph Lavagna <josephlavagna1998@gmail.com>
2021-06-28 15:40:18 +0100

    Added ConfigService and interface and setup DI

---
+++ src/Directory/Services/ConfigService.cs
@@ -0,0 +1,14 @@
+﻿using Biobanks.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biobanks.Services
+{
+    public class ConfigService : IConfigService
+    {
+
+    }
+}
