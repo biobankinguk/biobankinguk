71a5e309b9adfea88bd41540e322d8d9275e9479
Joseph Lavagna <josephlavagna1998@gmail.com>
2021-06-29 15:28:09 +0100

    IsFeatureFlag property and new migration

---
+++ src/Data/Migrations/20210629142527_ConfigIsFeatureFlag.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Biobanks.Data.Migrations
+{
+    public partial class ConfigIsFeatureFlag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFeatureFlag",
+                table: "Configs",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFeatureFlag",
+                table: "Configs");
+        }
+    }
+}
