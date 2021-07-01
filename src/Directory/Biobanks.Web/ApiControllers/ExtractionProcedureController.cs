eb2f44e55165e22d6f4086dd8d2097dd825f9555
Jonathan Couldridge <jcouldridge@outlook.com>
2021-07-01 10:04:03 +0100

    fixing extractionprocedurecontroller merge conflict whoopsie

--- src/Directory/Biobanks.Web/ApiControllers/ExtractionProcedureController.cs
+++ src/Directory/Biobanks.Web/ApiControllers/ExtractionProcedureController.cs
@@ -124,7 +124,7 @@ namespace Biobanks.Web.ApiControllers
             }
 
             //if ontology term id is in use by another ontology term
-            if ((await _biobankReadService.ListDiseaseOntologyTermsAsync()).Any(x => x.Id == model.OntologyTermId))
+            if ((await _biobankReadService.ListExtractionProceduresAsync()).Any(x => x.Id == model.OntologyTermId))
                 ModelState.AddModelError("OntologyTermId", "That ID is already in use. IDs must be unique across all ontology terms.");
 
             if (!ModelState.IsValid)
