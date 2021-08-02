fb76371ff6b3b92ecd1b8a62c21e726f81c4c618
James Fleming <james.fleming@nottingham.ac.uk>
2021-08-02 15:07:34 +0100

    Properly documented all CollectionService methods

--- src/Directory/Services/CollectionService.cs
+++ src/Directory/Services/CollectionService.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace Biobanks.Directory.Services
 {
+    /// <inheritdoc/>
     public class CollectionService : ICollectionService
     {
         private readonly IBiobankReadService _readService;
@@ -27,10 +28,7 @@ namespace Biobanks.Directory.Services
             _indexService = indexService;
         }
 
-        /// <summary>
-        /// Delete the Collection with a given Id. Will only proceed if the Collection has no SampleSets.
-        /// </summary>
-        /// <param name="id">The Id of the Collection to delete</param>
+        /// <inheritdoc/>
         public async Task<bool> DeleteCollection(int id)
         {
             if (await HasSampleSets(id))
@@ -45,9 +43,7 @@ namespace Biobanks.Directory.Services
             return true;
         }
 
-        /// <summary>
-        /// Add a new Collection
-        /// </summary>
+        /// <inheritdoc/>
         public async Task<Collection> AddCollection(Collection collection)
         {
             // Update Timestamp
@@ -60,10 +56,7 @@ namespace Biobanks.Directory.Services
             return collection;
         }
 
-        /// <summary>
-        /// Update an exisiting Collection.
-        /// </summary>
-        /// <param name="collection">The updated Collection</param>
+        /// <inheritdoc/>
         public async Task<Collection> UpdateCollection(Collection collection)
         {
             var currentCollection = await _db.Collections
@@ -97,11 +90,7 @@ namespace Biobanks.Directory.Services
             return collection;
         }
 
-        /// <summary>
-        /// Get the untracked Collection with associated Collection Id.
-        /// </summary>
-        /// <param name="id">The Id of the Collecton</param>
-        /// <returns>The collection with the given Id, or null if no collection exists with that Id</returns>
+        /// <inheritdoc/>
         public async Task<Collection> GetCollection(int id)
             => await _db.Collections
                 .AsNoTracking()
@@ -113,11 +102,7 @@ namespace Biobanks.Directory.Services
                 .Include(x => x.OntologyTerm)
                 .FirstOrDefaultAsync(x => x.CollectionId == id);
 
-        /// <summary>
-        /// Get the untracked Collection with associated Collection Id, including all SampleSets and MaterialDetails
-        /// </summary>
-        /// <param name="id">The Id of the Collecton</param>
-        /// <returns>The collection with the given Id, or null if no collection exists with that Id</returns>
+        /// <inheritdoc/>
         public async Task<Collection> GetEntireCollection(int id)
             => await _db.Collections
                 .AsNoTracking()
@@ -136,11 +121,7 @@ namespace Biobanks.Directory.Services
                 .Include(x => x.SampleSets.Select(y => y.MaterialDetails.Select(z => z.StorageTemperature)))
                 .FirstOrDefaultAsync(x => x.CollectionId == id);
 
-        /// <summary>
-        /// Get the untracked Collection with associated Collection Id, with all relevant properites for indexing.
-        /// </summary>
-        /// <param name="id">The Id of the Collecton</param>
-        /// <returns>The collection with the given Id, or null if no collection exists with that Id</returns>
+        /// <inheritdoc/>
         public async Task<Collection> GetIndexableCollection(int id)
             => await _db.Collections
                 .AsNoTracking()
@@ -155,11 +136,7 @@ namespace Biobanks.Directory.Services
                 .Include(x => x.SampleSets)
                 .FirstOrDefaultAsync(x => x.CollectionId == id);
 
-        /// <summary>
-        /// List untracked Collections
-        /// </summary>
-        /// <param name="organisationId">Optional filter by Organistation Id</param>
-        /// <returns>Enumerable of untracked Collections</returns>
+        /// <inheritdoc/>
         public async Task<IEnumerable<Collection>> ListCollections(int organisationId = default)
             => await _db.Collections
                 .AsNoTracking()
@@ -168,28 +145,18 @@ namespace Biobanks.Directory.Services
                 .Where(x => x.OrganisationId == organisationId || organisationId == default)
                 .ToListAsync();
 
-        /// <summary>
-        /// List untracked Collections, with given OntologyTerm value.
-        /// </summary>
-        /// <param name="ontologyTerm">Value of the OntologyTerm to filter by</param>
-        /// <returns>Enumerable of untracked Collections</returns>
+        /// <inheritdoc/>
         public async Task<IEnumerable<Collection>> ListCollectionsByOntologyTerm(string ontologyTerm)
             => await _db.Collections
                 .AsNoTracking()
                 .Where(x => x.OntologyTerm.Value == ontologyTerm)
                 .ToListAsync();
 
-        /// <summary>
-        /// Whether the Collection has been created from aggreagted data submitted via the API.
-        /// </summary>
-        /// <param name="id">The Id of the Collecton</param>
+        /// <inheritdoc/>
         public async Task<bool> IsFromApi(int id)
             => await _db.Collections.AnyAsync(x => x.CollectionId == id && x.FromApi);
 
-        /// <summary>
-        /// Check if the Collection has associated SampleSets
-        /// </summary>
-        /// <param name="id">The Id of the Collection</param>
+        /// <inheritdoc/>
         public async Task<bool> HasSampleSets(int id)
             => await _db.SampleSets.AnyAsync(x => x.CollectionId == id);
     }
--- src/Directory/Services/Contracts/ICollectionService.cs
+++ src/Directory/Services/Contracts/ICollectionService.cs
@@ -6,21 +6,89 @@ namespace Biobanks.Directory.Services.Contracts
 {
     public interface ICollectionService
     {
+        /// <summary>
+        /// Deletes an empty Collection. If the Collection has any SampleSets it will fail to be deleted
+        /// </summary>
+        /// <param name="id">The Id of the Collection to delete</param>
         Task<bool> DeleteCollection(int id);
 
+        /// <summary>
+        /// Create a new Collection. At the point of creation, the Collection's timestamp will be updated to reflect
+        /// the latest change
+        /// </summary>
+        /// <returns>The newly created Collection with assigned idenitity Id</returns>
         Task<Collection> AddCollection(Collection collection);
+
+        /// <summary>
+        /// Update an exisiting Collection on both database and search index
+        /// </summary>
+        /// <returns>The updated Collection with updated fields and timestamp</returns>
         Task<Collection> UpdateCollection(Collection collection);
 
+        /// <summary>
+        /// Get the untracked Collection with associated Collection Id
+        /// </summary>
+        /// <returns>
+        /// <para>The Collection including populated fields:</para>
+        /// <para>AccessCondition, AssociatedData, CollectionType, CollectionStatus, ConsentRestrictions and OntologyTerm</para>
+        /// <para>Note: AssociatedData does NOT include it's child entities</para>
+        /// </returns>
         Task<Collection> GetCollection(int id);
+
+        /// <summary>
+        /// Get the untracked Collection with associated Collection Id, including all SampleSets and MaterialDetails
+        /// </summary>
+        /// <returns>
+        /// <para>The Collection including all fields</para>
+        /// </returns>
         Task<Collection> GetEntireCollection(int id);
+
+        /// <summary>
+        /// Get the untracked Collection with associated Collection Id for use in search indexing
+        /// </summary>
+        /// <returns>
+        /// <para>The Collection including populated fields for indexing:</para>
+        /// <para>AccessCondition, AssociatedData, CollectionType, CollectionStatus, ConsentRestrictions, OntologyTerm and SampleSets</para>
+        /// <para>Note: AssociatedData does include it's child entities</para>
+        /// </returns>
         Task<Collection> GetIndexableCollection(int id);
 
+        /// <summary>
+        /// List untracked Collections
+        /// </summary>
+        /// <param name="organisationId">Optional filter by Organistation Id</param>
+        /// <returns>Enumerable of untracked Collections</returns>
         Task<IEnumerable<Collection>> ListCollections(int organisationId = default);
+
+        /// <summary>
+        /// List untracked Collections, with given OntologyTerm value.
+        /// </summary>
+        /// <param name="ontologyTerm">Value of the OntologyTerm to filter by</param>
+        /// <returns>Enumerable of untracked Collections</returns>
         Task<IEnumerable<Collection>> ListCollectionsByOntologyTerm(string ontologyTerm);
 
+        /// <summary>
+        /// Whether the Collection has been created from aggreagted data submitted via the API.
+        /// </summary>
+        /// <param name="id">The Id of the Collecton</param>
+        /// <returns>
+        /// <list>
+        ///     <item>
+        ///         <term>true</term>
+        ///         <description>If the Collection has been generated from aggregated data, submitted via the API</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>false</term>
+        ///         <description>If the Collection was created by a user via the Directory</description>
+        ///     </item>
+        /// </list>
+        /// </returns>
         Task<bool> IsFromApi(int id);
-        Task<bool> HasSampleSets(int id);
 
-        //Task<Collection> GetCollectionWithSampleSetsById(int id);
+        /// <summary>
+        /// Check if the Collection has associated SampleSets
+        /// </summary>
+        /// <param name="id">The Id of the Collection</param>
+        Task<bool> HasSampleSets(int id);
     }
 }
