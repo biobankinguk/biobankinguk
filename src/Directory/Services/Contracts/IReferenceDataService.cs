509f3a44158efbe3423c3d5680c5cfd8bc8e87f9
James Fleming <james.fleming@nottingham.ac.uk>
2021-09-03 14:19:51 +0100

    Create a generic ReferenceDataService

---
+++ src/Directory/Services/Contracts/IReferenceDataService.cs
@@ -0,0 +1,16 @@
+﻿using Biobanks.Entities.Data.ReferenceData;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Biobanks.Directory.Services.Contracts
+{
+    public interface IReferenceDataService
+    {
+        Task<ICollection<T>> List<T>() where T : ReferenceDataBase;
+
+        Task<T> Get<T>(int id) where T : ReferenceDataBase;
+
+        Task<bool> Exists<T>(string value) where T : ReferenceDataBase;
+
+    }
+}
---
+++ src/Directory/Services/ReferenceDataService.cs
@@ -0,0 +1,41 @@
+﻿using Biobanks.Directory.Data;
+using Biobanks.Directory.Services.Contracts;
+using Biobanks.Entities.Data.ReferenceData;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biobanks.Directory.Services
+{
+    public class ReferenceDataService : IReferenceDataService
+    {
+        private readonly BiobanksDbContext _db;
+
+        public ReferenceDataService(BiobanksDbContext db)
+        {
+            _db = db;
+        }
+
+        /// <inheritdoc/>
+        public async Task<int> Count<T>() where T : ReferenceDataBase
+            => await _db.Set<T>().CountAsync();
+
+        /// <inheritdoc/>
+        public async Task<bool> Exists<T>(string value) where T : ReferenceDataBase
+            => await _db.Set<T>().AnyAsync(x => x.Value == value);
+
+        /// <inheritdoc/>
+        public async Task<T> Get<T>(int id) where T : ReferenceDataBase
+            => await _db.Set<T>()
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+        /// <inheritdoc/>
+        public async Task<ICollection<T>> List<T>() where T : ReferenceDataBase
+            => await _db.Set<T>()
+                .AsNoTracking()
+                .OrderBy(x => x.SortOrder)
+                .ToListAsync();
+    }
+}
