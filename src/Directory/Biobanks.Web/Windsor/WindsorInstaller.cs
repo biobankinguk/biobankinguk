473634bd105e38ec24bb551cafdac62ec8d00264
James Fleming <james.fleming@nottingham.ac.uk>
2021-09-15 10:16:38 +0100

    Create CollectionStatus service and DI

--- src/Directory/Biobanks.Web/Windsor/WindsorInstaller.cs
+++ src/Directory/Biobanks.Web/Windsor/WindsorInstaller.cs
@@ -127,6 +127,10 @@ namespace Biobanks.Web.Windsor
                     .ImplementedBy(typeof(PublicationService))
                     .LifeStyle.Transient,
 
+                Component.For(typeof(IReferenceDataService<CollectionStatus>))
+                    .ImplementedBy(typeof(CollectionStatusService))
+                    .LifeStyle.Transient,
+
                 Component.For(typeof(IRegistrationDomainService))
                     .ImplementedBy(typeof(RegistrationDomainService))
                     .LifeStyle.Transient,
