ea2408d00530970bbfc1c15e303867d00026558c
James Fleming <james.fleming@nottingham.ac.uk>
2021-09-16 10:49:27 +0100

    Fix Usage/IsInUse implementation

--- src/Directory/Services/CountryService.cs
+++ src/Directory/Services/CountryService.cs
@@ -16,9 +16,9 @@ namespace Biobanks.Directory.Services
                 .Include(x => x.Organisations);
 
         public override async Task<int> GetUsageCount(int id)
-            => await _db.Countries.CountAsync(x => x.Id == id) + await _db.Counties.CountAsync(x => x.CountryId == id);
+            => await _db.Counties.CountAsync(x => x.CountryId == id) + await _db.Organisations.CountAsync(x => x.CountryId == id);
 
         public override async Task<bool> IsInUse(int id)
-            => await _db.Countries.AnyAsync(x => x.Id == id) || await _db.Counties.AnyAsync(x => x.CountryId == id);
+            => await _db.Counties.AnyAsync(x => x.CountryId == id) || await _db.Organisations.AnyAsync(x => x.CountryId == id);
     }
 }
