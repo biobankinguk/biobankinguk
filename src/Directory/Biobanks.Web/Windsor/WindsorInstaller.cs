bc6b4eb1ff8cef1e103156c617e6a6cf4be5155b
James Fleming <james.fleming@nottingham.ac.uk>
2021-09-16 14:24:06 +0100

    Create FunderService and DI

--- src/Directory/Biobanks.Web/Windsor/WindsorInstaller.cs
+++ src/Directory/Biobanks.Web/Windsor/WindsorInstaller.cs
@@ -127,6 +127,10 @@ namespace Biobanks.Web.Windsor
                     .ImplementedBy(typeof(PublicationService))
                     .LifeStyle.Transient,
 
+                Component.For(typeof(IReferenceDataService<FunderService>))
+                    .ImplementedBy(typeof(FunderService))
+                    .LifeStyle.Transient,
+
                 Component.For(typeof(IRegistrationDomainService))
                     .ImplementedBy(typeof(RegistrationDomainService))
                     .LifeStyle.Transient,
