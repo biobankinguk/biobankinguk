050609f5fbf5a5be4868f57caaa40817877a8d40
James Fleming <james.fleming@nottingham.ac.uk>
2021-09-16 09:52:12 +0100

    Created ConsentRestrictionService and DI

--- src/Directory/Biobanks.Web/Windsor/WindsorInstaller.cs
+++ src/Directory/Biobanks.Web/Windsor/WindsorInstaller.cs
@@ -127,6 +127,10 @@ namespace Biobanks.Web.Windsor
                     .ImplementedBy(typeof(PublicationService))
                     .LifeStyle.Transient,
 
+                Component.For(typeof(IReferenceDataService<ConsentRestriction>))
+                    .ImplementedBy(typeof(ConsentRestrictionService))
+                    .LifeStyle.Transient,
+
                 Component.For(typeof(IRegistrationDomainService))
                     .ImplementedBy(typeof(RegistrationDomainService))
                     .LifeStyle.Transient,
