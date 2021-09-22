a9bf13661a6fad0d93f27e1ac4670f7846bc292f
James Fleming <james.fleming@nottingham.ac.uk>
2021-09-22 09:47:43 +0100

    Created Storage Temperature Service and DI

---
+++ src/Directory/Services/StorageTemperatureService.cs
@@ -0,0 +1,18 @@
+﻿using Biobanks.Directory.Data;
+using Biobanks.Entities.Shared.ReferenceData;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace Biobanks.Directory.Services
+{
+    public class StorageTemperatureService : ReferenceDataService<StorageTemperature>
+    {
+        public StorageTemperatureService(BiobanksDbContext db) : base(db) { }
+
+        public override async Task<int> GetUsageCount(int id)
+            => await _db.MaterialDetails.CountAsync(x => x.StorageTemperatureId == id);
+
+        public override async Task<bool> IsInUse(int id)
+            => await _db.MaterialDetails.AnyAsync(x => x.StorageTemperatureId == id);
+    }
+}
