50f8342a254876beb240a74aac01b7d0a561290f
Jonathan Couldridge <jcouldridge@outlook.com>
2021-10-01 16:37:59 +0100

    more wip stuff

--- docs/pages/api-guide/sidebar-order.js
+++ docs/pages/api-guide/sidebar-order.js
@@ -2,5 +2,9 @@ export const apiSidebarOrdering = {
   "api-guide": {
     index: true,
     "getting-started": true,
+    "bulk-submissions": {
+      "process-overview": true,
+      examples: true,
+    },
   },
 };
