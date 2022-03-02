b7b629b578e546b191b5f9eb8bd9a2ff3e5f56f1
Miriam Aydt <miriam.aydt@yahoo.de>
2022-03-02 10:01:16 +0000

    added new project omop including dbcontext and models

--- src/Omop/Omop/Context/OmopDbContext.cs
+++ src/Omop/Omop/Context/OmopDbContext.cs
@@ -1,23 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Biobanks.Omop.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace Omop
+namespace Biobanks.Omop.Context
 {
     public class OmopDbContext : DbContext
     {
         public DbSet<ConditionOccurence> ConditionOccurences { get; set; }
 
-        public OmopDbContext() : base()
-        {
 
-        }
+        public OmopDbContext(DbContextOptions<OmopDbContext> options) : base(options)
+        { }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
-            Debug.WriteLine("OnConfiguring was called!");
-
-            // Generally use lazy loading.
-            optionsBuilder.UseLazyLoadingProxies();
         }
 
 
@@ -32,34 +28,29 @@ namespace Omop
                 }).IsUnique();
         }
 
-        public OmopDbContext(DbContextOptions options) : base(options)
-        {
 
+        //public class OmopDbContextFactory : IDesignTimeDbContextFactory<OmopDbContext>
+        //{
+        //    /*  Required for DesignTime creation of the context. EF operations can be done from
+        //     *  either Package Manager or via Dotnet EF Tools.
+        //     *  
+        //     *  For either option, the connection string of the database must be passed via
+        //     *  CLI arguments.
+        //     *  
+        //     *  Package Manager - https://docs.microsoft.com/en-us/ef/core/cli/powershell
+        //     *  >> <Migrations Command> -Args "<Connection-String>"
+        //     *  
+        //     *  dotnet CLI - https://docs.microsoft.com/en-us/ef/core/cli/dotnet
+        //     *  >> dotnet ef <CLI Command> -- "<Connection-String>"
+        //     */
 
-        }
+        //    public OmopDbContext CreateDbContext(string[] args)
+        //        => new(
+        //            new DbContextOptionsBuilder()
 
-        public class OmopDbContextFactory : IDesignTimeDbContextFactory<OmopDbContext>
-        {
-            /*  Required for DesignTime creation of the context. EF operations can be done from
-             *  either Package Manager or via Dotnet EF Tools.
-             *  
-             *  For either option, the connection string of the database must be passed via
-             *  CLI arguments.
-             *  
-             *  Package Manager - https://docs.microsoft.com/en-us/ef/core/cli/powershell
-             *  >> <Migrations Command> -Args "<Connection-String>"
-             *  
-             *  dotnet CLI - https://docs.microsoft.com/en-us/ef/core/cli/dotnet
-             *  >> dotnet ef <CLI Command> -- "<Connection-String>"
-             */
-
-            public OmopDbContext CreateDbContext(string[] args)
-                => new(
-                    new DbContextOptionsBuilder()
-
-                        .UseNsqlServer(args[0], opts => opts.EnableRetryOnFailure())
-                        .Options
-                    );
-        }
+        //                .UseNsqlServer(args[0], opts => opts.EnableRetryOnFailure())
+        //                .Options
+        //            );
+        //}
     }
 }
--- src/Omop/Omop/Entities/ConditionOccurence.cs
+++ src/Omop/Omop/Entities/ConditionOccurence.cs
@@ -1,34 +1,34 @@
 ﻿using System;
 
 namespace Biobanks.Omop.Entities
-
-/// <inheritdoc />
-public class ConditionOccurence
 {
-    public int ConditionOccurenceId { get; set; }
-    public int PersonId { get; set; }
-    public int ConditionConceptId { get; set; }
-    public Date ConditionStartDate { get; set; }
-    public DateTime ConditionStartDatetime { get; set; }
-    public Date ConditionEndDate { get; set; }
-    public DateTime ConditionEndDatetime { get; set; }
-    public int ConditionTypeConceptId { get; set; }
-    public int ConditionStatusConceptId { get; set; }
-    public string StopReason { get; set; }
-    public int ProviderId { get; set; }
-    public int VisitOccurenceId { get; set; }
-    public int VisitDetailId { get; set; }
-    /// <summary>
-    /// Represents the condition that occured. E.g. ICD10
-    /// </summary>
-    public string ConditionSourceValue { get; set; }
-    /// <summary>
-    /// Concept that represents the cindition source value
-    /// </summary>
-    public int ConditionSourceConceptId { get; set; }
-    /// <summary>
-    /// Represents the conidtion status
-    /// </summary>
-    public string ConditionStatusSourceValue { get; set; }
+    /// <inheritdoc />
+    public class ConditionOccurence
+    {
+        public int ConditionOccurenceId { get; set; }
+        public int PersonId { get; set; }
+        public int ConditionConceptId { get; set; }
+        public DateTime ConditionStartDate { get; set; }
+        public DateTime ConditionStartDatetime { get; set; }
+        public DateTime ConditionEndDate { get; set; }
+        public DateTime ConditionEndDatetime { get; set; }
+        public int ConditionTypeConceptId { get; set; }
+        public int ConditionStatusConceptId { get; set; }
+        public string StopReason { get; set; }
+        public int ProviderId { get; set; }
+        public int VisitOccurenceId { get; set; }
+        public int VisitDetailId { get; set; }
+        /// <summary>
+        /// Represents the condition that occured. E.g. ICD10
+        /// </summary>
+        public string ConditionSourceValue { get; set; }
+        /// <summary>
+        /// Concept that represents the cindition source value
+        /// </summary>
+        public int ConditionSourceConceptId { get; set; }
+        /// <summary>
+        /// Represents the conidtion status
+        /// </summary>
+        public string ConditionStatusSourceValue { get; set; }
+    }
 }
-
