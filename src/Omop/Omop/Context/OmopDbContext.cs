4b9beb9d201b0718f2eced5eeeea012a281fe5b3
Miriam Aydt <miriam.aydt@yahoo.de>
2022-03-02 10:53:51 +0000

    added new project omop including dbcontext and models

---
+++ src/Omop/Omop/Context/OmopDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Biobanks.Omop.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biobanks.Omop.Context
+{
+    public class OmopDbContext : DbContext
+    {
+        public DbSet<ConditionOccurence> ConditionOccurences { get; set; }
+
+
+        public OmopDbContext(DbContextOptions<OmopDbContext> options) : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            // Indices (for unique constraints)
+            model.Entity<ConditionOccurence>()
+                .HasIndex(x => new
+                {
+                    x.ConditionOccurenceId
+
+                }).IsUnique();
+        }
+
+
+        //public class OmopDbContextFactory : IDesignTimeDbContextFactory<OmopDbContext>
+        //{
+        //    /*  Required for DesignTime creation of the context. EF operations can be done from
+        //     *  either Package Manager or via Dotnet EF Tools.
+        //     *  
+        //     *  For either option, the connection string of the database must be passed via
+        //     *  CLI arguments.
+        //     *  
+        //     *  Package Manager - https://docs.microsoft.com/en-us/ef/core/cli/powershell
+        //     *  >> <Migrations Command> -Args "<Connection-String>"
+        //     *  
+        //     *  dotnet CLI - https://docs.microsoft.com/en-us/ef/core/cli/dotnet
+        //     *  >> dotnet ef <CLI Command> -- "<Connection-String>"
+        //     */
+
+        //    public OmopDbContext CreateDbContext(string[] args)
+        //        => new(
+        //            new DbContextOptionsBuilder()
+
+        //                .UseNsqlServer(args[0], opts => opts.EnableRetryOnFailure())
+        //                .Options
+        //            );
+        //}
+    }
+}
