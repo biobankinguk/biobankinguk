a3f34f6f78d29ea47ce994c5b66adc803a00c567
Olivia Pulukuri <oliviadozwert1@gmail.com>
2022-04-07 14:25:28 +0100

    AB#53255 Added code for AnnualStatisticGroupController

---
+++ src/Submissions/Api/Controllers/ReferenceData/AnnualStatisticGroupController.cs
@@ -0,0 +1,52 @@
+﻿using Biobanks.Entities.Data.ReferenceData;
+using Biobanks.Shared.Services.Contracts;
+using Biobanks.Submissions.Api.Models.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biobanks.Submissions.Api.Controllers.ReferenceData
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "Reference Data")]
+    public class AnnualStatisticGroupController : ControllerBase
+    {
+        private readonly IReferenceDataService<AnnualStatisticGroup> _annualStatisticGroupService;
+
+
+        public AnnualStatisticGroupController(IReferenceDataService<AnnualStatisticGroup> annualStatisticGroupService)
+        {
+            _annualStatisticGroupService = annualStatisticGroupService;
+        }
+        /// <summary>
+        /// Generate annual statistic group list
+        /// </summary>
+        /// <returns>A list of annual statistic groups</returns>
+        /// <response code="200">Request Successful</response>
+        [HttpGet]
+        [AllowAnonymous]
+        [SwaggerResponse(200, Type = typeof(AnnualStatisticGroupModel))]
+        public async Task<IList> Get()
+        {
+            var model = (await _annualStatisticGroupService.List())
+                .Select(x =>
+
+                Task.Run(async () => new AnnualStatisticGroupModel
+                {
+                    AnnualStatisticGroupId = x.Id,
+                    Name = x.Value,
+                }).Result)
+
+                .ToList();
+
+            return model;
+        }
+
+
+
+    }
+}
