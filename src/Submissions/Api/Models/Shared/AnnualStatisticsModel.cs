d2fee291fd5f203818f39094f3d23774ae4fd90d
Olivia Pulukuri <oliviadozwert1@gmail.com>
2022-04-07 14:00:56 +0100

    AB#53255 Added AnnualStatisticController Code

---
+++ src/Submissions/Api/Models/Shared/AnnualStatisticsModel.cs
@@ -0,0 +1,28 @@
+﻿using Biobanks.Submissions.Api.Models.Shared;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Biobanks.Submissions.Api.Models.ADAC
+{
+    public class AnnualStatisticsModel
+    {
+        public ICollection<AnnualStatisticModel> AnnualStatistics { get; set; }
+        public ICollection<AnnualStatisticGroupModel> AnnualStatisticGroups { get; set; }
+    }
+
+    public class AnnualStatisticModel
+    {
+        public int Id { get; set; }
+
+        public int AnnualStatisticGroupId { get; set; }
+
+        public string AnnualStatisticGroupName { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        //public int SortOrder { get; set; }
+
+        //public int UsageCount { get; set; }
+    }
+}
