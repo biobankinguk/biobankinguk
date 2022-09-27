5f1cc0afc152a56f33eae5ceb2c93cb59d98dd66
Andy Rae <andy.rae@nottingham.ac.uk>
2022-09-27 15:09:05 +0100

    Add documentation to new interface

--- src/Submissions/Api/Services/Directory/Contracts/IContentPageService.cs
+++ src/Submissions/Api/Services/Directory/Contracts/IContentPageService.cs
@@ -8,51 +8,51 @@ namespace Biobanks.Submissions.Api.Services.Directory.Contracts
     public interface IContentPageService
     {
         /// <summary>
-        /// 
+        /// Create a new content page
         /// </summary>
-        /// <param name="title"></param>
-        /// <param name="body"></param>
-        /// <param name="slug"></param>
-        /// <param name="isEnabled"></param>
+        /// <param name="title">Title of the page</param>
+        /// <param name="body">Page body</param>
+        /// <param name="slug">Page </param>
+        /// <param name="isEnabled">Whether the page is shown publicly</param>
         /// <returns></returns>
         Task Create(string title, string body, string slug, bool isEnabled);
 
         /// <summary>
-        /// 
+        /// Update an existing content page
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="title"></param>
-        /// <param name="body"></param>
-        /// <param name="slug"></param>
-        /// <param name="isEnabled"></param>
+        /// <param name="id">Id of the page to update</param>
+        /// <param name="title">New title of the page</param>
+        /// <param name="body">New body of the page</param>
+        /// <param name="slug">New slug of the page</param>
+        /// <param name="isEnabled">Whether the page is shown publicly</param>
         /// <returns></returns>
         Task Update(int id, string title, string body, string slug, bool isEnabled);
 
         /// <summary>
-        /// 
+        /// Delete an existing content page
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">The Id of the page</param>
         /// <returns></returns>
         Task Delete(int id);
 
         /// <summary>
-        /// 
+        /// List all content pages
         /// </summary>
-        /// <returns></returns>
+        /// <returns>An Enumerable of all content pages</returns>
         Task<IEnumerable<ContentPage>> ListContentPages();
 
         /// <summary>
-        /// 
+        /// Get a content page by its Id
         /// </summary>
         /// <param name="id"></param>
-        /// <returns></returns>
+        /// <returns>The content page with assigned Id</returns>
         Task<ContentPage> GetById(int id);
 
         /// <summary>
-        /// 
+        /// Get a content page by its slug
         /// </summary>
         /// <param name="routeSlug"></param>
-        /// <returns></returns>
+        /// <returns>The content page with assigned slug</returns>
         Task<ContentPage> GetBySlug(string routeSlug);
     }
 }
