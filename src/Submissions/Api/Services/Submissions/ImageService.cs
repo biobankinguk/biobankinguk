3aea4da3af2ea03af4c4c862d297369363773689
Moiz <moiziqbal5021@gmail.com>
2022-09-27 13:09:49 +0100

    use of imageflow

--- src/Submissions/Api/Services/Submissions/ImageService.cs
+++ src/Submissions/Api/Services/Submissions/ImageService.cs
@@ -1,23 +1,27 @@
 using System;
 using System.IO;
+using Imageflow.Fluent;
 
 namespace Biobanks.Services
 {
     // General helpers for working with images
     public static class ImageService
     {
-        public static MemoryStream ResizeImageStream(Stream inStream, int maxX, int maxY)
+        public static  MemoryStream ResizeImageStream(Stream inStream, int maxX, int maxY)
         {
             if (inStream.Length < 1) throw new ArgumentException("The provided Input Stream contains no data");
 
             var outStream = new MemoryStream();
 
-            var job = new ImageResizer.ImageJob(
-                inStream,
-                outStream,
-                new ImageResizer.Instructions($"width={maxX};height={maxY};mode=max"));
+            using (var b = new ImageJob())
+            {
+                var job =  b.BuildCommandString(
+                    (IBytesSource)inStream,
+                    (IOutputDestination)outStream,
+                    $"width={maxX};height={maxY};mode=max")
+                                .Finish();
 
-            job.Build();
+            }
 
             return outStream;
         }
