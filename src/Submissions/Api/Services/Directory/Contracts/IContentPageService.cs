53f1ec9b90f4653d5015c117fda0774aabe47a23
Andy Rae <andy.rae@nottingham.ac.uk>
2022-09-27 14:57:11 +0100

    Port contentpage service

---
+++ src/Submissions/Api/Services/Directory/Contracts/IContentPageService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Biobanks.Entities.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Biobanks.Submissions.Api.Services.Directory.Contracts
+{
+    public interface IContentPageService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="body"></param>
+        /// <param name="slug"></param>
+        /// <param name="isEnabled"></param>
+        /// <returns></returns>
+        Task Create(string title, string body, string slug, bool isEnabled);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="title"></param>
+        /// <param name="body"></param>
+        /// <param name="slug"></param>
+        /// <param name="isEnabled"></param>
+        /// <returns></returns>
+        Task Update(int id, string title, string body, string slug, bool isEnabled);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task Delete(int id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<ContentPage>> ListContentPages();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<ContentPage> GetById(int id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="routeSlug"></param>
+        /// <returns></returns>
+        Task<ContentPage> GetBySlug(string routeSlug);
+    }
+}
+
