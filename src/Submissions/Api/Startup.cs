7ba382a224053a6e122565d4278467e0c563609a
MoizIqbal97 <90258816+MoizIqbal97@users.noreply.github.com>
2022-09-28 10:49:24 +0100

    AB#76187: RegistartionDomainService and SampleSetExtension (#416)
    added regtation domain service
    
    Co-authored-by: Philip Van Krimpen <squigglebunchkins@gmail.com>

--- src/Submissions/Api/Startup.cs
+++ src/Submissions/Api/Startup.cs
@@ -228,7 +228,8 @@ namespace Biobanks.Submissions.Api
                 .AddTransient<IAnalyticsService, AnalyticsService>()
                 .AddTransient<IGoogleAnalyticsReportingService, GoogleAnalyticsReportingService>()
 
-                .AddTransient<ISubmissionExpiryService, SubmissionExpiryService>();
+                .AddTransient<ISubmissionExpiryService, SubmissionExpiryService>()
+                .AddTransient<IRegistrationDomainService, RegistrationDomainService>();
 
 
             //Directory Services
