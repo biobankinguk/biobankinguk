2b3a6450d594181a256a605ae59b7250d68cebdb
Andy Rae <andy.rae@nottingham.ac.uk>
2022-09-28 11:48:46 +0100

    Mark directory service obsolete

--- src/Directory/Services/Contracts/IEmailService.cs
+++ src/Directory/Services/Contracts/IEmailService.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Threading.Tasks;
 
 namespace Biobanks.Services.Contracts
 {
+    [Obsolete("To be deleted when the Directory core version goes live." +
+        " Any changes made here will need to be made in the corresponding core version"
+        , false)]
     public interface IEmailService
     {
         Task SendDirectoryAdminNewRegisterRequestNotification(string requesterName, string requesterEmail, string entityName, string entityType);
