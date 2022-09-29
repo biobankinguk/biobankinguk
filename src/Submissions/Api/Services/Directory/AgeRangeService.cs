be3d4607b79718b0d740eefbe1b4468e45708297
Andy Rae <andy.rae@nottingham.ac.uk>
2022-09-29 14:04:29 +0100

    Port ageranges service

---
+++ src/Submissions/Api/Services/Directory/AgeRangeService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Biobanks.Data;
+using Biobanks.Entities.Data.ReferenceData;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biobanks.Submissions.Api.Services.Directory
+{
+    public class AgeRangeService : ReferenceDataService<AgeRange>
+    {
+        public AgeRangeService(BiobanksDbContext db) : base(db) { }
+
+        public override async Task<int> GetUsageCount(int id)
+            => await _db.SampleSets.CountAsync(x => x.AgeRangeId == id);
+
+        public override async Task<bool> IsInUse(int id)
+            => await _db.SampleSets.AnyAsync(x => x.AgeRangeId == id);
+
+        public override async Task<AgeRange> Update(AgeRange entity)
+        {
+            var existing = await base.Update(entity);
+            existing.UpperBound = entity.UpperBound;
+            existing.LowerBound = entity.LowerBound;
+            await _db.SaveChangesAsync();
+
+            return existing;
+        }
+    }
+}
+
