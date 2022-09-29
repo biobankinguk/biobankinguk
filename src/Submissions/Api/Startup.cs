5728f4353d45f410ff073c1cb5e18a92c201d7ee
Andy Rae <andy.rae@nottingham.ac.uk>
2022-09-29 14:12:08 +0100

    Register annualstatistic service

--- src/Submissions/Api/Startup.cs
+++ src/Submissions/Api/Startup.cs
@@ -259,7 +259,8 @@ namespace Biobanks.Submissions.Api
                 // Reference Data
                 services
                     .AddTransient<Services.Directory.Contracts.IReferenceDataService<AccessCondition>, AccessConditionService>()
-                    .AddTransient<Services.Directory.Contracts.IReferenceDataService<AgeRange>, AgeRangeService>();
+                    .AddTransient<Services.Directory.Contracts.IReferenceDataService<AgeRange>, AgeRangeService>()
+                    .AddTransient<Services.Directory.Contracts.IReferenceDataService<AnnualStatistic>, AnnualStatisticService>();
             }
 
 
