38ba2faee471ca281e08776e0083aaa1bd386129
Andy Rae <andy.rae@nottingham.ac.uk>
2022-09-29 10:35:01 +0100

    Mark directory service obsolete

--- src/Directory/Services/Contracts/ITokenLoggingService.cs
+++ src/Directory/Services/Contracts/ITokenLoggingService.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Threading.Tasks;
 
 namespace Biobanks.Services.Contracts
 {
+    [Obsolete("To be deleted when the Directory core version goes live." +
+        " Any changes made here will need to be made in the corresponding core version"
+        , false)]
     public interface ITokenLoggingService
     {
         //Validation records get created and return an id, so you can update success status
