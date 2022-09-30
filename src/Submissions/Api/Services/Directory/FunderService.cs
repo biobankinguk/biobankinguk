cf5ba0d8367aa313b7400113f868d48849d96597
Andy Rae <andy.rae@nottingham.ac.uk>
2022-09-30 10:30:32 +0100

    Port funder service

---
+++ src/Submissions/Api/Services/Directory/FunderService.cs
@@ -0,0 +1,30 @@
+﻿using Biobanks.Data;
+using Biobanks.Entities.Data.ReferenceData;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biobanks.Submissions.Api.Services.Directory
+{
+    public class FunderService : ReferenceDataService<Funder>
+    {
+        public FunderService(BiobanksDbContext db) : base(db) { }
+
+        protected override IQueryable<Funder> Query()
+            => base.Query()
+                .Include(x => x.Organisations);
+
+        public override async Task<int> GetUsageCount(int id)
+            => await Query()
+                .Where(x => x.Id == id)
+                .SelectMany(x => x.Organisations)
+                .CountAsync();
+
+        public override async Task<bool> IsInUse(int id)
+            => await Query()
+                .Where(x => x.Id == id)
+                .SelectMany(x => x.Organisations)
+                .AnyAsync();
+    }
+}
+
