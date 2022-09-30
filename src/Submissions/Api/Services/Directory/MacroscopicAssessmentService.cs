8061bb08d7122cc91bf5a8e7753a88512ec2542c
Andy Rae <andy.rae@nottingham.ac.uk>
2022-09-30 10:34:58 +0100

    Port macroscopicAssessmentService

---
+++ src/Submissions/Api/Services/Directory/MacroscopicAssessmentService.cs
@@ -0,0 +1,19 @@
+﻿using Biobanks.Data;
+using Biobanks.Entities.Data.ReferenceData;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biobanks.Submissions.Api.Services.Directory
+{
+    public class MacroscopicAssessmentService : ReferenceDataService<MacroscopicAssessment>
+    {
+        public MacroscopicAssessmentService(BiobanksDbContext db) : base(db) { }
+
+        public override async Task<int> GetUsageCount(int id)
+            => await _db.MaterialDetails.CountAsync(x => x.MacroscopicAssessmentId == id);
+
+        public override async Task<bool> IsInUse(int id)
+            => await _db.MaterialDetails.AnyAsync(x => x.MacroscopicAssessmentId == id);
+    }
+}
+
