65a4c4544b8e519ec075b6845c31125fa331178f
Moiz <moiziqbal5021@gmail.com>
2022-09-30 10:25:12 +0100

    port over services

---
+++ src/Submissions/Api/Services/Directory/ConsentRestrictionService.cs
@@ -0,0 +1,23 @@
+﻿using Biobanks.Data;
+using Biobanks.Entities.Data.ReferenceData;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Biobanks.Submissions.Api.Services.Directory
+{
+    public class ConsentRestrictionService : ReferenceDataService<ConsentRestriction>
+    {
+        public ConsentRestrictionService(BiobanksDbContext db) : base(db) { }
+
+        protected override IQueryable<ConsentRestriction> Query()
+            => base.Query().Include(x => x.Collections);
+
+        public override async Task<int> GetUsageCount(int id)
+            => await Query().Where(x => x.Id == id).Where(x => x.Collections.Count() > 0).CountAsync();
+
+        public override async Task<bool> IsInUse(int id)
+            => await Query().Where(x => x.Id == id).Where(x => x.Collections.Count() > 0).AnyAsync();
+    }
+}
