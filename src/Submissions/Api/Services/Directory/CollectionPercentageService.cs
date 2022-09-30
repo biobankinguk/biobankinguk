51b0367ba2163d7c3ea586c106ba98d610624219
Andy Rae <andy.rae@nottingham.ac.uk>
2022-09-30 10:01:12 +0100

    Port collectionPercentageService

--- src/Submissions/Api/Services/Directory/CollectionPercentageService.cs
+++ src/Submissions/Api/Services/Directory/CollectionPercentageService.cs
@@ -1,21 +1,18 @@
-﻿using Biobanks.Directory.Data;
+﻿using System;
+using Biobanks.Data;
 using Biobanks.Entities.Data.ReferenceData;
-using System;
-using System.Data.Entity;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-namespace Biobanks.Directory.Services
+namespace Biobanks.Submissions.Api.Services.Directory
 {
-    [Obsolete("To be deleted when the Directory core version goes live." +
-        " Any changes made here will need to be made in the corresponding core version"
-        , false)]
     public class CollectionPercentageService : ReferenceDataService<CollectionPercentage>
     {
         public CollectionPercentageService(BiobanksDbContext db) : base(db) { }
 
         public override async Task<int> GetUsageCount(int id)
             => await _db.MaterialDetails.CountAsync(x => x.CollectionPercentageId == id);
-        
+
         public override async Task<bool> IsInUse(int id)
             => await _db.MaterialDetails.AnyAsync(x => x.CollectionPercentageId == id);
 
@@ -28,6 +25,6 @@ namespace Biobanks.Directory.Services
 
             return existing;
         }
-
     }
 }
+
