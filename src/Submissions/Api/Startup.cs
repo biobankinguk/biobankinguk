3ea3613ee9c43b5d4f6ea8858c67b36f436d4a93
MoizIqbal97 <90258816+MoizIqbal97@users.noreply.github.com>
2022-09-30 11:26:45 +0100

    AB#76181: Port network service (#422)
    * port over service and startup changes
    
    * marked old service as obsolete
    
    * using statement fix

--- src/Submissions/Api/Startup.cs
+++ src/Submissions/Api/Startup.cs
@@ -53,6 +53,7 @@ using Biobanks.Submissions.Api.Services.Directory;
 using Biobanks.Search.Contracts;
 using Biobanks.Search.Elastic;
 using Biobanks.Omop.Context;
+using Biobanks.Search.Legacy;
 using Npgsql;
 
 namespace Biobanks.Submissions.Api
@@ -253,8 +254,11 @@ namespace Biobanks.Submissions.Api
                     .AddTransient(typeof(IGenericEFRepository<>), typeof(IGenericEFRepository<>))
                     .AddTransient<IBiobankReadService, BiobankReadService>()
                     .AddTransient<IBiobankIndexService, BiobankIndexService>()
-                    .AddTransient<ILogoStorageProvider, SqlServerLogoStorageProvider>();
-             //   .AddTransient<ElasticCapabilityIndexProvider, ICapabilityIndexProvider>();
+                    .AddTransient<ILogoStorageProvider, SqlServerLogoStorageProvider>()
+                    .AddTransient<IIndexProvider, LegacyIndexProvider>()
+                    .AddTransient<INetworkService, NetworkService>();
+                //   .AddTransient<ElasticCapabilityIndexProvider, ICapabilityIndexProvider>();
+
 
             }
 
