4fa74a1379f7f9c9e19cbb4273bb3c26c00376db
Andy Rae <andy.rae@nottingham.ac.uk>
2022-10-03 12:14:59 +0100

    Mark old services obsolete

--- src/Directory/Services/StorageTemperatureService.cs
+++ src/Directory/Services/StorageTemperatureService.cs
@@ -1,11 +1,15 @@
 ﻿using Biobanks.Directory.Data;
 using Biobanks.Entities.Shared.ReferenceData;
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Biobanks.Directory.Services
 {
+    [Obsolete("To be deleted when the Directory core version goes live." +
+    " Any changes made here will need to be made in the corresponding core version"
+    , false)]
     public class StorageTemperatureService : ReferenceDataService<StorageTemperature>
     {
         public StorageTemperatureService(BiobanksDbContext db) : base(db) { }
