e6b871ac74ffd1d1352aa9660f676afd5d52d46a
Andy Rae <andy.rae@nottingham.ac.uk>
2022-10-05 09:39:57 +0100

    Working controller

--- src/Submissions/Api/Controllers/ReferenceData/AnnualStatisticGroupController.cs
+++ src/Submissions/Api/Controllers/ReferenceData/AnnualStatisticGroupController.cs
@@ -1,5 +1,5 @@
 ﻿using Biobanks.Entities.Data.ReferenceData;
-using Biobanks.Shared.Services.Contracts;
+using Biobanks.Submissions.Api.Services.Directory.Contracts;
 using Biobanks.Submissions.Api.Models.Shared;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +22,7 @@ namespace Biobanks.Submissions.Api.Controllers.ReferenceData
         {
             _annualStatisticGroupService = annualStatisticGroupService;
         }
+
         /// <summary>
         /// Generate annual statistic group list
         /// </summary>
@@ -46,7 +47,82 @@ namespace Biobanks.Submissions.Api.Controllers.ReferenceData
             return model;
         }
 
+        [HttpDelete("{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var model = await _annualStatisticGroupService.Get(id);
 
+            if (await _annualStatisticGroupService.IsInUse(id))
+            {
+                ModelState.AddModelError("Name", "This annual statistic group is currently in use and cannot be deleted.");
+            }
 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _annualStatisticGroupService.Delete(id);
+
+            //Everything went A-OK!
+            return Accepted(model.Value);
+        }
+
+        [HttpPut("{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult> Put(int id, AnnualStatisticGroupModel model)
+        {
+            var existing = await _annualStatisticGroupService.Get(model.Name);
+
+            //If this name is valid, it already exists
+            if (existing != null && existing.Id != id)
+            {
+                ModelState.AddModelError("Name", "That name is already in use by another annual statistic group. Annual Statistic Group names must be unique.");
+            }
+
+            if (await _annualStatisticGroupService.IsInUse(id))
+            {
+                ModelState.AddModelError("Name", "This annual statistic group is currently in use and cannot be edited.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _annualStatisticGroupService.Update(new AnnualStatisticGroup
+            {
+                Id = model.AnnualStatisticGroupId,
+                Value = model.Name
+            });
+
+            //Everything went A-OK!
+            return Accepted(model.Name);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Post(AnnualStatisticGroupModel model)
+        {
+            //If this name is valid, it already exists
+            if (await _annualStatisticGroupService.Exists(model.Name))
+            {
+                ModelState.AddModelError("Name", "That name is already in use. Annual Statistic Group names must be unique.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _annualStatisticGroupService.Add(new AnnualStatisticGroup
+            {
+                Value = model.Name
+            });
+
+            //Everything went A-OK!
+            return Accepted(model.Name);
+        }
     }
 }
