7f82c14f3d3d56e91b60beb563309bfd3f49929a
Moiz <moiziqbal5021@gmail.com>
2022-10-07 13:07:29 +0100

    mark old controller as Obsolete

--- src/Directory/Biobanks.Web/ApiControllers/AssociatedDataProcurementTimeFrameController.cs
+++ src/Directory/Biobanks.Web/ApiControllers/AssociatedDataProcurementTimeFrameController.cs
@@ -8,9 +8,14 @@ using System.Linq;
 using Biobanks.Entities.Data.ReferenceData;
 using Biobanks.Web.Filters;
 using Biobanks.Directory.Services.Contracts;
+using System;
 
 namespace Biobanks.Web.ApiControllers
 {
+    [Obsolete("To be deleted when the Directory core version goes live." +
+    " Any changes made here will need to be made in the corresponding core version"
+    , false)]
+
     [UserApiAuthorize(Roles = "ADAC")]
     [RoutePrefix("api/AssociatedDataProcurementTimeFrame")]
     public class AssociatedDataProcurementTimeFrameController : ApiBaseController
