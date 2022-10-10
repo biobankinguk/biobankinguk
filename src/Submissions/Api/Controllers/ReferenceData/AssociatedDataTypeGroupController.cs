19d33acac6507a825807e6bafb63d35c45fda328
Moiz <moiziqbal5021@gmail.com>
2022-10-10 15:04:59 +0100

    swagger annotation

--- src/Submissions/Api/Controllers/ReferenceData/AssociatedDataTypeGroupController.cs
+++ src/Submissions/Api/Controllers/ReferenceData/AssociatedDataTypeGroupController.cs
@@ -27,6 +27,11 @@ namespace Biobanks.Submissions.Api.Controllers.ReferenceData
             _associatedDataTypeGroupService = associatedDataTypeGroupService;
         }
 
+        /// <summary>
+        /// Generate a list of Associated Data Type Group.
+        /// </summary>
+        /// <returns>List of Associated Data Type Group.</returns>
+        /// <response code="200">Request Successful</response>
         [HttpGet("")]
         [AllowAnonymous]
         public async Task<IList> Get()
@@ -46,7 +51,12 @@ namespace Biobanks.Submissions.Api.Controllers.ReferenceData
             return model;
         }
 
-
+        /// <summary>
+        /// Delete an Associated Data Type Group.
+        /// </summary>
+        /// <param name="id">Id of the model to delete.</param>
+        /// <returns>The delete model.</returns>
+        /// <response code="200">Request Successful</response>
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
@@ -68,6 +78,12 @@ namespace Biobanks.Submissions.Api.Controllers.ReferenceData
             return Ok(model);
         }
 
+        /// <summary>
+        /// Insert a new Associated Data Type Group.
+        /// </summary>
+        /// <param name="model">Model of new  Associated Data Type Group.</param>
+        /// <returns>The inserted model.</returns>
+        /// <response code="200">Request Successful</response>
         [HttpPost("")]
         public async Task<IActionResult> Post(AssociatedDataTypeGroupModel model)
         {
@@ -91,6 +107,13 @@ namespace Biobanks.Submissions.Api.Controllers.ReferenceData
             return Ok(model);
         }
 
+        /// <summary>
+        /// Update an Associated Data Type Group.
+        /// </summary>
+        /// <param name="id">Id of the model to update.</param>
+        /// <param name="model">Model with updates values.</param>
+        /// <returns>The updated model.</returns>
+        /// <response code="200">Request Successful</response>
         [HttpPut("{id}")]
         public async Task<IActionResult> Put(int id, AssociatedDataTypeGroupModel model)
         {
