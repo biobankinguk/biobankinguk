125ab1c85fe137b9e3aa2415a55f5845c38bafa2
Andy Rae <andy.rae@nottingham.ac.uk>
2022-10-13 14:22:09 +0100

    Add docstrings

--- src/Submissions/Api/Controllers/ReferenceData/ServiceOfferingController.cs
+++ src/Submissions/Api/Controllers/ReferenceData/ServiceOfferingController.cs
@@ -24,8 +24,13 @@ namespace Biobanks.Submissions.Api.Controllers.ReferenceData
             _serviceOfferingService = serviceOfferingService;
         }
 
+        /// <summary>
+        /// Generate a list of Service Offering.
+        /// </summary>
+        /// <returns>The list of Service Offerings.</returns>
         [HttpGet]
         [AllowAnonymous]
+        [SwaggerResponse(200, Type = typeof(ServiceOffering))]
         public async Task<IList> Get()
         {
             var models = (await _serviceOfferingService.List())
@@ -44,16 +49,20 @@ namespace Biobanks.Submissions.Api.Controllers.ReferenceData
             return models;
         }
 
-        [HttpDelete("{id}")]
-        [AllowAnonymous]
-        public async Task<ActionResult> Delete(int id)
+        /// <summary>
+        /// Insert a new Service Offering.
+        /// </summary>
+        /// <param name="model">Model to insert.</param>
+        /// <returns>The inserted model.</returns>
+        [HttpPost]
+        [SwaggerResponse(200, Type = typeof(ServiceOffering))]
+        [SwaggerResponse(400, "Invalid request.")]
+        public async Task<ActionResult> Post(ServiceOfferingModel model)
         {
-            var model = await _serviceOfferingService.Get(id);
-
-            // If in use, prevent update
-            if (await _serviceOfferingService.IsInUse(id))
+            //If this description is valid, it already exists
+            if (await _serviceOfferingService.Exists(model.Name))
             {
-                ModelState.AddModelError("ServiceOffering", $"The service offering \"{model.Value}\" is currently in use, and cannot be deleted.");
+                ModelState.AddModelError("Name", "That name is already in use. Service offering names must be unique.");
             }
 
             if (!ModelState.IsValid)
@@ -61,14 +70,25 @@ namespace Biobanks.Submissions.Api.Controllers.ReferenceData
                 return BadRequest(ModelState);
             }
 
-            await _serviceOfferingService.Delete(id);
+            await _serviceOfferingService.Add(new ServiceOffering
+            {
+                Value = model.Name,
+                SortOrder = model.SortOrder
+            });
 
             //Everything went A-OK!
             return Ok(model);
         }
 
+        /// <summary>
+        /// Update an existing Service Offering.
+        /// </summary>
+        /// <param name="id">Id of the model to update.</param>
+        /// <param name="model">The model with updated values.</param>
+        /// <returns>The updated model.</returns>
         [HttpPut("{id}")]
-        [AllowAnonymous]
+        [SwaggerResponse(200, Type = typeof(ServiceOffering))]
+        [SwaggerResponse(400, "Invalid request.")]
         public async Task<ActionResult> Put(int id, ServiceOfferingModel model)
         {
             // Validate model
@@ -100,14 +120,22 @@ namespace Biobanks.Submissions.Api.Controllers.ReferenceData
             return Ok(model);
         }
 
-        [HttpPost]
-        [AllowAnonymous]
-        public async Task<ActionResult> Post(ServiceOfferingModel model)
+        /// <summary>
+        /// Delete an existing Service Offering.
+        /// </summary>
+        /// <param name="id">Id of the model to delete.</param>
+        /// <returns>The deleted model.</returns>
+        [HttpDelete("{id}")]
+        [SwaggerResponse(200, Type = typeof(ServiceOffering))]
+        [SwaggerResponse(400, "Invalid request.")]
+        public async Task<ActionResult> Delete(int id)
         {
-            //If this description is valid, it already exists
-            if (await _serviceOfferingService.Exists(model.Name))
+            var model = await _serviceOfferingService.Get(id);
+
+            // If in use, prevent update
+            if (await _serviceOfferingService.IsInUse(id))
             {
-                ModelState.AddModelError("Name", "That name is already in use. Service offering names must be unique.");
+                ModelState.AddModelError("ServiceOffering", $"The service offering \"{model.Value}\" is currently in use, and cannot be deleted.");
             }
 
             if (!ModelState.IsValid)
@@ -115,18 +143,21 @@ namespace Biobanks.Submissions.Api.Controllers.ReferenceData
                 return BadRequest(ModelState);
             }
 
-            await _serviceOfferingService.Add(new ServiceOffering
-            {
-                Value = model.Name,
-                SortOrder = model.SortOrder
-            });
+            await _serviceOfferingService.Delete(id);
 
             //Everything went A-OK!
             return Ok(model);
         }
 
+        /// <summary>
+        /// Move an existing Service Offering.
+        /// </summary>
+        /// <param name="id">The Id of the model to move.</param>
+        /// <param name="model">The model with updated values.</param>
+        /// <returns>The moved model.</returns>
         [HttpPost("{id}/move")]
-        [AllowAnonymous]
+        [SwaggerResponse(200, Type = typeof(ServiceOffering))]
+        [SwaggerResponse(400, "Invalid request.")]
         public async Task<ActionResult> Move(int id, ServiceOfferingModel model)
         {
             await _serviceOfferingService.Update(new ServiceOffering
