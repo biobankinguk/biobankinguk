eaf283bcd6d0dc384a30032ef04961ca88c50f7c
Andy Rae <andy.rae@nottingham.ac.uk>
2022-10-13 13:31:15 +0100

    Mark old obsolete

--- src/Directory/Biobanks.Web/ApiControllers/RegistrationReasonController.cs
+++ src/Directory/Biobanks.Web/ApiControllers/RegistrationReasonController.cs
@@ -10,6 +10,9 @@ using Biobanks.Directory.Services.Contracts;
 
 namespace Biobanks.Web.ApiControllers
 {
+    [Obsolete("To be deleted when the Directory core version goes live." +
+    " Any changes made here will need to be made in the corresponding core version"
+    , false)]
     [UserApiAuthorize(Roles = "ADAC")]
     [RoutePrefix("api/RegistrationReason")]
     public class RegistrationReasonController : ApiBaseController
