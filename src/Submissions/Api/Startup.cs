e737336d8c362cda00fc3c13adc790a44035b186
MoizIqbal97 <90258816+MoizIqbal97@users.noreply.github.com>
2022-10-14 15:41:07 +0100

    AB#78740: Port over CollectionType Controller  (#444)
    port over

--- src/Submissions/Api/Startup.cs
+++ src/Submissions/Api/Startup.cs
@@ -248,6 +248,7 @@ namespace Biobanks.Submissions.Api
                 .AddTransient<Services.Directory.Contracts.IReferenceDataService<AnnualStatistic>, AnnualStatisticService>()
                 .AddTransient<Services.Directory.Contracts.IReferenceDataService<AnnualStatisticGroup>, AnnualStatisticGroupService>()
                 .AddTransient<Services.Directory.Contracts.IReferenceDataService<AssociatedDataProcurementTimeframe>, AssociatedDataProcurementTimeframeService>()
+                .AddTransient<Services.Directory.Contracts.IReferenceDataService<CollectionType>, CollectionTypeService>()
                 .AddTransient<Services.Directory.Contracts.IReferenceDataService<CollectionStatus>, CollectionStatusService>()
                 .AddTransient<Services.Directory.Contracts.IReferenceDataService<AssociatedDataTypeGroup>, AssociatedDataTypeGroupService>()
                 .AddTransient<Services.Directory.Contracts.IReferenceDataService<DonorCount>, DonorCountService>()
@@ -283,7 +284,6 @@ namespace Biobanks.Submissions.Api
                 // Reference Data
                 services
                     .AddTransient<Services.Directory.Contracts.IReferenceDataService<AssociatedDataType>, AssociatedDataTypeService>()
-                    .AddTransient<Services.Directory.Contracts.IReferenceDataService<CollectionType>, CollectionTypeService>()
                     .AddTransient<Services.Directory.Contracts.IReferenceDataService<ConsentRestriction>, ConsentRestrictionService>()
                     .AddTransient<Services.Directory.Contracts.IReferenceDataService<Country>, CountryService>()
                     .AddTransient<Services.Directory.Contracts.IReferenceDataService<County>, CountyService>()
