e8338cb31ed9e80598a75f08faa37a9b6b6c7a3f
Andy Rae <andy.rae@nottingham.ac.uk>
2022-10-20 12:02:49 +0100

    Add email models and config

---
+++ src/Submissions/Api/Config/EmailSenderOptions.cs
@@ -0,0 +1,27 @@
+namespace Biobanks.Submissions.Api.Config
+{
+    public record BaseEmailSenderOptions
+    {
+        public BaseEmailSenderOptions()
+        {
+            if (string.IsNullOrWhiteSpace(ReplyToAddress))
+                ReplyToAddress = FromAddress;
+        }
+
+        public string ServiceName { get; init; } = "Biobanks";
+        public string FromName { get; init; } = "No Reply";
+        public string FromAddress { get; init; } = "noreply@example.com";
+        public string ReplyToAddress { get; init; } = string.Empty;
+    };
+
+    public record LocalDiskEmailOptions : BaseEmailSenderOptions
+    {
+        public string LocalPath { get; init; } = "/temp";
+    }
+
+    public record SendGridOptions : BaseEmailSenderOptions
+    {
+        public string SendGridApiKey { get; init; } = string.Empty;
+    }
+    
+}
---
+++ src/Submissions/Api/Models/Emails/EmailAddress.cs
@@ -0,0 +1,7 @@
+namespace Biobanks.Submissions.Api.Models.Emails
+{
+    public record EmailAddress(string Address)
+    {
+        public string? Name { get; init; }
+    }
+}
