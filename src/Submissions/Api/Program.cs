843a17ebe000885492015d1715e12fb004044abb
Moiz <moiziqbal5021@gmail.com>
2022-10-24 10:39:55 +0100

    fixed warning and removed comment

--- src/Submissions/Api/Program.cs
+++ src/Submissions/Api/Program.cs
@@ -161,7 +161,6 @@ builder.Services.AddSwaggerGen(opts =>
     });
     opts.DocInclusionPredicate((name, api) => true);
 
-    // using System.Reflection;
     var xmlFilename = builder.Configuration["Swagger:Filename"];
     opts.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
