4887c8117acfdd04b9ff1d336785f53063171f52
Andy Rae <andy.rae@nottingham.ac.uk>
2022-10-31 13:55:01 +0000

    Port error controller

---
+++ src/Submissions/Api/Controllers/Directory/ErrorController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biobanks.Submissions.Api.Controllers.Directory;
+
+public class ErrorController : Controller
+{
+    // GET: Error
+    public ActionResult CSRFToken()
+    {
+        return View();
+    }
+
+    public ActionResult Throw()
+    {
+        return View("Error");
+    }
+
+    public ActionResult ThrowGlobal()
+    {
+        return View("GlobalErrors");
+    }
+
+    public ActionResult ThrowStatic()
+    {
+        return new PhysicalFileResult("~/Error/error.html", "text/html");
+    }
+}
