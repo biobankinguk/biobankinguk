b92e55bd323896705406fbefa21983788324d2b8
Moiz <moiziqbal5021@gmail.com>
2022-11-08 15:22:17 +0000

    markdown helper

---
+++ src/Submissions/Api/HtmlHelpers/MarkdownHelper.cs
@@ -0,0 +1,24 @@
+﻿using MarkdownDeep;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Biobanks.Submissions.Api.HtmlHelpers
+{
+    public static class MarkdownHelper
+    {
+        public static HtmlString Markdown(this IHtmlHelper helper, string markdown)
+        {
+            var markdownTransformed = new Markdown
+            {
+                ExtraMode = true,
+                MarkdownInHtml = true
+                //SafeMode = true
+            }.Transform(markdown);
+
+            return new HtmlString(
+                markdownTransformed
+             );
+
+        }
+    }
+}
