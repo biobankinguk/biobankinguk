4cc033b1a9d22089f778093b8441e4b192a857b8
Andy Rae <andy.rae@nottingham.ac.uk>
2022-11-10 16:30:54 +0000

    Mark controller deprecated

--- src/Directory/Biobanks.Web/Controllers/VersionController.cs
+++ src/Directory/Biobanks.Web/Controllers/VersionController.cs
@@ -7,6 +7,9 @@ using System.Web.Mvc;
 
 namespace Biobanks.Web.Controllers
 {
+    [Obsolete("To be deleted when the Directory core version goes live." +
+              " Any changes made here will need to be made in the corresponding core version"
+        , false)]
     [AllowAnonymous]
     public class VersionController : Controller
     {
