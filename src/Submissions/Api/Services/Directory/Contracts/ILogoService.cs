01f9a2f6b5f8d1b4a7fd2c4c286a56698255f9fa
Moiz <moiziqbal5021@gmail.com>
2022-11-10 14:56:23 +0000

    extract getlogoblob from read service

---
+++ src/Submissions/Api/Services/Directory/Contracts/ILogoService.cs
@@ -0,0 +1,10 @@
+﻿using Biobanks.Entities.Data;
+using System.Threading.Tasks;
+
+namespace Biobanks.Submissions.Api.Services.Directory.Contracts
+{
+    public interface ILogoService
+    {
+        Task<Blob> GetLogoBlobAsync(string logoName);
+    }
+}
