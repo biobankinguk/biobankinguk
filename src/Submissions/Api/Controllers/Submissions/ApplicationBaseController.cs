7a9eb76926ed6db52b18d42599158eea90bbd113
Andy Rae <andy.rae@nottingham.ac.uk>
2022-11-10 16:32:18 +0000

    Remove pagestrings

--- src/Submissions/Api/Controllers/Submissions/ApplicationBaseController.cs
+++ src/Submissions/Api/Controllers/Submissions/ApplicationBaseController.cs
@@ -11,19 +11,6 @@ namespace Biobanks.Submissions.Api.Controllers.Submissions
         // TODO: Update or remove the user model.
         // public ApplicationUserPrincipal CurrentUser => User.ToApplicationUserPrincipal();
         
-        public Dictionary<string, string> Config => GetSiteConfig();
-
-        /// <summary>
-        /// Gets the Page Strings config.
-        /// </summary>
-        /// <returns>A dictionary of the config keys and values.</returns>
-        protected Dictionary<string, string> GetSiteConfig()
-        {
-            var config = PageStrings.DefaultConfigs;
-
-            return config.ToDictionary(x => x.Key, x => x.Value);
-        }
-
         protected void SetTemporaryFeedbackMessage(string message, FeedbackMessageType type, bool containsHtml = false)
             => TempData[ViewConstants.FeedbackMessageKey] = new FeedbackMessage
             {
