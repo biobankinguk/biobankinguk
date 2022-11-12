4cda76c804222f1ffdeff0d75dca083805c656b0
Jon Couldridge <jcouldridge@outlook.com>
2022-11-12 10:30:14 +0000

    adding more configkeys and siteproperties for views

--- src/Submissions/Api/Config/ConfigKey.cs
+++ src/Submissions/Api/Config/ConfigKey.cs
@@ -1,35 +1,39 @@
-﻿using System;
 namespace Biobanks.Submissions.Api.Config
 {
-    public static class ConfigKey
-    {
-        // Ref Data Name Configuration Options
-        public const string MacroscopicAssessmentName = "site.display.macroscopicassessment.name";
-        public const string DonorCountName = "site.display.donorcount.name";
-        public const string StorageTemperatureName = "site.display.storagetemperature.name";
-        public const string ShowPreservationPercentage = "true";
+  public static class ConfigKey
+  {
+    // General Site Display Properties
+    public const string DisplayLiveChat = "site.display.livechat";
+
+    // Ref Data Name Configuration Options
+    public const string MacroscopicAssessmentName = "site.display.macroscopicassessment.name";
+    public const string DonorCountName = "site.display.donorcount.name";
+    public const string StorageTemperatureName = "site.display.storagetemperature.name";
+    public const string ShowPreservationPercentage = "true";
 
 
-        // Editable Homepage
-        public const string HomepageTitle = "site.homepage.title";
-        public const string HomepageSearchTitle = "site.homepage.searchtitle";
-        public const string HomepageSearchSubTitle = "site.homepage.searchsubtitle";
-        public const string HomepageResourceRegistration = "site.homepage.resourceregistration";
-        public const string HomepageResourceRegistration2 = "site.homepage.resourceregistration2";
-        public const string HomepageNetworkRegistration = "site.homepage.networkregistration";
-        public const string HomepageNetworkRegistration2 = "site.homepage.networkregistration2";
-        public const string HomepageSearchRadioSamplesCollected = "site.homepage.searchradiosamplescollected";
-        public const string HomepageSearchRadioAccessSamples = "site.homepage.searchradioaccesssamples";
-        public const string HomepageFinalParagraph = "site.homepage.finalparagraph";
-
-        public const string RegisterBiobankTitle = "site.register.biobank.title";
-        public const string RegisterNetworkTitle = "site.register.network.title";
+    // Editable Homepage
+    public const string HomepageTitle = "site.homepage.title";
+    public const string HomepageSearchTitle = "site.homepage.searchtitle";
+    public const string HomepageSearchSubTitle = "site.homepage.searchsubtitle";
+    public const string HomepageResourceRegistration = "site.homepage.resourceregistration";
+    public const string HomepageResourceRegistration2 = "site.homepage.resourceregistration2";
+    public const string HomepageNetworkRegistration = "site.homepage.networkregistration";
+    public const string HomepageNetworkRegistration2 = "site.homepage.networkregistration2";
+    public const string HomepageSearchRadioSamplesCollected = "site.homepage.searchradiosamplescollected";
+    public const string HomepageSearchRadioAccessSamples = "site.homepage.searchradioaccesssamples";
+    public const string HomepageFinalParagraph = "site.homepage.finalparagraph";
+    public const string RegisterBiobankTitle = "site.register.biobank.title";
+    public const string RegisterNetworkTitle = "site.register.network.title";
         
-        //Editable Termspage
-        public const string TermpageInfo = "site.termpage.pageinfo";
+    //Editable Termspage
+    public const string TermpageInfo = "site.termpage.pageinfo";
 
-        // Sample Resource Configuration Options
-        public const string SampleResourceName = "site.sampleresource.name";
-    }
+    public const string RegisterBiobankTitle = "site.register.biobank.title";
+    public const string RegisterNetworkTitle = "site.register.network.title";
+
+    // Sample Resource Configuration Options
+    public const string SampleResourceName = "site.sampleresource.name";
+  }
 }
 
