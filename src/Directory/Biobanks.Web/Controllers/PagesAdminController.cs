048078274bea5e9260bd1477e1fced68786484de
Moiz <moiziqbal5021@gmail.com>
2022-11-14 16:20:21 +0000

    mark old controller as obsolete

--- src/Directory/Biobanks.Web/Controllers/PagesAdminController.cs
+++ src/Directory/Biobanks.Web/Controllers/PagesAdminController.cs
@@ -3,12 +3,16 @@ using Biobanks.Web.Filters;
 using Biobanks.Web.Models.ADAC;
 using Biobanks.Web.Utilities;
 using MvcSiteMapProvider.Web.Mvc.Filters;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace Biobanks.Web.Controllers
 {
+    [Obsolete("To be deleted when the Directory core version goes live." +
+    " Any changes made here will need to be made in the corresponding core version"
+    , false)]
     [UserAuthorize(Roles = "ADAC")]
     public class PagesAdminController : ApplicationBaseController
     {
