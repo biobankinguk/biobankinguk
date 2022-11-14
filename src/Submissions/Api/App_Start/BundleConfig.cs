64789a857d00b6f0b1515cdd55856fb6074a3d94
Moiz <moiziqbal5021@gmail.com>
2022-11-14 15:54:09 +0000

    added scripts

---
+++ src/Submissions/Api/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+
+namespace Biobanks.Submissions.Api.App_Start
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/pagesadmin").Include(
+                "~/Scripts/Directory/directory-pages-admin.js",
+                "~/Scripts/bootbox*",
+                "~/Scripts/Directory/directory-refdata-utility.js"));
+        }
+    }
+}
