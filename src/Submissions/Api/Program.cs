40bcbf345862d77ac79255cc16b7a89adc8958cb
Moiz <moiziqbal5021@gmail.com>
2022-12-01 16:47:53 +0000

    areas folder structure

--- src/Submissions/Api/Program.cs
+++ src/Submissions/Api/Program.cs
@@ -412,6 +412,11 @@ else
 app.UseHangfireDashboard();
 
 app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "MyArea",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
