54cde25669cd5eb6b7af635a035cd2fbb7eaa40f
Andy Rae <github@rae.li>
2022-12-06 12:10:09 +0000

    AB#84262: Registration domain rules controller (#489)
    * Account controller from directory with some core replacements and claims/roles
    
    * Model and views. Helper/service classes too, but some of these will most likely be replaced by default core identity behaviour.
    
    * Fixed up more of the account controller
    
    * Replaced custom last login date calls with middleware in program.
    
    * Added todo for fix
    
    * Remove last login update
    
    * Auto stash before merge of "NetCoreIdentity" and "main"
    
    * Auto stash before merge of "NetCoreIdentity" and "main"
    
    Entity updates
    
    * Added in old  custom claims manager functionality into login middleware
    
    * Package update
    
    * Further account controller cleanup. Removed context switching due to change to biobank registration and routing.
    
    * Dropped directory adaptions (replace with actual identity classes)
    
    * Removed login redirect due to change in biobanking routing
    
    * Removed custom claims manager. Can hopefully be handled by middleware
    
    * Fixes for read/write service
    
    * Fixes for account controller
    
    * Regex fix
    
    * Set directory account controller back
    
    * Fixed remaining errors in services
    
    * View fixes
    
    * fix for login page.
    
    * moved controller
    
    * merge conficts
    
    * replaced read service dependency
    
    * added organisation directory service
    
    * user manager added
    
    * ported models
    
    * token log added
    
    * added views
    
    * email sender update
    
    * replaced JSON object with action result
    
    * fixed controller errors
    
    * fixed views
    
    * areas folder structure
    
    * email views added
    
    * fixed layout
    
    * moved model and fixed warning
    
    * removed method from read service
    
    * removed to and from
    
    * resolved comments
    
    * resolved comments
    
    * user register view update
    
    * replaced viewbag
    
    * Add controller
    
    * Add view
    
    * Update json serializer
    
    * Indentation
    
    * Update partial tag
    
    Co-authored-by: Phil Van Krimpen <philip.vankrimpen@nottingham.ac.uk>
    Co-authored-by: Moiz <moiziqbal5021@gmail.com>

---
+++ src/Submissions/Api/Areas/Admin/Controllers/RegistrationDomainRulesController.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Biobanks.Submissions.Api.Areas.Admin.Models;
+using Biobanks.Submissions.Api.Models.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Biobanks.Submissions.Api.Services.Directory;
+
+namespace Biobanks.Submissions.Api.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class RegistrationDomainRulesController : Controller
+{
+  private readonly RegistrationDomainService _registrationDomainService;
+  
+  public RegistrationDomainRulesController(RegistrationDomainService registrationDomainService)
+  {
+    _registrationDomainService = registrationDomainService;
+  }
+  
+  public async Task<ActionResult> BlockAllowList()
+  {
+    var rules = (await _registrationDomainService.ListRules())
+      .Select(x => new RegistrationDomainRuleModel
+      {
+        Id = x.Id,
+        DateModified = x.DateModified,
+        RuleType = x.RuleType,
+        Source = x.Source,
+        Value = x.Value
+
+      })
+      .ToList();
+
+    return View(new BlockAllowListModel
+    {
+      RegistrationDomainRules = rules
+    });
+  }
+  
+}
