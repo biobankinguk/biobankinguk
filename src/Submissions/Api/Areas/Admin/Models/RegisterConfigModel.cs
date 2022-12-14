5e24c9115e591cfee9caec1dd3382381dbd8a218
Andy Rae <github@rae.li>
2022-12-14 10:09:52 +0000

    AB#84285: SiteConfigController port (#491)
    * Add site config controller
    
    * Add config keys
    
    * Updated config controller
    
    * Add views
    
    * Update views
    
    * Update configservice
    
    * Empty commit for actions...

---
+++ src/Submissions/Api/Areas/Admin/Models/RegisterConfigModel.cs
@@ -0,0 +1,13 @@
+namespace Biobanks.Submissions.Api.Areas.Admin.Models;
+
+public class RegisterConfigModel
+{
+    public string BiobankTitle { get; set; }
+    public string BiobankDescription { get; set; }
+    public string NetworkTitle { get; set; }
+    public string NetworkDescription { get; set; }
+    public string EnableRegistrationHelpUrl { get; set; }
+    public string RegistrationHelpUrl { get; set; }
+    public string RegistrationEmails { set; get; }
+    
+}
