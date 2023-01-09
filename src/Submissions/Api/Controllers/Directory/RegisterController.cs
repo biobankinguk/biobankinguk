fd254c74d2ca81f765141054b31570c47565acc0
Andy Rae <hello@rae.li>
2023-01-09 14:19:44 +0000

    Add initial controller

---
+++ src/Submissions/Api/Controllers/Directory/RegisterController.cs
@@ -0,0 +1,263 @@
+using System;
+using System.Threading.Tasks;
+using Biobanks.Entities.Data;
+using Biobanks.Submissions.Api.Config;
+using Biobanks.Submissions.Api.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biobanks.Submissions.Api.Controllers.Directory;
+
+public class RegisterController : Controller
+{
+  public RegisterController()
+  {
+  }
+  
+  // GET: Register
+  //public ActionResult Index() //We can use this later when ordinary people can register plain user accounts for themselves
+  //{
+  //    return View();
+  //}
+
+  //Register a biobank
+  [AllowAnonymous]
+  public ActionResult Biobank()
+  {
+      return View(new RegisterEntityModel
+      {
+          EntityName = "Biobank"
+      });
+  }
+
+
+  //Register a biobank - ADAC Invite
+  [UserAuthorize(Roles = "ADAC")]
+  public ActionResult AdacBiobank()
+  {
+      return View("Biobank", new RegisterEntityModel
+      {
+          EntityName = "Biobank",
+          AdacInvited = true
+      });
+  }
+
+  public async Task<bool> RegistrationHoneypotTrap(RegisterEntityModel model)
+  {
+      //check if honeypot field is true
+      if (model.AcceptTerms && !model.AdacInvited)
+      {
+          //check if domain rule exist for user
+          var rule = await _registrationDomainService.GetRuleByValue(model.Email);
+
+          if (rule != null)
+          {
+              //update rule to block user
+              rule.RuleType = "Block";
+              rule.DateModified = DateTime.Now;
+              rule.Source = "Automatic block: honeypot";
+              await _registrationDomainService.Update(rule);
+          }
+          else
+          {
+              // add new rule to block user
+              await _registrationDomainService.Add(new RegistrationDomainRule
+              {
+                  RuleType = "Block",
+                  Source = "Automatic block: honeypot",
+                  Value = model.Email,
+                  DateModified = DateTime.Now
+              });
+          }
+
+          return true;
+      }
+
+      return false;
+  }
+
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  [AllowAnonymous]
+  [VerifyRecaptcha]
+  public async Task<ActionResult> Biobank(RegisterEntityModel model)
+  {
+      model.EntityName = "Biobank";
+
+      if (!ModelState.IsValid)
+      {
+          return View(model);
+      }
+
+      //check for honeypot trap
+      if (await RegistrationHoneypotTrap(model))
+          return View("RegisterConfirmation");
+
+      //check for duplicate Biobank name
+      var existingOrg = await _organisationService.GetByName(model.Entity);
+
+      if (existingOrg != null)
+      {
+          SetTemporaryFeedbackMessage($"{model.Entity} already exists. Please contact {existingOrg.ContactEmail} and ask them to add you as an admin.", FeedbackMessageType.Danger);
+
+          return View(model);
+      }
+
+      if (Uri.IsWellFormedUriString(model.Name, UriKind.Absolute) || Uri.IsWellFormedUriString(model.Entity, UriKind.Absolute))
+      {
+          SetTemporaryFeedbackMessage("The admin name or organisation name fields cannot be URIs. Please enter a non URI value.", FeedbackMessageType.Danger);
+          return View(model);
+      }
+
+      //check for duplicate name against non-declined requests too
+      if (await _organisationService.RegistrationRequestExists(model.Entity))
+      {
+          var supportEmail = ConfigurationManager.AppSettings["AdacSupportEmail"];
+          SetTemporaryFeedbackMessage(
+              $"Registration is already in progress for {model.Entity}. If you think this is in error please contact <a href=\"mailto:{supportEmail}\">{supportEmail}</a>.",
+              FeedbackMessageType.Danger, true);
+
+          return View(model);
+      }
+
+      //Check if email is on the allow/block list
+      if (!await _registrationDomainService.ValidateEmail(model.Email) && !model.AdacInvited)
+      {
+          var supportEmail = ConfigurationManager.AppSettings["AdacSupportEmail"];
+          SetTemporaryFeedbackMessage(
+              $"Sorry, registrations from this email domain are not allowed. If you think this is in error please contact <a href=\"mailto:{supportEmail}\">{supportEmail}</a>.",
+              FeedbackMessageType.Danger, true);
+
+          return View(model);
+      }
+
+      //Create a register request for this user and Biobank
+      var request = await _organisationService.AddRegistrationRequest(
+          new OrganisationRegisterRequest
+          {
+              UserName = model.Name,
+              UserEmail = model.Email,
+              OrganisationName = model.Entity,
+              RequestDate = DateTime.Now
+          });
+
+      if (model.AdacInvited)
+      {
+          //ADAC Invited requests should be automatically accepted, and return to the ADAC Admin Requests view
+          return RedirectToAction("AcceptBiobankRequest", "ADAC", new { requestId = request.OrganisationRegisterRequestId });
+      }
+      else
+      {
+          if (await _configService.GetFlagConfigValue(ConfigKey.RegistrationEmails) == true)
+          {
+              // Non ADAC Invited requests should notify ADAC users
+              await _emailService.SendDirectoryAdminNewRegisterRequestNotification(
+                  model.Name,
+                  model.Email,
+                  model.Entity,
+                  model.EntityName);
+          }
+      }
+
+      return View("RegisterConfirmation");
+  }
+
+  //Register a Network
+  [AllowAnonymous]
+  public ActionResult Network()
+  {
+      return View(new RegisterEntityModel
+      {
+          EntityName = "Network"
+      });
+  }
+
+  //Register a biobank - ADAC Invite
+  [UserAuthorize(Roles = "ADAC")]
+  public ActionResult AdacNetwork()
+  {
+      return View("Network", new RegisterEntityModel
+      {
+          EntityName = "Network",
+          AdacInvited = true
+      });
+  }
+
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  [AllowAnonymous]
+  [VerifyRecaptcha]
+  public async Task<ActionResult> Network(RegisterEntityModel model)
+  {
+      ViewBag.RegisterEntityName = "network";
+
+      if (!ModelState.IsValid)
+      {
+          return View(model);
+      }
+
+      //check for honeypot trap
+      if (await RegistrationHoneypotTrap(model))
+          return View("RegisterConfirmation");
+
+      //check for duplicate Network name
+      var existingNetwork = await _networkService.GetByName(model.Entity);
+
+      if (existingNetwork != null)
+      {
+          SetTemporaryFeedbackMessage($"{model.Entity} already exists. Please contact {existingNetwork.Email} and ask them to add you as an admin.", FeedbackMessageType.Danger);
+          return View(model);
+      }
+
+      //check for duplicate name against non-declined requests too
+      if (await _networkService.HasActiveRegistrationRequest(model.Entity))
+      {
+          var supportEmail = ConfigurationManager.AppSettings["AdacSupportEmail"];
+          SetTemporaryFeedbackMessage($"Registration is already in progress for {model.Entity}. If you think this is in error please contact {supportEmail}.", FeedbackMessageType.Danger);
+
+          return View(model);
+      }
+
+      //Check if email is on the allow/block list
+      if (!await _registrationDomainService.ValidateEmail(model.Email) && !model.AdacInvited)
+      {
+          var supportEmail = ConfigurationManager.AppSettings["AdacSupportEmail"];
+          SetTemporaryFeedbackMessage(
+              $"Sorry, registrations from this email domain are not allowed. If you think this is in error please contact <a href=\"mailto:{supportEmail}\">{supportEmail}</a>.",
+              FeedbackMessageType.Danger, true);
+
+          return View(model);
+      }
+
+      //Create a register request for this user and Network
+      var request = await _networkService.AddRegistrationRequest(
+          new NetworkRegisterRequest
+          {
+              UserName = model.Name,
+              UserEmail = model.Email,
+              NetworkName = model.Entity,
+              RequestDate = DateTime.Now
+          });
+
+      if (model.AdacInvited)
+      {
+          return RedirectToAction("AcceptNetworkRequest", "ADAC",
+              new { requestId = request.NetworkRegisterRequestId });
+      }
+      else
+      {
+          if (await _configService.GetFlagConfigValue(ConfigKey.RegistrationEmails) == true)
+          {
+              // Non ADAC Invited requests should notify ADAC users
+              await _emailService.SendDirectoryAdminNewRegisterRequestNotification(
+              model.Name,
+              model.Email,
+              model.Entity,
+              model.EntityName);
+          }
+      }
+
+      return View("RegisterConfirmation");
+  }
+
+}
