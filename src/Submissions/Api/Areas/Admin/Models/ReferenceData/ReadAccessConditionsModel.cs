97b9921b86e986f4b64585f5023d50dd9b17f5bd
Andy Rae <hello@rae.li>
2023-01-24 10:16:21 +0000

    Fix namespace changes

--- src/Submissions/Api/Areas/Admin/Models/ReferenceData/ReadAccessConditionsModel.cs
+++ src/Submissions/Api/Areas/Admin/Models/ReferenceData/ReadAccessConditionsModel.cs
@@ -1,6 +1,8 @@
+using Biobanks.Submissions.Api.Models.Shared;
+
 namespace Biobanks.Submissions.Api.Areas.Admin.Models.ReferenceData;
 
-public class ReadAccessConditionsModel : Biobanks.Submissions.Api.Models.Shared.AccessConditionModel
+public class ReadAccessConditionsModel : AccessConditionModel
 {
   //Sum of all Collections and Capabilities
   public int AccessConditionCount { get; set; }
