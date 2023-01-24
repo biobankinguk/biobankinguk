86ba1eb5c15e6abe763593c322df58cfdee7ba11
Andy Rae <hello@rae.li>
2023-01-24 10:15:15 +0000

    Resolve conflicts

--- src/Submissions/Api/Controllers/ReferenceData/AccessConditionController.cs
+++ src/Submissions/Api/Controllers/ReferenceData/AccessConditionController.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 namespace Biobanks.Submissions.Api.Controllers.ReferenceData
 {
 
+    [AllowAnonymous]
     [Route("api/[controller]")]
     [ApiController]
     [ApiExplorerSettings(GroupName = "Reference Data")]
@@ -58,6 +59,7 @@ namespace Biobanks.Submissions.Api.Controllers.ReferenceData
         [SwaggerResponse(200, Type = typeof(AccessConditionModel))]
         [SwaggerResponse(400, "Invalid request body.")]
         public async Task<ActionResult> Post(AccessConditionModel model)
+        // public async Task<ActionResult> Post([FromForm] AccessConditionModel model)
         {
             //If this description is valid, it already exists
             if (await _accessConditionService.Exists(model.Description))
