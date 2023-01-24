432faa6517a88117b91d4446f4f962274ddd498c
Jon Couldridge <jcouldridge@outlook.com>
2023-01-24 08:47:58 +0000

    fixing compiler warnings

--- src/Submissions/Api/Commands/Helpers/ConfigureCommandServices.cs
+++ src/Submissions/Api/Commands/Helpers/ConfigureCommandServices.cs
@@ -12,6 +12,7 @@ public static class ConfigureCommandServices
   /// 
   /// Approximately similar interface to the standard .NET Hosting Model's ConfigureServices()
   /// </summary>
+  /// <param name="_"></param>
   /// <param name="c">The invocation context from withina Command Handler</param>
   /// <param name="configure">The configuration action</param>
   /// <returns></returns>
@@ -21,12 +22,13 @@ public static class ConfigureCommandServices
     configure.Invoke(c, s);
     return s.BuildServiceProvider();
   }
-  
+
   /// <summary>
   /// Specify DI Services configuration then build a service provider from that configuration.
-  ///
+  /// 
   /// Approximately similar interface to the standard .NET Hosting Model's ConfigureServices()
   /// </summary>
+  /// <param name="_"></param>
   /// <param name="configure">The configuration action</param>
   /// <returns></returns>
   public static IServiceProvider ConfigureServices(this Command _, Action<IServiceCollection> configure)
