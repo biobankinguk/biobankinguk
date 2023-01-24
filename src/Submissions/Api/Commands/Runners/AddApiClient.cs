9c86c71f94be5e3ace66653d8805f060133ddaef
Jon Couldridge <jcouldridge@outlook.com>
2023-01-24 11:48:33 +0000

    namespace tidy

--- src/Submissions/Api/Commands/Runners/AddApiClient.cs
+++ src/Submissions/Api/Commands/Runners/AddApiClient.cs
@@ -11,114 +11,113 @@ using ConsoleTableExt;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
-namespace Biobanks.Submissions.Api.Commands.Runners
-{
-  internal class AddApiClient
-  {
-    private readonly ILogger<AddApiClient> _logger;
-    private readonly IConsole _console;
-    private readonly ApplicationDbContext _db;
+namespace Biobanks.Submissions.Api.Commands.Runners;
 
-    public AddApiClient(
-      ILoggerFactory logger,
-      IConsole console,
-      ApplicationDbContext db)
+internal class AddApiClient
+{
+  private readonly ILogger<AddApiClient> _logger;
+  private readonly IConsole _console;
+  private readonly ApplicationDbContext _db;
+
+  public AddApiClient(
+    ILoggerFactory logger,
+    IConsole console,
+    ApplicationDbContext db)
+  {
+    _logger = logger.CreateLogger<AddApiClient>();
+    _console = console;
+    _db = db;
+  }
+
+  public async Task Run(bool generate, List<int> biobankIds, string clientId, string clientSecret, string clientName)
+  {
+    #region conditional / dependent argument validation
+
+    if (generate)
     {
-      _logger = logger.CreateLogger<AddApiClient>();
-      _console = console;
-      _db = db;
+      if (!string.IsNullOrEmpty(clientId) || !string.IsNullOrEmpty(clientSecret))
+      {
+        const string message = "The `--generate` option is exclusive from `--client-id` and `--client-secret`.";
+        _logger.LogError(message);
+        _console.Error.WriteLine($"Error: {message}");
+        return;
+      }
+
+      clientId = Crypto.GenerateId();
+      clientSecret = Crypto.GenerateId();
     }
 
-    public async Task Run(bool generate, List<int> biobankIds, string clientId, string clientSecret, string clientName)
+    if (string.IsNullOrEmpty(clientId))
     {
-      #region conditional / dependent argument validation
+      const string message = "One of `--client-id` or `--generate` must be specified.";
+      _logger.LogError(message);
+      _console.Error.WriteLine($"Error: {message}");
+      return;
+    }
 
-      if (generate)
+    if (string.IsNullOrEmpty(clientSecret))
+    {
+      const string message = "One of `--client-secret` or `--generate` must be specified.";
+      _logger.LogError(message);
+      _console.Error.WriteLine($"Error: {message}");
+      return;
+    }
+
+    // Confirm before adding SuperAdmin
+    if (!biobankIds.Any())
+    {
+      var proceed = new Prompt(_console)
+        .YesNo("No Biobank Ids were specified. This will create SuperAdmin credentials. Is this correct?");
+      if (!proceed) return;
+    }
+
+    #endregion
+
+    var apiClient = new ApiClient
+    {
+      Name = clientName ?? clientId,
+      ClientId = clientId,
+      ClientSecretHash = clientSecret.Sha256(),
+      Organisations = new List<Entities.Data.Organisation>()
+    };
+
+    foreach (var id in biobankIds)
+    {
+      var biobank = await _db.Organisations
+        .Include(o => o.ApiClients)
+        .SingleOrDefaultAsync(x => x.OrganisationId == id);
+      if (biobank is null)
       {
-        if (!string.IsNullOrEmpty(clientId) || !string.IsNullOrEmpty(clientSecret))
-        {
-          const string message = "The `--generate` option is exclusive from `--client-id` and `--client-secret`.";
-          _logger.LogError(message);
-          _console.Error.WriteLine($"Error: {message}");
-          return;
-        }
-
-        clientId = Crypto.GenerateId();
-        clientSecret = Crypto.GenerateId();
-      }
-
-      if (string.IsNullOrEmpty(clientId))
-      {
-        const string message = "One of `--client-id` or `--generate` must be specified.";
-        _logger.LogError(message);
-        _console.Error.WriteLine($"Error: {message}");
+        const string message = "Could not find the specified Biobank with ID: {0}";
+        _logger.LogError(message, id);
+        _console.Error.WriteLine(string.Format($"Error: {message}", id));
         return;
       }
 
-      if (string.IsNullOrEmpty(clientSecret))
-      {
-        const string message = "One of `--client-secret` or `--generate` must be specified.";
-        _logger.LogError(message);
-        _console.Error.WriteLine($"Error: {message}");
-        return;
-      }
+      apiClient.Organisations.Add(biobank);
+    }
 
-      // Confirm before adding SuperAdmin
-      if (!biobankIds.Any())
-      {
-        var proceed = new Prompt(_console)
-          .YesNo("No Biobank Ids were specified. This will create SuperAdmin credentials. Is this correct?");
-        if (!proceed) return;
-      }
+    await _db.ApiClients.AddAsync(apiClient);
+    await _db.SaveChangesAsync();
 
-      #endregion
+    // Output
+    const string successMessage = "Client `{0}` added successfully to Biobank Ids: {1}";
+    _logger.LogInformation(successMessage, clientName ?? clientId, biobankIds);
+    _console.Out.WriteLine(string.Format(successMessage, clientName ?? clientId, biobankIds));
 
-      var apiClient = new ApiClient
+    if (generate)
+    {
+      var outputRows = new List<List<object>>
       {
-        Name = clientName ?? clientId,
-        ClientId = clientId,
-        ClientSecretHash = clientSecret.Sha256(),
-        Organisations = new List<Entities.Data.Organisation>()
+        new() { "Client ID", clientId },
+        new() { "Client Secret", clientSecret }
       };
 
-      foreach (var id in biobankIds)
-      {
-        var biobank = await _db.Organisations
-          .Include(o => o.ApiClients)
-          .SingleOrDefaultAsync(x => x.OrganisationId == id);
-        if (biobank is null)
-        {
-          const string message = "Could not find the specified Biobank with ID: {0}";
-          _logger.LogError(message, id);
-          _console.Error.WriteLine(string.Format($"Error: {message}", id));
-          return;
-        }
-
-        apiClient.Organisations.Add(biobank);
-      }
-
-      await _db.ApiClients.AddAsync(apiClient);
-      await _db.SaveChangesAsync();
-
-      // Output
-      const string successMessage = "Client `{0}` added successfully to Biobank Ids: {1}";
-      _logger.LogInformation(successMessage, clientName ?? clientId, biobankIds);
-      _console.Out.WriteLine(string.Format(successMessage, clientName ?? clientId, biobankIds));
-
-      if (generate)
-      {
-        var outputRows = new List<List<object>>
-        {
-          new() { "Client ID", clientId },
-          new() { "Client Secret", clientSecret }
-        };
-
-        _console.Out.Write(ConsoleTableBuilder
-          .From(outputRows)
-          .WithCharMapDefinition(CharMapDefinition.FramePipDefinition)
-          .Export()
-          .ToString());
-      }
+      _console.Out.Write(ConsoleTableBuilder
+        .From(outputRows)
+        .WithCharMapDefinition(CharMapDefinition.FramePipDefinition)
+        .Export()
+        .ToString());
     }
   }
 }
