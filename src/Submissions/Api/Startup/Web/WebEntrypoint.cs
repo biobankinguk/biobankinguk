eaf3f85bd64f96bc4ca8555d6ae8ec39131180af
Jon Couldridge <jcouldridge@outlook.com>
2023-01-25 12:58:46 +0000

    actually calling the initialisation in the web app

--- src/Submissions/Api/Startup/Web/WebEntrypoint.cs
+++ src/Submissions/Api/Startup/Web/WebEntrypoint.cs
@@ -386,15 +386,7 @@ public static class WebEntrypoint
 
 
     var app = builder.Build();
-
-// Set cache isolated from running of the app
-    using (var scope = app.Services.CreateScope())
-    {
-      var configCache = scope.ServiceProvider
-        .GetRequiredService<IConfigService>();
-
-      await configCache.PopulateSiteConfigCache();
-    }
+    
 
     app.GnuTerryPratchett();
 
--- src/Submissions/Api/Startup/Web/WebInitialisation.cs
+++ src/Submissions/Api/Startup/Web/WebInitialisation.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Biobanks.Data;
 using Biobanks.Submissions.Api.Services.DataSeeding;
+using Biobanks.Submissions.Api.Services.Directory;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,11 +18,20 @@ public static class WebInitialisation
   /// <param name="app"></param>
   public static async Task Initialise(this WebApplication app)
   {
-    var db = app.Services.GetRequiredService<ApplicationDbContext>();
-    var roles = app.Services.GetRequiredService<RoleManager<IdentityRole>>();
-
+    using var scope = app.Services.CreateScope();
+    
+    var db = scope.ServiceProvider
+      .GetRequiredService<ApplicationDbContext>();
+    var roles = scope.ServiceProvider
+      .GetRequiredService<RoleManager<IdentityRole>>();
+    
     // The Web app always tries to ensure fixed ref data is correct
     await new FixedRefDataSeeder(db, roles).Seed();
+    
+    // Set cache isolated from running of the app
+    var configCache = scope.ServiceProvider
+      .GetRequiredService<IConfigService>();
+    await configCache.PopulateSiteConfigCache();
   }
   
   /// <summary>
