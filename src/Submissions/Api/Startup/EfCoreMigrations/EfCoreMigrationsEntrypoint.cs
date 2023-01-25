3537675aee0bbe3ffbc502a4d1b698bce71c4e53
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2023-01-25 13:43:04 +0000

    AB#95014: Add User CLI (#517)
    * moving migrations bundle dbcontext fix to the right entrypoint
    
    * data seeding refactor, including "fixed" refdata seeding
    
    * add user sql script ported to cli
    
    * actually calling the initialisation in the web app
    
    * actually calling the initialisation in the web app for real this time

--- src/Submissions/Api/Startup/EfCoreMigrations/EfCoreMigrationsEntrypoint.cs
+++ src/Submissions/Api/Startup/EfCoreMigrations/EfCoreMigrationsEntrypoint.cs
@@ -18,11 +18,27 @@ public static class EfCoreMigrations
   public static void BootstrapDbContext(string[] args)
   {
     using var _ = Host.CreateDefaultBuilder(args)
-      .ConfigureServices((b, s) => s
-        .AddDbContext<ApplicationDbContext>(
-          o => o.UseNpgsql(
-            b.Configuration.GetConnectionString("Default"),
-            pgo => pgo.EnableRetryOnFailure())))
+      .ConfigureServices((b, s) =>
+      {
+        var connectionString =
+          b.Configuration.GetConnectionString("Default");
+
+        s.AddDbContext<ApplicationDbContext>(
+            o =>
+            {
+              // migration bundles don't like null connection strings (yet)
+              // https://github.com/dotnet/efcore/issues/26869
+              // so if no connection string is set we register without one for now.
+              // if running migrations, `--connection` should be set on the command line
+              // in real environments, connection string should be set via config
+              // all other cases will error when db access is attempted.
+              if (string.IsNullOrWhiteSpace(connectionString))
+                o.UseNpgsql();
+              else
+                o.UseNpgsql(connectionString,
+                  pgo => pgo.EnableRetryOnFailure());
+            });
+      })
       .Build();
   }
 }
--- src/Submissions/Api/Startup/Web/WebEntrypoint.cs
+++ src/Submissions/Api/Startup/Web/WebEntrypoint.cs
@@ -73,20 +73,8 @@ public static class WebEntrypoint
       options.UseNpgsql("Omop"));
 
     builder.Services.AddDbContext<ApplicationDbContext>(o =>
-    {
-      // migration bundles don't like null connection strings (yet)
-      // https://github.com/dotnet/efcore/issues/26869
-      // so if no connection string is set we register without one for now.
-      // if running migrations, `--connection` should be set on the command line
-      // in real environments, connection string should be set via config
-      // all other cases will error when db access is attempted.
-      var connectionString = builder.Configuration.GetConnectionString("Default");
-      if (string.IsNullOrWhiteSpace(connectionString))
-        o.UseNpgsql();
-      else
-        o.UseNpgsql(connectionString,
-          o => o.EnableRetryOnFailure());
-    });
+      o.UseNpgsql(connectionString,
+        pgo => pgo.EnableRetryOnFailure()));
 
 //identity
     builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -399,14 +387,7 @@ public static class WebEntrypoint
 
     var app = builder.Build();
 
-// Set cache isolated from running of the app
-    using (var scope = app.Services.CreateScope())
-    {
-      var configCache = scope.ServiceProvider
-        .GetRequiredService<IConfigService>();
-
-      await configCache.PopulateSiteConfigCache();
-    }
+    await app.Initialise();
 
     app.GnuTerryPratchett();
 
@@ -485,7 +466,7 @@ public static class WebEntrypoint
     app.MapControllerRoute(
       name: "default",
       pattern: "{controller=Home}/{action=Index}/{id?}");
-    
+
     await app.RunAsync();
   }
 }
