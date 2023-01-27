c75da14c474de059391df7237f3f78457fd46f6b
Andy Rae <hello@rae.li>
2023-01-27 11:10:51 +0000

    Fixed crud js

--- src/Submissions/Api/Frontend/js/pages/Admin/adac-service-offering.js
+++ src/Submissions/Api/Frontend/js/pages/Admin/adac-service-offering.js
@@ -1,174 +1,198 @@
 ﻿function ServiceOffering(id, name, sortOrder) {
-    this.id = ko.observable(id);
-    this.name = ko.observable(name);
-    this.sortOrder = ko.observable(sortOrder)
+  this.id = ko.observable(id);
+  this.name = ko.observable(name);
+  this.sortOrder = ko.observable(sortOrder);
 }
 function ServiceOfferingModal(id, name, sortOrder) {
-    this.modalModeAdd = "Add";
-    this.modalModeEdit = "Update";
-    this.mode = ko.observable(this.modalModeAdd);
-    this.serviceOffering = ko.observable(
-        new ServiceOffering(id, name, sortOrder)
-    );
+  this.modalModeAdd = "Add";
+  this.modalModeEdit = "Update";
+  this.mode = ko.observable(this.modalModeAdd);
+  this.serviceOffering = ko.observable(
+    new ServiceOffering(id, name, sortOrder)
+  );
 }
 function AdacServiceOfferingViewModel() {
-    var _this = this;
+  var _this = this;
 
-    this.modalId = "#service-offering-modal";
-    this.modal = new ServiceOfferingModal(0, "", 0);
-    this.dialogErrors = ko.observableArray([]);
+  this.modalId = "#service-offering-modal";
+  this.modal = new ServiceOfferingModal(0, "", 0);
+  this.dialogErrors = ko.observableArray([]);
 
-    this.showModal = function () {
-        _this.dialogErrors.removeAll(); //clear errors on a new show
-        $(_this.modalId).modal("show");
-    };
+  this.showModal = function () {
+    _this.dialogErrors.removeAll(); //clear errors on a new show
+    $(_this.modalId).modal("show");
+  };
 
-    this.hideModal = function () {
-        $(_this.modalId).modal("hide");
-    };
+  this.hideModal = function () {
+    $(_this.modalId).modal("hide");
+  };
 
-    this.openModalForAdd = function () {
-        _this.modal.mode(_this.modal.modalModeAdd);
-        _this.modal.serviceOffering(new ServiceOffering(0, "", 0));
-        _this.showModal();
-    };
+  this.openModalForAdd = function () {
+    _this.modal.mode(_this.modal.modalModeAdd);
+    _this.modal.serviceOffering(new ServiceOffering(0, "", 0));
+    _this.showModal();
+  };
 
-    this.openModalForEdit = function (_, event) {
-        _this.modal.mode(_this.modal.modalModeEdit);
+  this.openModalForEdit = function (_, event) {
+    _this.modal.mode(_this.modal.modalModeEdit);
 
-        var serviceOffering = $(event.currentTarget).data("service-offering");
-        _this.modal.serviceOffering(
-            new ServiceOffering(
-                serviceOffering.Id,
-                serviceOffering.Name,
-                serviceOffering.SortOrder
-            )
-        );
-        _this.showModal();
-    };
+    var serviceOffering = $(event.currentTarget).data("service-offering");
+    _this.modal.serviceOffering(
+      new ServiceOffering(
+        serviceOffering.id,
+        serviceOffering.name,
+        serviceOffering.sortOrder
+      )
+    );
+    _this.showModal();
+  };
 
-    this.modalSubmit = function (e) {
-        e.preventDefault();
-        var form = $(e.target); // get form as a jquery object
+  this.modalSubmit = function (e) {
+    e.preventDefault();
+    var form = $(e.target); // get form as a jquery object
 
-        // Get Action Type
-        var action = _this.modal.mode();
-        if (action == 'Add') {
-            addRefData(_this, form.data("resource-url"), form.serialize(),
-                form.data("success-redirect"), form.data("refdata-type")); // cf. adac-refdata-utility.js
-        } else if (action == 'Update') {
-            editRefData(_this, form.data("resource-url") + '/' + $(e.target.Id).val(), form.serialize(),
-                form.data("success-redirect"), form.data("refdata-type"));
-        }
-    };
+    // Get Form Data
+    var data = serializeFormData(form);
+
+    // Get Action Type
+    var action = _this.modal.mode();
+    if (action == "Add") {
+      addRefData(
+        _this,
+        form.data("resource-url"),
+        data,
+        form.data("success-redirect"),
+        form.data("refdata-type")
+      ); // cf. adac-refdata-utility.js
+    } else if (action == "Update") {
+      editRefData(
+        _this,
+        form.data("resource-url") + "/" + $(e.target.Id).val(),
+        data,
+        form.data("success-redirect"),
+        form.data("refdata-type")
+      );
+    }
+  };
 }
 
 var adacServiceOfferingVM;
 $(function () {
-    //jquery plugin to serialise checkboxes as bools
-    (function ($) {
-        $.fn.serialize = function () {
-            return $.param(this.serializeArray());
-        };
-        $.fn.serializeArray = function () {
-            var o = $.extend(
-                {
-                    checkboxesAsBools: true,
-                },
-                {}
-            );
+  //jquery plugin to serialise checkboxes as bools
+  (function ($) {
+    $.fn.serialize = function () {
+      return $.param(this.serializeArray());
+    };
+    $.fn.serializeArray = function () {
+      var o = $.extend(
+        {
+          checkboxesAsBools: true,
+        },
+        {}
+      );
 
-            var rselectTextarea = /select|textarea/i;
-            var rinput = /text|hidden|password|search/i;
-            return this.map(function () {
-                return this.elements ? $.makeArray(this.elements) : this;
-            })
-                .filter(function () {
-                    return (
-                        this.name &&
-                        !this.disabled &&
-                        (this.checked ||
-                            (o.checkboxesAsBools && this.type === "checkbox") ||
-                            rselectTextarea.test(this.nodeName) ||
-                            rinput.test(this.type))
-                    );
-                })
-                .map(function (i, elem) {
-                    var val = $(this).val();
-                    return val == null
-                        ? null
-                        : $.isArray(val)
-                            ? $.map(val, function (innerVal) { return ({ name: elem.name, value: innerVal })})
-                            : {
-                                name: elem.name,
-                                value:
-                                    o.checkboxesAsBools && this.type === "checkbox" //moar ternaries!
-                                        ? this.checked
-                                            ? "true"
-                                            : "false"
-                                        : val,
-                            };
-                })
-                .get();
-        };
-    })(jQuery);
+      var rselectTextarea = /select|textarea/i;
+      var rinput = /text|hidden|password|search/i;
+      return this.map(function () {
+        return this.elements ? $.makeArray(this.elements) : this;
+      })
+        .filter(function () {
+          return (
+            this.name &&
+            !this.disabled &&
+            (this.checked ||
+              (o.checkboxesAsBools && this.type === "checkbox") ||
+              rselectTextarea.test(this.nodeName) ||
+              rinput.test(this.type))
+          );
+        })
+        .map(function (i, elem) {
+          var val = $(this).val();
+          return val == null
+            ? null
+            : $.isArray(val)
+            ? $.map(val, function (innerVal) {
+                return { name: elem.name, value: innerVal };
+              })
+            : {
+                name: elem.name,
+                value:
+                  o.checkboxesAsBools && this.type === "checkbox" //moar ternaries!
+                    ? this.checked
+                      ? "true"
+                      : "false"
+                    : val,
+              };
+        })
+        .get();
+    };
+  })(jQuery);
 
-    $(function () {
-        var table = $("#service-offering")["DataTable"]({
-            paging: false,
-            info: false,
-            autoWidth: false,
-            rowReorder: true,
-            columnDefs: [
-                { orderable: true, "visible": false, className: 'reorder', targets: 0 }, // Column must be orderable for rowReorder
-                { orderable: false, targets: '_all' }
-            ],
-            language: {
-                search: "Filter: ",
-            },
-        });
-
-        // Re-Order Event
-        table.on('row-reorder', function (e, diff, edit) {
-
-            // Find the row that was moved
-            var triggerRow = diff.filter(function(row) {return row.node == edit.triggerRow.node()})[0];
-
-            //AJAX Update
-            $.ajax({
-                url: $(triggerRow.node).data('resource-url') +
-                    "/" + $(triggerRow.node).data('offering-id') + "/move",
-                type: 'POST',
-                dataType: 'json',
-                data: {
-                    id: $(triggerRow.node).data('offering-id'),
-                    name: $(triggerRow.node).data('offering-name'),
-                    sortOrder: (triggerRow.newPosition + 1) //1-indexable
-                }
-            });
-        });
+  $(function () {
+    var table = $("#service-offering")["DataTable"]({
+      paging: false,
+      info: false,
+      autoWidth: false,
+      rowReorder: true,
+      columnDefs: [
+        { orderable: true, visible: false, className: "reorder", targets: 0 }, // Column must be orderable for rowReorder
+        { orderable: false, targets: "_all" },
+      ],
+      language: {
+        search: "Filter: ",
+      },
     });
 
-    $("#modal-service-offering-form").submit(function (e) {
-        adacServiceOfferingVM.modalSubmit(e);
+    // Re-Order Event
+    table.on("row-reorder", function (e, diff, edit) {
+      // Find the row that was moved
+      var triggerRow = diff.filter(function (row) {
+        return row.node == edit.triggerRow.node();
+      })[0];
+
+      //AJAX Update
+      $.ajax({
+        url:
+          $(triggerRow.node).data("resource-url") +
+          "/" +
+          $(triggerRow.node).data("offering-id") +
+          "/move",
+        type: "POST",
+        dataType: "json",
+        data: {
+          id: $(triggerRow.node).data("offering-id"),
+          name: $(triggerRow.node).data("offering-name"),
+          sortOrder: triggerRow.newPosition + 1, //1-indexable
+        },
+      });
     });
+  });
 
-    $(".delete-confirm").click(function (e) {
-        e.preventDefault();
+  $("#modal-service-offering-form").submit(function (e) {
+    adacServiceOfferingVM.modalSubmit(e);
+  });
 
-        var $link = $(this);
-        var linkData = $link.data("refdata-model")
-        var url = $link.data("resource-url") + "/" + linkData.Id;
+  $(".delete-confirm").click(function (e) {
+    e.preventDefault();
 
-        bootbox.confirm("Are you sure you want to delete " + linkData.Name + "?",
-            function (confirmation) {
-                if (confirmation) {
-                    deleteRefData(url, $link.data("success-redirect"), $link.data("refdata-type"));
-                }
-            }
-        );
-    });
+    var $link = $(this);
+    var linkData = $link.data("refdata-model");
+    var url = $link.data("resource-url") + "/" + linkData.id;
 
-    adacServiceOfferingVM = new AdacServiceOfferingViewModel();
-    ko.applyBindings(adacServiceOfferingVM);
+    bootbox.confirm(
+      "Are you sure you want to delete " + linkData.name + "?",
+      function (confirmation) {
+        if (confirmation) {
+          deleteRefData(
+            url,
+            $link.data("success-redirect"),
+            $link.data("refdata-type")
+          );
+        }
+      }
+    );
+  });
+
+  adacServiceOfferingVM = new AdacServiceOfferingViewModel();
+  ko.applyBindings(adacServiceOfferingVM);
 });
--- src/Submissions/Api/Frontend/js/pages/Admin/adac-storage-temperatures.js
+++ src/Submissions/Api/Frontend/js/pages/Admin/adac-storage-temperatures.js
@@ -2,163 +2,186 @@
 var adacStorageTemperatureVM;
 
 function StorageTemperature(id, value, sortOrder) {
-    this.id = id;
-    this.value = ko.observable(value);
-    this.sortOrder = sortOrder;
+  this.id = id;
+  this.value = ko.observable(value);
+  this.sortOrder = sortOrder;
 }
 
 function StorageTemperatureModal(id, value, sortOrder) {
-    this.modalModeAdd = "Add";
-    this.modalModeEdit = "Update";
+  this.modalModeAdd = "Add";
+  this.modalModeEdit = "Update";
 
-    this.mode = ko.observable(this.modalModeAdd);
+  this.mode = ko.observable(this.modalModeAdd);
 
-    this.storageTemperature = ko.observable(
-        new StorageTemperature(id, value, sortOrder)
-    );
+  this.storageTemperature = ko.observable(
+    new StorageTemperature(id, value, sortOrder)
+  );
 }
 
 function AdacStorageTemperatureViewModel() {
-    var _this = this;
+  var _this = this;
 
-    this.modalId = "#storage-temperatures-modal";
-    this.modal = new StorageTemperatureModal(0, "", 0);
-    this.dialogErrors = ko.observableArray([]);
+  this.modalId = "#storage-temperatures-modal";
+  this.modal = new StorageTemperatureModal(0, "", 0);
+  this.dialogErrors = ko.observableArray([]);
 
-    this.showModal = function () {
-        _this.dialogErrors.removeAll(); //clear errors on a new show
-        $(_this.modalId).modal("show");
-    };
+  this.showModal = function () {
+    _this.dialogErrors.removeAll(); //clear errors on a new show
+    $(_this.modalId).modal("show");
+  };
 
-    this.hideModal = function () {
-        $(_this.modalId).modal("hide");
-    };
+  this.hideModal = function () {
+    $(_this.modalId).modal("hide");
+  };
 
-    this.openModalForAdd = function () {
-        _this.modal.mode(_this.modal.modalModeAdd);
-        _this.modal.storageTemperature(new StorageTemperature(0, "", 0));
-        _this.showModal();
-    };
+  this.openModalForAdd = function () {
+    _this.modal.mode(_this.modal.modalModeAdd);
+    _this.modal.storageTemperature(new StorageTemperature(0, "", 0));
+    _this.showModal();
+  };
 
-    this.openModalForEdit = function (_, event) {
-        _this.modal.mode(_this.modal.modalModeEdit);
+  this.openModalForEdit = function (_, event) {
+    _this.modal.mode(_this.modal.modalModeEdit);
 
-        var storageTemperature = $(event.currentTarget).data("storage-temperature");
+    var storageTemperature = $(event.currentTarget).data("storage-temperature");
 
-        _this.modal.storageTemperature(
-            new StorageTemperature(
-                storageTemperature.Id,
-                storageTemperature.Value,
-                storageTemperature.SortOrder
-            )
-        );
+    _this.modal.storageTemperature(
+      new StorageTemperature(
+        storageTemperature.id,
+        storageTemperature.value,
+        storageTemperature.sortOrder
+      )
+    );
 
-        _this.showModal();
-    };
+    _this.showModal();
+  };
 
-    this.modalSubmit = function (e) {
-        e.preventDefault();
-        var form = $(e.target); // get form as a jquery object
+  this.modalSubmit = function (e) {
+    e.preventDefault();
+    var form = $(e.target); // get form as a jquery object
 
-        // Get Action Type
-        var action = _this.modal.mode();
-        if (action == 'Add') {
-            addRefData(_this, form.data("resource-url"), form.serialize(),
-                form.data("success-redirect"), form.data("refdata-type")); // cf. adac-refdata-utility.js
-        } else if (action == 'Update') {
-            editRefData(_this, form.data("resource-url") + '/' + $(e.target.Id).val(), form.serialize(),
-                form.data("success-redirect"), form.data("refdata-type"));
-        }
-    };
+    // Get Form Data
+    var data = serializeFormData(form);
+
+    // Get Action Type
+    var action = _this.modal.mode();
+    if (action == "Add") {
+      addRefData(
+        _this,
+        form.data("resource-url"),
+        data,
+        form.data("success-redirect"),
+        form.data("refdata-type")
+      ); // cf. adac-refdata-utility.js
+    } else if (action == "Update") {
+      editRefData(
+        _this,
+        form.data("resource-url") + "/" + $(e.target.Id).val(),
+        data,
+        form.data("success-redirect"),
+        form.data("refdata-type")
+      );
+    }
+  };
 }
 
 $(function () {
-    $("#modal-storage-temperatures-form").submit(function (e) {
-        adacStorageTemperatureVM.modalSubmit(e);
-    });
+  $("#modal-storage-temperatures-form").submit(function (e) {
+    adacStorageTemperatureVM.modalSubmit(e);
+  });
 
-    $(".delete-confirm").click(function (e) {
-        e.preventDefault();
+  $(".delete-confirm").click(function (e) {
+    e.preventDefault();
 
-        var $link = $(this);
-        var linkData = $link.data("refdata-model")
-        var url = $link.data("resource-url") + "/" + linkData.Id;
+    var $link = $(this);
+    var linkData = $link.data("refdata-model");
+    var url = $link.data("resource-url") + "/" + linkData.id;
 
-        bootbox.confirm("Are you sure you want to delete " + linkData.Description + "?",
-            function (confirmation) {
-                if (confirmation) {
-                    deleteRefData(url, $link.data("success-redirect"), $link.data("refdata-type"));
-                }
-            }
-        );
-    });
+    bootbox.confirm(
+      "Are you sure you want to delete " + linkData.description + "?",
+      function (confirmation) {
+        if (confirmation) {
+          deleteRefData(
+            url,
+            $link.data("success-redirect"),
+            $link.data("refdata-type")
+          );
+        }
+      }
+    );
+  });
 
-    adacStorageTemperatureVM = new AdacStorageTemperatureViewModel();
-    ko.applyBindings(adacStorageTemperatureVM);
+  adacStorageTemperatureVM = new AdacStorageTemperatureViewModel();
+  ko.applyBindings(adacStorageTemperatureVM);
 });
 
 $(function () {
-    $("#change-storage-temperature-name-form").submit(function (e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "UpdateReferenceTermName",
-            data: { newReferenceTermKey: "site.display.preservation.name", newReferenceTermName: document.getElementById("StorageTemperatureName").value},
-            dataType: "json",
-            success: function () {
-                location.reload();
-            }
-        });
+  $("#change-storage-temperature-name-form").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/Admin/SiteConfig/UpdateReferenceTermName",
+      data: {
+        newReferenceTermKey: "site.display.storagetemperature.name",
+        newReferenceTermName: document.getElementById("StorageTemperatureName")
+          .value,
+      },
+      dataType: "json",
+      success: function () {
+        location.reload();
+      },
     });
+  });
 
-    $("#storageTemperatureTitle").click(function () {
-        document.getElementById("titleName").setAttribute("hidden", true);
-        document.getElementById("change-storage-temperature-name-form").removeAttribute("hidden");
+  $("#storageTemperatureTitle").click(function () {
+    document.getElementById("titleName").setAttribute("hidden", true);
+    document
+      .getElementById("change-storage-temperature-name-form")
+      .removeAttribute("hidden");
+  });
 
-    });
-
-    $("#storageTemperatureTitleCancel").click(function () {
-        document.getElementById("change-storage-temperature-name-form").setAttribute("hidden", true);
-        document.getElementById("titleName").removeAttribute("hidden");
-
-    });
-
+  $("#storageTemperatureTitleCancel").click(function () {
+    document
+      .getElementById("change-storage-temperature-name-form")
+      .setAttribute("hidden", true);
+    document.getElementById("titleName").removeAttribute("hidden");
+  });
 });
-
 
 // DataTables
 $(function () {
-    var table = $("#storage-temperatures")["DataTable"]({
-        paging: false,
-        info: false,
-        autoWidth: false,
-        rowReorder: true,
-        columnDefs: [
-            { orderable: true, "visible": false, className: 'reorder', targets: 0 }, // Column must be orderable for rowReorder
-            { orderable: false, targets: '_all' }
-        ],
-        language: {
-            search: "Filter: ",
-        },
+  var table = $("#storage-temperatures")["DataTable"]({
+    paging: false,
+    info: false,
+    autoWidth: false,
+    rowReorder: true,
+    columnDefs: [
+      { orderable: true, visible: false, className: "reorder", targets: 0 }, // Column must be orderable for rowReorder
+      { orderable: false, targets: "_all" },
+    ],
+    language: {
+      search: "Filter: ",
+    },
+  });
+
+  // Re-Order Event
+  table.on("row-reorder", function (e, diff, edit) {
+    // Find the row that was moved
+    var triggerRow = diff.filter(function (row) {
+      return row.node == edit.triggerRow.node();
+    })[0];
+    var $row = $(triggerRow.node);
+
+    //AJAX Update
+    $.ajax({
+      url: $row.data("resource-url") + "/" + $row.data("id") + "/move",
+      type: "POST",
+      dataType: "json",
+      data: {
+        id: $row.data("id"),
+        value: $row.data("value"),
+        sortOrder: triggerRow.newPosition + 1, //1-indexable
+      },
     });
-
-    // Re-Order Event
-    table.on('row-reorder', function (e, diff, edit) {
-
-        // Find the row that was moved
-        var triggerRow = diff.filter(function(row) { return row.node == edit.triggerRow.node()})[0];
-        var $row = $(triggerRow.node);
-
-        //AJAX Update
-        $.ajax({
-            url: $row.data('resource-url') + "/" + $row.data('id') + "/move",
-            type: 'POST',
-            dataType: 'json',
-            data: {
-                id: $row.data('id'),
-                value: $row.data('value'),
-                sortOrder: (triggerRow.newPosition + 1) //1-indexable
-            }
-        });
-    });
+  });
 });
