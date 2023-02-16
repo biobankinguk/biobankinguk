6632234a9a389fe0f5cc91334920a195bbd57108
Andy Rae <hello@rae.li>
2023-02-16 15:36:08 +0000

    Fix submissions journeys

--- src/Submissions/Api/Frontend/js/pages/Biobank/submissions.js
+++ src/Submissions/Api/Frontend/js/pages/Biobank/submissions.js
@@ -1,31 +1,35 @@
 ﻿function copyText(fieldId) {
-    /* Get the text field */
-    var source = document.getElementById(fieldId);
+  /* Get the text field */
+  var source = document.getElementById(fieldId);
 
-    /* Select the text field */
-    source.select();
-    source.setSelectionRange(0, 99999); /* For mobile devices */
+  /* Select the text field */
+  source.select();
+  source.setSelectionRange(0, 99999); /* For mobile devices */
 
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
 }
 
 //copy text
 $(".copy-text").click(function (e) {
-    e.preventDefault();
-    var $btn = $(this);
-    var txtbox = $btn.data("target")
-    copyText(txtbox);
+  e.preventDefault();
+  var $btn = $(this);
+  var txtbox = $btn.data("target");
+  copyText(txtbox);
 });
 
 //generate client id
 $("#generatekey").click(function (e) {
   var $btn = $(this);
-  $.post($btn.data("generate-url"), {
-    biobankId: $btn.data("biobank-id")
-  }, function (data) {
-    $("#clientId").val(data.ClientId);
-    $("#clientSecret").val(data.ClientSecret);
-    $("#clientSecretWrapper").removeAttr("hidden");
-  });
+  $.post(
+    $btn.data("generate-url"),
+    {
+      biobankId: $btn.data("biobank-id"),
+    },
+    function (data) {
+      $("#clientId").val(data.clientId);
+      $("#clientSecret").val(data.clientSecret);
+      $("#clientSecretWrapper").removeAttr("hidden");
+    }
+  );
 });
