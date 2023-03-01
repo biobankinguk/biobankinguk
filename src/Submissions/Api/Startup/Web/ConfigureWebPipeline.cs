1532f3ecdea16947061d060d5f86f95f6edf6623
MoizIqbal97 <90258816+MoizIqbal97@users.noreply.github.com>
2023-03-01 11:26:53 +0000

    AB#89361: Admin Analytics testing (#526)
    * initial fixes- di fix
    
    * tested the analytical controller
    
    * routing fix
    
    * removed anonomoys
    
    * imports
    
    * fixed js bundeling
    
    * removed
    
    * fixed references
    
    * navigation changes
    
    * navigation bar changes
    
    * Add missing profiles
    
    * Revert changes
    
    * Revert view changes
    
    * hangfire changes
    
    * analytics report
    
    * removed console.log
    
    ---------
    
    Co-authored-by: Andy Rae <hello@rae.li>
    Co-authored-by: Philip Van Krimpen <squigglebunchkins@gmail.com>

--- src/Submissions/Api/Startup/Web/ConfigureWebPipeline.cs
+++ src/Submissions/Api/Startup/Web/ConfigureWebPipeline.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using Biobanks.Submissions.Api.Auth;
 using ClacksMiddleware.Extensions;
+using Core.Jobs;
 using Hangfire;
+using Hangfire.Common;
 using Hangfire.Dashboard;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerUI;
@@ -105,6 +108,7 @@ public static class ConfigureWebPipeline
 
   private static WebApplication UseAndMapHangfireDashboard(this WebApplication app)
   {
+
     // In Development, Map the dashboard without any auth
     if (app.Environment.IsDevelopment())
     {
@@ -124,6 +128,9 @@ public static class ConfigureWebPipeline
 
     // Add the middleware
     app.UseHangfireDashboard();
+
+    // Run Hangfire Jobs
+    RecurringJob.AddOrUpdate<AnalyticsJob>("analyticjob", x => x.Run(), Cron.Yearly(4));
 
     return app;
   }
