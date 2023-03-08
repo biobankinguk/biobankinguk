652f463ccdccf09f4ef80d74387393343fbe47d5
Andy Rae <hello@rae.li>
2023-03-08 09:41:21 +0000

    99216 Fix ADT groups building

--- src/Submissions/Api/Areas/Admin/Controllers/ReferenceDataController.cs
+++ src/Submissions/Api/Areas/Admin/Controllers/ReferenceDataController.cs
@@ -525,16 +525,16 @@ public class ReferenceDataController : Controller
           )
           .ToList();
 
-      var groups = associatedDataTypes
-          .Where(x => x.AssociatedDataTypeGroup != null)
-          .GroupBy(x => x.AssociatedDataTypeGroupId)
-          .Select(x => x.First())
-          .Select(x => new AssociatedDataTypeGroupModel
-          {
-              AssociatedDataTypeGroupId = x.AssociatedDataTypeGroup.Id,
-              Name = x.AssociatedDataTypeGroup.Value,
-          })
-          .ToList();
+      var groups = (await _associatedDataTypeGroupService.List())
+        .Select(x =>
+          Task.Run(() => Task.FromResult(new AssociatedDataTypeGroupModel
+            {
+              AssociatedDataTypeGroupId = x.Id,
+              Name = x.Value
+            }))
+            .Result
+        )
+        .ToList();
 
       return View(new AssociatedDataTypesModel
       {
