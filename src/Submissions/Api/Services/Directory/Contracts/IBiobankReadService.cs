e9446d6941d120c2bb14e4a07db0bfa20ea495b3
MoizIqbal97 <90258816+MoizIqbal97@users.noreply.github.com>
2023-03-14 15:56:25 +0000

    AB#39005: Generic Repo Replace (#564)
    * started replacing the generic repo
    
    * deleteBiobankfix
    
    * write service gets rid of generic repo
    
    * fixed an error
    
    * disease status update
    
    * logostorage update
    
    * biobank service update generic repo
    
    * read service genric repo changes
    
    * read and write service changes
    
    * fix warning
    
    * resolved comments

--- src/Submissions/Api/Services/Directory/Contracts/IBiobankReadService.cs
+++ src/Submissions/Api/Services/Directory/Contracts/IBiobankReadService.cs
@@ -9,15 +9,6 @@ namespace Biobanks.Submissions.Api.Services.Directory.Contracts
 {
     public interface IBiobankReadService
     {
-        Task<Collection> GetCollectionByIdAsync(int id);
-        Task<bool> IsCollectionFromApi(int id);
-
-        Task<Collection> GetCollectionByIdForIndexingAsync(int id);
-        Task<Collection> GetCollectionWithSampleSetsByIdAsync(int id);
-        Task<IEnumerable<int>> GetCollectionIdsByOntologyTermAsync(string ontologyTerm);
-        Task<IEnumerable<Collection>> ListCollectionsAsync();
-        Task<IEnumerable<Collection>> ListCollectionsAsync(int organisationId);
-        Task<SampleSet> GetSampleSetByIdForIndexingAsync(int id);
         bool CanThisBiobankAdministerThisCollection(int biobankId, int collectionId);
         bool CanThisBiobankAdministerThisSampleSet(int biobankId, int sampleSetId);
         bool CanThisBiobankAdministerThisCapability(int biobankId, int capabilityId);
@@ -29,14 +20,8 @@ namespace Biobanks.Submissions.Api.Services.Directory.Contracts
         " Any changes made here will need to be made in the corresponding service."
         , false)]
         Task<int> GetExtractionProcedureMaterialDetailsCount(string id);
-        Task<bool> IsExtractionProcedureInUse(string id);
         Task<int> GetMaterialTypeMaterialDetailCount(int id);
 
         Task<int> GetServiceOfferingOrganisationCount(int id);
-
-        Task<IEnumerable<SampleSet>> GetSampleSetsByIdsForIndexingAsync(IEnumerable<int> sampleSetIds);
-
-        Task<IEnumerable<SampleSet>> GetSampleSetsByIdsForIndexDeletionAsync(IEnumerable<int> sampleSetIds);
-
     }
 }
