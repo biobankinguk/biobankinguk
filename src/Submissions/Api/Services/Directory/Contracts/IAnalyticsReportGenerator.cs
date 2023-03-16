f13e2d26f11b248cbd06ad95e5d5cc01a577e2ae
Andy Rae <hello@rae.li>
2023-03-16 11:02:59 +0000

    Clean up analytics report generator

--- src/Submissions/Api/Services/Directory/Contracts/IAnalyticsReportGenerator.cs
+++ src/Submissions/Api/Services/Directory/Contracts/IAnalyticsReportGenerator.cs
@@ -6,9 +6,7 @@ namespace Biobanks.Submissions.Api.Services.Directory.Contracts
 {
     public interface IAnalyticsReportGenerator
     {
-        void Dispose();
         Task<OrganisationReportDto> GetBiobankReport(int Id, int year, int quarter, int period);
-        Task<DirectoryAnalyticReportDTO> GetDirectoryReport(int year, int quarter, int period);
         Task<ProfileStatusDTO> GetProfileStatus(string biobankId);
     }
 }
